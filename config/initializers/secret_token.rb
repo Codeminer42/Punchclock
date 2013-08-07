96823581889d9831b1da4cf891297b9b18baaaae
Eduardo Gurgel <eduardo.gurgel@codeminer42.com>
2013-08-07 15:29:26 -0300

    Bootstrap Rails 4, rspec and guard

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Punchclock::Application.config.secret_key_base = 'bd9c547600bbc13f2444d7d218287b5ef5fb4d53d338f7e656f24c970e1539365ba885e9324662dad7f205d217a9eec4dc7f024931d1ac8f0857696fff730fdf'
