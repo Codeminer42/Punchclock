7408a5b671ae5969eaf62e8a66e9c507948eee1c
Giovanni Oliveira <giovanni.oliveira@gmail.com>
2013-08-08 19:17:08 -0300

    Add Google Apps authentication
    Add devise for login support
    Add omniauth for authentication with Google Apps
    Add settings logic and dotenv

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Punchclock::Application.routes.draw do
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  get 'home/logout'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -39,7 +42,7 @@ Punchclock::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
