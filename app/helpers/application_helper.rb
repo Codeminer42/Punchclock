9f1521be105299984970cf19ea509d746a9613ba
Giovanni Oliveira <giovanni.oliveira@gmail.com>
2013-08-27 16:00:17 -0300

    Add time_format helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def time_format(hours)
+    hour = (hours - hours % 1).to_i.to_s
+    mins = ((hours % 1) * 60).to_i
+    mins = mins < 10 ? ('0' + mins.to_s) : mins.to_s
+    hour + ':' + mins
+  end
 end
