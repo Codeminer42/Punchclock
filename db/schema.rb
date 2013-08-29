40fa4a80646e100a69a0f1e214119fa51d29bb0a
Giovanni Oliveira <giovanni.oliveira@gmail.com>
2013-08-29 14:51:05 -0300

    Add user migration for login with database instead of omniauth

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130820185522) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+ActiveRecord::Schema.define(version: 20130829164634) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -68,8 +65,8 @@ ActiveRecord::Schema.define(version: 20130820185522) do
   add_index "punches", ["user_id"], name: "index_punches_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "email",              default: "", null: false
-    t.integer  "sign_in_count",      default: 0
+    t.string   "email",                  default: "", null: false
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -77,8 +74,13 @@ ActiveRecord::Schema.define(version: 20130820185522) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
