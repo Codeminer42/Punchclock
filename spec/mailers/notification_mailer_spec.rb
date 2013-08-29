40a884c1f384d60fdeb21dcb58e554de5c9947ed
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-08-29 15:55:54 -0300

    add a simple spec for mailer

---
+++ spec/mailers/notification_mailer_spec.rb
@@ -0,0 +1,24 @@
+require "spec_helper"
+
+describe NotificationMailer do
+  describe "notification email" do
+  	let(:user) { User.new(name:"username", email:"username@domain.sf") }
+  	let(:mail) { NotificationMailer.notify_user_registration(user) }
+
+  	it 'renders the subject' do
+  		mail.subject.should == 'Welcome to Punchclock'
+  	end
+
+    it 'renders the receiver email' do
+      mail.to.should == [user.email]
+    end
+ 
+    it 'renders the sender email' do
+      mail.from.should == ['do-not-reply@punchclock.com']
+    end
+ 
+    it 'assigns @name' do
+      mail.body.encoded.should match(user.name)
+    end
+  end
+end
