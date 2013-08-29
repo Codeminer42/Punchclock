f396232e996906a0806a6d0afa15a9ada1c67fd9
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-08-29 16:12:13 -0300

    add action mailer params for production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,16 @@ Punchclock::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #Configure e-mail
+  ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.raise_delivery_errors = true
+  ActionMailer::Base.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => ENV['GMAIL_USERNAME'],
+    :password             => ENV['GMAIL_PASSWORD'],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }  
 end
