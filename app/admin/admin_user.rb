dec0e049774b4495ed52a8c5498012214635173c
Giovanni Oliveira <giovanni.oliveira@gmail.com>
2013-08-30 14:17:50 -0300

    Fix form

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -1,27 +1,9 @@
 ActiveAdmin.register AdminUser do
   index do
-    if current_admin_user.is_super
-      column :company
-      column :email
-      column :is_super
-      default_actions
-    else
-      column :email do |admin|
-        next if admin.company_id != current_admin_user.company_id
-        mail_to admin.email
-      end
-      #actions
-      column '' do |admin|
-        next if admin.company_id != current_admin_user.company_id
-
-        links = ''.html_safe
-        links += link_to "View", admin_admin_user_path(admin)
-        links += ' '
-        links += link_to "Edit", edit_admin_admin_user_path(admin) unless admin.is_super
-        links += ' '
-        links += link_to "Delete", admin_admin_user_path(admin), :method => :delete unless admin.is_super
-      end
-    end
+    column :company
+    column :email
+    column :is_super if current_admin_user.is_super
+    default_actions
   end
 
   filter :email
@@ -31,14 +13,11 @@ ActiveAdmin.register AdminUser do
       f.input :email
       f.input :password
       f.input :password_confirmation
-      f.input :is_super, label: 'CAN MANAGE ALL COMPANIES?', :input_html => { disabled: !current_admin_user.is_super }
+      f.input :is_super, label: 'CAN MANAGE ALL COMPANIES?'
       if current_admin_user.is_super
         f.input :company
       else
-        f.input :company, :input_html => {
-          value: current_admin_user.company_id,
-          disabled: true
-        }, collection: {
+        f.input :company, collection: {
           current_admin_user.company.name => current_admin_user.company_id
         }
       end
@@ -50,7 +29,7 @@ ActiveAdmin.register AdminUser do
       if current_admin_user.is_super?
         AdminUser.all
       else
-        AdminUser.where(company_id: current_admin_user.company_id)
+        current_admin_user.company.admin_users
       end
     end
 
