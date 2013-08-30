e0f63160aa469fc6f38ef5df0fc213d417cdb91f
Giovanni Oliveira <giovanni.oliveira@gmail.com>
2013-08-30 18:43:48 -0300

    Add cancan to all controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path, alert: exception.message
+  end
+
+  def current_ability
+    @current_ability ||= Ability.new(current_admin_user)
+  end
 end
