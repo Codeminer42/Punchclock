2b869a93292686ee0cbe74a5e65f33380f7ad5a8
Giovanni Oliveira <giovanni.oliveira@gmail.com>
2013-08-30 18:47:15 -0300

    Add access check to admin_users

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -1,12 +1,14 @@
+require 'active_admin_can_can'
+
 ActiveAdmin.register AdminUser do
+  filter :email
+
   index do
     column :company
     column :email
-    column :is_super if current_admin_user.is_super
+    column :is_super if can? :manage_all, Company
     default_actions
   end
-
-  filter :email
 
   form do |f|
     f.inputs "Admin Details" do
@@ -14,7 +16,7 @@ ActiveAdmin.register AdminUser do
       f.input :password
       f.input :password_confirmation
       f.input :is_super, label: 'CAN MANAGE ALL COMPANIES?'
-      if current_admin_user.is_super
+      if current_admin_user.is_super?
         f.input :company
       else
         f.input :company, collection: {
@@ -24,7 +26,15 @@ ActiveAdmin.register AdminUser do
     end
     f.actions
   end
+
   controller do
+    include ActiveAdminCanCan
+    load_resource AdminUser
+
+    before_filter do
+      authorize! AdminUser
+    end
+
     def scoped_collection
       if current_admin_user.is_super?
         AdminUser.all
@@ -34,8 +44,8 @@ ActiveAdmin.register AdminUser do
     end
 
     def permitted_params
-      params.permit admin_user: [:email, :password, :password_confirmation,
-                                 :is_super, :company_id]
+      params.permit admin_user: [ :email, :password, :password_confirmation,
+                                 :is_super, :company_id ]
     end
   end
 end
