3ee343265e87ab285243f3256a4c1341dbb0e2b8
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-09-06 11:09:25 -0300

    fix factories admin and super users

--- spec/factories/admins.rb
+++ spec/factories/admins.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
-  factory :admin do
+  factory :admin_user do
     email { Faker::Internet.email }
     password { Faker::Lorem.characters(8) }
-    company_id { Company.create(name:Faker::Name.name).id }
+    company_id { Company.create(name:Faker::Company.name).id }
   end
 end
--- spec/factories/supers.rb
+++ spec/factories/supers.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
-  factory :super do
+  factory :super, class: AdminUser do
     email { Faker::Internet.email }
     password { Faker::Lorem.characters(8) }
-    company_id { Company.create(name:Faker::Name.name).id }
+    company_id { Company.create(name:Faker::Company.name).id }
     is_super { true }
   end
 end
