42226352ef8750a50348b2173428514182354889
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-09-06 14:27:44 -0300

    add abilities to manage resources

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,10 +2,14 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    can :read, ActiveAdmin::Page, :name => "Dashboard"
+
     if user.is_super?
-      can :manage_all, Company
+      can :manage, Company
+      can :manage, AdminUser
     else
-      can :manage_own, Company
+      can :manage, Company, :id => user.company.id
+      can :manage, AdminUser, :company_id => user.company.id, :is_super => nil
     end
   end
 end
