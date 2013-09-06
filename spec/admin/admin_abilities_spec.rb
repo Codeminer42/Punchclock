8a6561df3148c7e40532f672ef1849b654bc3135
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-09-06 11:21:27 -0300

    add spec for admin/super abilities

---
+++ spec/admin/admin_abilities_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+require 'cancan/matchers'
+
+describe "AdminUser" do
+	describe "abilities" do
+		subject(:ability){ Ability.new(user) }
+		let(:user){ nil }
+
+		context "when is an account manager" do
+			let(:user) { FactoryGirl.create(:admin_user) }
+
+			it { should be_able_to(:manage, Company.find(user.company.id)) }
+			it { should_not be_able_to(:manage, Company.new) }
+			it { should be_able_to(:manage, AdminUser.new(company_id: user.company.id)) }
+			it { should_not be_able_to(:manage, AdminUser.new(company_id: user.company.id, is_super:true)) }
+			it { should_not be_able_to(:manage, AdminUser.new) }
+		end
+
+		context "when is an super account manager" do
+			let(:user) { FactoryGirl.create(:super) }
+
+			it { should be_able_to(:manage, Company.new) }
+			it { should be_able_to(:manage, AdminUser.new) }
+		end
+	end
+end
