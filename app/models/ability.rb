79099a51329c9fe51309e99841fca09867a064a4
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-09-06 14:56:39 -0300

    add super abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,8 +5,11 @@ class Ability
     can :read, ActiveAdmin::Page, :name => "Dashboard"
 
     if user.is_super?
-      can :manage, Company
       can :manage, AdminUser
+      can :manage, Company
+      can :manage, Project
+      can :manage, User
+      can :manage, Punch
     else
       can :manage, Company, :id => user.company.id
       can :manage, AdminUser, :company_id => user.company.id, :is_super => nil
