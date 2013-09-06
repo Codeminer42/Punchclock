714f1ba1e53f81c6472b09525c9d6277e6f8ad8e
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-09-06 14:28:11 -0300

    fix permissions and inc security for admin_user dashboard

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -1,12 +1,10 @@
-require 'active_admin_can_can'
-
 ActiveAdmin.register AdminUser do
   filter :email
 
   index do
     column :company
     column :email
-    column :is_super if can? :manage_all, Company
+    column :is_super if can? :manage_all, Company if current_admin_user.is_super?
     default_actions
   end
 
@@ -15,8 +13,8 @@ ActiveAdmin.register AdminUser do
       f.input :email
       f.input :password
       f.input :password_confirmation
-      f.input :is_super, label: 'CAN MANAGE ALL COMPANIES?'
       if current_admin_user.is_super?
+        f.input :is_super, label: 'CAN MANAGE ALL COMPANIES?'
         f.input :company
       else
         f.input :company, collection: {
@@ -28,24 +26,18 @@ ActiveAdmin.register AdminUser do
   end
 
   controller do
-    include ActiveAdminCanCan
-    load_resource AdminUser
-
-    before_filter do
-      authorize! AdminUser
-    end
-
-    def scoped_collection
-      if current_admin_user.is_super?
-        AdminUser.all
-      else
-        current_admin_user.company.admin_users
-      end
-    end
-
     def permitted_params
       params.permit admin_user: [ :email, :password, :password_confirmation,
-                                 :is_super, :company_id ]
+                                  :is_super, :company_id ]
+    end
+
+    def new
+      if current_admin_user.is_super?
+        @admin_user = AdminUser.new
+      else
+        @admin_user = AdminUser.new(company_id:current_admin_user.company.id)
+      end
+      new!
     end
   end
 end
