81a67ed103e3f399ea35a97a69b89627f1627798
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-09-09 09:10:27 -0300

    add actions to create a new user

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -13,6 +13,13 @@ ActiveAdmin.register User do
     f.inputs do
       f.input :name
       f.input :email
+      if current_admin_user.is_super?
+        f.input :company
+      else
+        f.input :company, collection: {
+          user.company.name => current_admin_user.company_id
+        }
+      end
       f.input :password
     end
     f.actions
@@ -20,15 +27,21 @@ ActiveAdmin.register User do
 
   controller do
     def permitted_params
-      params.permit user: [:name, :email, :password]
+      params.permit user: [:name, :email, :company_id, :password]
+    end
+
+    def new
+      @user = User.new
+      @user.company_id = current_admin_user.company.id unless current_admin_user.is_super?
+      new!
     end
 
     def create
-      create! do |success, failure| 
+      create! do |success, failure|
         success.html do
           NotificationMailer.notify_user_registration(@user)
           redirect_to resource_path
-        end        
+        end
       end
     end
   end
