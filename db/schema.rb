3684a0b1f6f8e05f0644128bf0c102fc64df19d1
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-09-10 12:45:01 -0300

    update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130902135446) do
+ActiveRecord::Schema.define(version: 20130909134132) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -71,8 +74,10 @@ ActiveRecord::Schema.define(version: 20130902135446) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "company_id"
   end
 
+  add_index "punches", ["company_id"], name: "index_punches_on_company_id", using: :btree
   add_index "punches", ["project_id"], name: "index_punches_on_project_id", using: :btree
   add_index "punches", ["user_id"], name: "index_punches_on_user_id", using: :btree
 
@@ -90,8 +95,10 @@ ActiveRecord::Schema.define(version: 20130902135446) do
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer  "company_id"
   end
 
+  add_index "users", ["company_id"], name: "index_users_on_company_id", using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
