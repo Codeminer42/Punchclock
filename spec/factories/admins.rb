9a5a3ae5c6b351b179a963e6c0e0b025ad6ec08b
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-09-10 14:11:38 -0300

    refactor company factory

--- spec/factories/admins.rb
+++ spec/factories/admins.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   factory :admin_user do
     email { Faker::Internet.email }
     password { Faker::Lorem.characters(8) }
-    company_id { Company.create(name:Faker::Company.name).id }
+    company { FactoryGirl.create(:company) }
   end
 end
