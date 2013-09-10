d7ecba63c94cf29b2e94c9e7f440db3396aed5e7
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-09-10 15:45:51 -0300

    add permissions for Users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,19 +2,23 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :read, ActiveAdmin::Page, :name => "Dashboard"
+    if user.instance_of? AdminUser
+      can :read, ActiveAdmin::Page, :name => "Dashboard"
 
-    if user.is_super?
-      can :manage, AdminUser
-      can :manage, Company
-      can :manage, Project
-      can :manage, User
-      can :manage, Punch
-    else
-      can :manage, Company, id: user.company.id
-      can :manage, AdminUser, company_id: user.company.id, is_super: nil
-      can :manage, Project, company_id: user.company.id
-      can :manage, User, company_id: user.company.id
+      if user.is_super?
+        can :manage, AdminUser
+        can :manage, Company
+        can :manage, Project
+        can :manage, User
+        can :manage, Punch
+      else
+        can :manage, Company, id: user.company.id
+        can :manage, AdminUser, company_id: user.company.id, is_super: nil
+        can :manage, Project, company_id: user.company.id
+        can :manage, User, company_id: user.company.id
+        can :manage, Punch, company_id: user.company.id, user: { company: { id: user.company.id } }, project: { company: { id: user.company.id } }
+      end
+    elsif user.instance_of? User
       can :manage, Punch, company_id: user.company.id, user: { company: { id: user.company.id } }, project: { company: { id: user.company.id } }
     end
   end
