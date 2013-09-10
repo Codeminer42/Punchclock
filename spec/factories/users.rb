cd38c296e6e675451610ebbfd5ea538f980c671c
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-09-10 13:58:55 -0300

    fix factory for user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :user do
     email { Faker::Internet.email }
     password { Faker::Lorem.characters(8) }
+    company_id { Company.create(name:Faker::Company.name).id }
   end
 end
