95e71d5b8d4e8b94f8694128d0a2ba9911c43da6
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-09-12 11:33:58 -0300

    fix comma

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,7 +35,7 @@ end
 unless User.exists?(email: 'fernando.martinez@codeminer42.com')
   puts 'Creating default user'
   AdminUser.create(
-    name:                  'Fernando Martínez'
+    name:                  'Fernando Martínez',
     email:                 'fernando.martinez@codeminer42.com',
     password:              'password',
     company_id:            Company.find_by_name('Codeminer42').id
