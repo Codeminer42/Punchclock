3288173a16233eff50fe655d9ab3bb1070610d1c
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-09-12 18:58:34 -0300

    add update user info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,11 @@ class UsersController < ApplicationController
 	end
 
 	def update_user
+		current_user.update_attributes(name:params[:user][:name], email:params[:user][:email])
+		if current_user.save
+			flash[:notice] = "User updated successfully!"
+		end
+		redirect_to user_account_path
 	end
 
 	def account_password
@@ -10,4 +15,9 @@ class UsersController < ApplicationController
 
 	def update_password
 	end
+
+private
+  def permitted_params
+    params.permit(user: [:name, :email])
+  end
 end
