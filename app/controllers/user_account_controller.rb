5d1e19504101adad3b53a2bc10a4fd4a077eb7b1
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-09-13 10:57:10 -0300

    add routes for user account controller

--- app/controllers/user_account_controller.rb
+++ app/controllers/user_account_controller.rb
@@ -1,2 +1,22 @@
 class UserAccountController < ApplicationController
+	before_action :authenticate_user!
+	before_action :authorize
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+private
+	def user_params
+		params.permit(user: [:name, :email])
+	end
+
+	def authorize
+		redirect_to root_path unless params[:id] == current_user.id.to_s
+	end
 end
