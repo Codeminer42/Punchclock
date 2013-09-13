e5d6f1cb28aaf5b1d146807ec1f6693a3662858d
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-09-13 15:23:36 -0300

    remove old controller

--- spec/controllers/user_account_controller_spec.rb
+++ spec/controllers/user_account_controller_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe UserAccountController do
+login_user
 
+	describe "PATCH update_user" do
+		let(:user) { controller.current_user }
+
+		describe "when user update your account" do
+			it "update your name" do
+				user_params = {
+					name: 'Joselito'
+				}
+				params = {
+					user: user_params
+				}
+			end
+		end
+
+		describe "when user account update fails" do
+		end
+	end
 end
