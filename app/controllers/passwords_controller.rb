5cae25886a3b0291f8d3c82ba89b1493169910fe
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-09-16 09:05:49 -0300

    add notify user when password changes

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -5,6 +5,7 @@ class PasswordsController < ApplicationController
 		if current_user.update_with_password(user_params[:user])
 			sign_in(current_user, bypass: true)
 			flash[:notice] = "Password updated successfully!"
+			NotificationMailer.notify_user_password_change(current_user, user_params[:user][:password]).deliver
 			redirect_to edit_user_path(current_user)
 		else
 			flash.now[:alert] = current_user.errors.full_messages.first
