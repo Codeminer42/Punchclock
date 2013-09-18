d6afa7af2e88420f9b121dc33abef514ab193434
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-09-18 13:38:48 -0300

    add spec to admin's notification

--- spec/mailers/notification_mailer_spec.rb
+++ spec/mailers/notification_mailer_spec.rb
@@ -2,6 +2,39 @@ require "spec_helper"
 
 describe NotificationMailer do
   describe "notification email" do
+    context "when admin user has been registered" do
+      let(:admin_user) { FactoryGirl.create(:admin_user) }
+      let(:mail) { NotificationMailer.notify_admin_registration(admin_user) }
+
+      it 'renders the subject' do
+        mail.subject.should == 'You was registered on Punchclock'
+      end
+
+      it 'renders the receiver email' do
+        mail.to.should == [admin_user.email]
+      end
+
+      it 'renders the sender email' do
+        mail.from.should == ['do-not-reply@punchclock.com']
+      end
+
+      it 'should have a company' do
+        mail.body.encoded.should match(admin_user.company.name)
+      end
+
+      it 'assigns @email' do
+        mail.body.encoded.should match(admin_user.email)
+      end
+
+      it 'assigns @password' do
+        mail.body.encoded.should match(admin_user.password)
+      end
+
+      it 'assigns link to edit password path' do
+        mail.body.encoded.should have_link('here', href: edit_admin_admin_user_url(admin_user))
+      end
+    end
+
     context "when user has been registered" do
     	let(:user) { User.new(name:"username", email:"username@domain.sf", password:"12345678", password_confirmation:"12345678") }
     	let(:mail) { NotificationMailer.notify_user_registration(user) }
