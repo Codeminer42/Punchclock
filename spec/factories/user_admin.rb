0448c4cf5bd0cd1614f340f81f2f1a54b9c9f7ac
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-09-20 09:24:29 -0300

    add factory for user admin

---
+++ spec/factories/user_admin.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user_admin, class: User do
+    email { Faker::Internet.email }
+    password { Faker::Lorem.characters(8) }
+    company_id { Company.create(name:Faker::Company.name).id }
+    is_admin { true }
+  end
+end
