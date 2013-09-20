15428991141302fd082aa446d675f1a0a8efa086
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-09-20 13:47:44 -0300

    add new registration form

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,3 +1,11 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
+	def create
+		super
+	end
+
+private
+	def user_params
+		params.permit(user: [:name, :email, :company, :password, :password_confirmation])
+	end
 end
