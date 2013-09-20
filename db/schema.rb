47ac4906a29e1f9f70894262fee70358796638a6
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-09-20 09:27:14 -0300

    add is_admin migration to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130919141756) do
+ActiveRecord::Schema.define(version: 20130920122639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,6 +97,7 @@ ActiveRecord::Schema.define(version: 20130919141756) do
     t.datetime "remember_created_at"
     t.integer  "company_id"
     t.decimal  "hour_cost"
+    t.boolean  "is_admin"
   end
 
   add_index "users", ["company_id"], name: "index_users_on_company_id", using: :btree
