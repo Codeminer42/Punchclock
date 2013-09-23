48000c19997652d8ac6b31dac772d584430715a8
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-09-23 10:28:24 -0300

    fix user_admin factory

--- spec/factories/user_admin.rb
+++ spec/factories/user_admin.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :user_admin, class: User do
+  	name { Faker::Internet.name }
     email { Faker::Internet.email }
     password { Faker::Lorem.characters(8) }
     company_id { Company.create(name:Faker::Company.name).id }
