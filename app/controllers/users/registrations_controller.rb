1a0e48b839d49e1ac94b7399debac7974f431e94
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-09-23 09:36:10 -0300

    add custom signup page

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,11 +1,18 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
+	def new
+		build_resource({company: Company.new})
+		respond_with self.resource
+	end
+
 	def create
+		binding.pry
 		super
 	end
 
 private
-	def user_params
-		params.permit(user: [:name, :email, :company, :password, :password_confirmation])
+	def sign_up_params
+		allow = [:name, :email, :password, :password_confirmation, company_attributes: [:name]]
+		params.require(:user).permit(allow)
 	end
 end
