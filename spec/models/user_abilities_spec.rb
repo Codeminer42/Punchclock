89c1b451d5437e0ae131f96e4577bf783da62b6d
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-09-24 17:02:49 -0300

    add spec for user admin manage projects

--- spec/models/user_abilities_spec.rb
+++ spec/models/user_abilities_spec.rb
@@ -2,16 +2,24 @@ require 'spec_helper'
 require 'cancan/matchers'
 
 describe "User" do
-	describe "abilities" do
+	describe "user abilities" do
+		let(:user) { FactoryGirl.build(:user) }
 		subject(:ability){ UserAbility.new(user) }
-		let(:user){ nil }
 
 		context "when is creating punches" do
-			let(:user) { FactoryGirl.create(:user) }
-
 			it { should be_able_to(:manage, Punch.new(company_id: user.company.id, user: User.new(company_id: user.company.id), project: Project.new(company_id: user.company.id))) }
 			it { should_not be_able_to(:manage, Punch.new(company_id: user.company.id, user: User.new, project: Project.new(company_id: user.company.id))) }
 			it { should_not be_able_to(:manage, Punch.new(company_id: user.company.id, user: User.new(company_id: user.company.id), project: Project.new)) }
+			it { should_not be_able_to(:manage, Punch.new) }
+		end
+	end
+
+	describe "user admin abilities" do
+		let(:user) { FactoryGirl.build(:user, is_admin: true) }
+		subject(:ability){ UserAbility.new(user) }
+
+		context "when is creating projects" do
+			it { should be_able_to(:manage, Punch.new(company_id: user.company.id)) }
 			it { should_not be_able_to(:manage, Punch.new) }
 		end
 	end
