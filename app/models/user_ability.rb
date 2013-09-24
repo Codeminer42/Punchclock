f48181e7fae3e39bd29d1c95f7a420b6c8f30500
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-09-24 10:40:16 -0300

    add user admin ability for punches

--- app/models/user_ability.rb
+++ app/models/user_ability.rb
@@ -4,6 +4,7 @@ class UserAbility
   def initialize(user)
   	if user.is_admin?
     	can :manage, Punch, company: { id: user.company.id }
+    	can :update, Company, id: user.company.id
   	else
   		can :manage, Punch, company_id: user.company.id, user: { company: { id: user.company.id } }, project: { company: { id: user.company.id } }
   	end
