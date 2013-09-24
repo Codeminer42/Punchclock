c1bd7f790165e11db440cbab01dbeb67890a379a
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-09-24 17:03:01 -0300

    fix abilities for user admin

--- app/models/user_ability.rb
+++ app/models/user_ability.rb
@@ -3,9 +3,9 @@ class UserAbility
 
   def initialize(user)
   	if user.is_admin?
-    	can :manage, Punch, company: { id: user.company.id }
+    	can :manage, Punch, company_id: user.company.id
     	can :update, Company, id: user.company.id
-    	can :manage, Project, company: { id: user.company.id }
+    	can :manage, Project, company_id: user.company.id
   	else
   		can :manage, Punch, company_id: user.company.id, user: { company: { id: user.company.id } }, project: { company: { id: user.company.id } }
   	end
