608ad6ad891a36204eb26e0ac378c217ff1724ca
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-09-26 16:22:28 -0300

    add index for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
-class UsersController < ApplicationController
+class UsersController < InheritedResources::Base
+	before_action :authenticate_user!
+	load_and_authorize_resource except: [:create]
+
+private
+	def user_params
+		allow = [:name, :email, :password, :password_confirmation]
+		params.require(:user).permit(allow)
+	end
 end
