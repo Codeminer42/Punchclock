203729d2f81434821074519b20141f099d56ad3e
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-09-26 13:47:20 -0300

    add company controller update method

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -1,2 +1,19 @@
 class CompanyController < InheritedResources::Base
+	before_action :authenticate_user!
+	load_and_authorize_resource
+
+	def update
+		if @company.update(company_params)
+			flash.now[:notice] = "Company successful updated"
+			redirect_to root_url
+		else
+			render action: :edit
+		end
+	end
+
+private
+	def company_params
+		allow = [:name]
+		params.require(:company).permit(allow)
+	end
 end
