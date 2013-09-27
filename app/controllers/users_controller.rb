39a1d8463922316766485373fec557dc075da0f5
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-09-27 14:22:12 -0300

    add edit/update method to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < InheritedResources::Base
 	before_action :authenticate_user!
-	load_and_authorize_resource except: [:create]
+	load_and_authorize_resource
 
+	def update
+		if @user.update(user_params)
+			flash.now[:notice] = "User updated successfully!"
+		end
+		render :edit
+	end
 private
 	def user_params
-		allow = [:name, :email, :password, :password_confirmation]
+		allow = [:name, :email]
 		params.require(:user).permit(allow)
 	end
 end
