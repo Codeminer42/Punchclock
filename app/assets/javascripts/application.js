b4819ac744a245da56863e9f83651964146d8aa2
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-10-01 16:40:51 -0300

    add jquery_nested_form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery_nested_form
 //= require jquery
 //= require jquery_ujs
 // require turbolinks
