a6623c25b456e34ff7d20557aad31ce7e28aedf6
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-10-01 12:54:19 -0300

    add gravatar for an user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,11 @@ module ApplicationHelper
     mins = mins < 10 ? ('0' + mins.to_s) : mins.to_s
     hour + ':' + mins
   end
+
+  def gravatar_for(user, options = {size:50})
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
