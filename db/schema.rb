1e0fced7e314d0caf948f9ea17f9018849b084ed
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-10-02 10:27:39 -0300

    add company id to comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131001164016) do
+ActiveRecord::Schema.define(version: 20131002132529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,8 +58,10 @@ ActiveRecord::Schema.define(version: 20131001164016) do
     t.integer  "punch_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "company_id"
   end
 
+  add_index "comments", ["company_id"], name: "index_comments_on_company_id", using: :btree
   add_index "comments", ["punch_id"], name: "index_comments_on_punch_id", using: :btree
   add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
