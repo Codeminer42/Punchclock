05b8cadf341b5e1fdd72ea1a0200d3093626ac3b
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-10-03 14:04:33 -0300

    change relation punch with comment to has_one

--- app/models/punch.rb
+++ app/models/punch.rb
@@ -2,10 +2,10 @@ class Punch < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
   belongs_to :company
-  has_many :comments
+  has_one :comment
   validates :from, :to, :project_id, :user_id, :company_id, presence: true
   validate :check_time
-  accepts_nested_attributes_for :comments, allow_destroy: true
+  accepts_nested_attributes_for :comment, allow_destroy: true
 
   def delta
     (self.to - self.from) / 3600
