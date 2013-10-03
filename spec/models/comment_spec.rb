0debcf7a2327f5e932f2d5da288bc5605dafc774
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-10-03 17:59:46 -0300

    add test validations of Comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
 describe Comment do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:company_id) }
+  it { should validate_presence_of(:user_id) }
 end
