72bf4a2c5e4699338b7acb3fbbd526bc214f6304
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-10-04 13:04:04 -0300

    change factories to new syntax

--- spec/factories/admins.rb
+++ spec/factories/admins.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   factory :admin_user do
     email { Faker::Internet.email }
     password { Faker::Lorem.characters(8) }
-    company { FactoryGirl.create(:company) }
+    company
   end
 end
--- spec/factories/user_admin.rb
+++ spec/factories/user_admin.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
   	name { Faker::Internet.name }
     email { Faker::Internet.email }
     password { Faker::Lorem.characters(8) }
-    company_id { Company.create(name:Faker::Company.name).id }
+    company
     is_admin { true }
   end
 end
