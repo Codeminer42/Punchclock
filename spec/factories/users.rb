4191686f0f219072a51ab3958e7d6c593207930a
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-10-04 13:06:38 -0300

    remove unused spec that was covered on user_ability

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
   	name { Faker::Internet.name }
     email { Faker::Internet.email }
     password { Faker::Lorem.characters(8) }
-    company { FactoryGirl.create(:company) }
+    company
     hour_cost { 15.0 }
   end
 end
