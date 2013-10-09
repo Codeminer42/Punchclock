47a76df42064b59783a8c110a50dc316480ec6a8
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-10-09 13:22:32 -0300

    add index for notifications

--- app/controllers/notification_controller.rb
+++ app/controllers/notification_controller.rb
@@ -1,2 +1,8 @@
-class NotificationController < ApplicationController
+class NotificationController < InheritedResources::Base
+  before_action :authenticate_user!
+  load_and_authorize_resource
+
+  def index
+    @notifications = current_user.notifications
+  end
 end
