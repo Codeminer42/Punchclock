51dc447e729bd3c7fd4535ae784d7d7a56241840
Fernando Martínez <fernando.martinez@codeminer42.com>
2013-10-09 13:20:58 -0300

    add default path to event_path before save action

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Notification do
-  let(:notification) { FactoryGirl.create(:notification) }
+  let(:notification) { FactoryGirl.build(:notification) }
 
   it { should validate_presence_of(:user_id) }
   it { should validate_presence_of(:message) }
@@ -9,6 +9,13 @@ describe Notification do
   describe "notification has been sent from an user" do
     it "should have a from user" do
       notification.from.should_not be_nil
+    end
+  end
+
+  describe "save notification" do
+    it "should have a default event path when no path was set" do
+      n = Notification.create(user_id: notification.user_id, message: notification.message)
+      n.event_path.should eq('#')
     end
   end
 end
