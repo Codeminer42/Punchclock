d13e0b2d1823353361b998da6ea1783c982431f5
Sadjow Leão <sadjow@gmail.com>
2013-12-27 12:44:00 -0300

    Fix options url host on staging environment

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -81,7 +81,7 @@ Punchclock::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => "punchclock.cm42.io" }
+  config.action_mailer.default_url_options = { :host => "punchclock-staging.herokuapp.com" }
   config.action_mailer.perform_deliveries = true
 
   ActionMailer::Base.smtp_settings = {
