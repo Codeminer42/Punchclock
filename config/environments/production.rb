048cf4914912b26912c42ba9005801df0e0a8323
Sadjow Leão <sadjow@gmail.com>
2013-12-27 11:35:29 -0300

    Change default host on url options for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Punchclock::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => "punchlock.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => "punchclock.cm42.io" }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
