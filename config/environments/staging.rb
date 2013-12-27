c8fe04645a84f120071e0feabe03767af87d9c1c
Sadjow Leão <sadjow@gmail.com>
2013-12-27 12:35:35 -0300

    Configure to use Mailgun on staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -83,12 +83,13 @@ Punchclock::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => "punchclock.cm42.io" }
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
+
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'heroku.com',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com'
   }
 end
