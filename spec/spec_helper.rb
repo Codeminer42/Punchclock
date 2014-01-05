559811f620a5b0d085b07c86d8d9c6f1be51361a
Sadjow Leão <sadjow@gmail.com>
2014-01-05 17:45:56 -0300

    Add FactoryGirl::Syntax::Methods into spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,13 +9,20 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+
   config.include Devise::TestHelpers, type: :controller
   config.include Warden::Test::Helpers, type: :feature
   config.include RequestHelpers, type: :feature
+
   config.extend ControllerMacros, :type => :controller
+
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
   config.use_transactional_fixtures = true
+
   config.infer_base_class_for_anonymous_controllers = false
+
   config.order = "random"
 
   config.before(type: :request) do
