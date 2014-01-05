24c2a76ab9c307d47c8d984229310ff2dcf871d4
Sadjow Leão <sadjow@gmail.com>
2014-01-05 14:26:01 -0300

    Remove useless comments and variable from seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 unless Company.exists?(name: 'Codeminer42')
   puts 'Creating default company'
-  company = Company.create name: 'Codeminer42'
+  Company.create name: 'Codeminer42'
 end
 
 unless AdminUser.exists?(email: 'super@codeminer42.com')
