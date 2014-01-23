f4846ff7dcc561b6046491d938d050a1428a898b
Fernando Martínez <fernando.martinez@codeminer42.com>
2014-01-23 16:08:57 -0300

    refactor home controller: remove logout and redirect methods

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,6 @@
 class HomeController < ApplicationController
-  before_action :go_to_punches, :except => :logout
 
-  def index; end
-
-  def logout
-    reset_session
-    redirect_to action: :index
-  end
-
-private
-
-  def go_to_punches
-    redirect_to(punches_path) if user_signed_in?
+  def index
   end
 
 end
