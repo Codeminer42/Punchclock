fa3020b1a69285a9718b15fd265bfd2df675b94f
Dante <dante.alighierimds@gmail.com>
2014-02-26 11:44:06 -0300

    refactor User to not use total_hours of punches

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,22 +2,13 @@ class User < ActiveRecord::Base
   belongs_to :company
   devise :invitable, :database_authenticatable, :recoverable, :rememberable,
          :trackable, :validatable, :confirmable, :invitable
-  has_many :punches
+  has_many :punches 
   has_many :notifications
   validates :name, presence: true
   validates :email, uniqueness: true, presence: true
   validates :company, presence: true
 
   accepts_nested_attributes_for :company
-
-  def total_hours(result = nil)
-    total_punches = result.nil? ? self.punches : result
-    total_sum = 0.0
-    total_punches.each do |punch|
-      total_sum = total_sum + (punch.to - punch.from)
-    end
-    total_sum / 3600
-  end
 
   def self.find_for_googleapps_oauth(access_token, signed_in_resource=nil)
     data = access_token['info']
