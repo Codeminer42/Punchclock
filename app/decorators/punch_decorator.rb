11ff51194208c3f799a84fc86fd1429e0d085608
Dante <dante.alighierimds@gmail.com>
2014-02-26 11:46:21 -0300

    refactor code to use punch_decorator

--- app/decorators/punch_decorator.rb
+++ app/decorators/punch_decorator.rb
@@ -18,14 +18,5 @@ class PunchDecorator < ApplicationDecorator
   def localize_date_time(datetime)
     I18n.l datetime, format: :short
   end
-
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
-  #   def created_at
-  #     helpers.content_tag :span, class: 'time' do
-  #       object.created_at.strftime("%a %m/%d/%y")
-  #     end
-  #   end
-
+  
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,9 @@
 module ApplicationHelper
-  def time_format(hours)
-    hours *=  60
-    hours = hours.to_int
-    hour = (hours / 60).to_s
-    mins = (hours % 60)
-    mins = mins < 10 ? ('0' + mins.to_s) : mins.to_s
-    hour + ':' + mins
-  end
 
-  def gravatar_for(user, options = {size:50})
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    size = options[:size]
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
-  end
+	def gravatar_for(user, options = {size:50})
+	    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+	    size = options[:size]
+	    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+	    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+	end
 end
