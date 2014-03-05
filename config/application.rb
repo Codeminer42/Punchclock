15603134dc39aa22a795a56df9b7acd00ebc4d05
MildoJr <mildojr@mildojr.com>
2014-03-05 16:48:34 -0300

    Add gem rails-i18n and gem responders

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module Punchclock
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
     I18n.enforce_available_locales = false
+    config.i18n.locale = :'pt-BR'
   end
 end
