4e283a0279a8d57f2a89da336f5b4b368e356917
MildoJr <mildojr@mildojr.com>
2014-03-06 18:24:45 -0300

    Fix indentation

--- app/decorators/punches_decorator.rb
+++ app/decorators/punches_decorator.rb
@@ -2,6 +2,6 @@ class PunchesDecorator < Draper::CollectionDecorator
   delegate :reorder, :page, :current_page, :total_pages, :limit_value,
            :total_count, :num_pages, :to_key
   def total_hours
-  	h.secs_to_formated_hour(object.total)
+    h.secs_to_formated_hour(object.total)
   end
 end
