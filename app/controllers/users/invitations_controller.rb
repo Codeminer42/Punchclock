9f1146d936b3a31f8543891b478c386c2f027769
Halan Pinheiro <halan.pinheiro@codeminer42.com>
2014-04-23 20:53:19 -0300

    Little improvement on invitation controller.

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,20 +1,21 @@
 module Users
   class InvitationsController < Devise::InvitationsController
     def create
-      super
-      # skip validations
-      if resource.errors.blank?
-        resource.update_attribute(:name,
-                                  resource_params[:email].split('@').first)
-        resource.update_attribute(:is_admin, false)
-        resource.update_attribute(:company_id, current_user.company_id)
-      end
+      super { update_with_defaults }
     end
 
     protected
+    
+    def update_with_defaults
+      resource.update_attributes({
+        name: resource_params[:email][/(.*)@/, 1],
+        is_admin: false,
+        company_id: current_user.company_id
+      })
+    end
 
     def update_resource_params
-      allow = [:name, :password, :password_confirmation, :invitation_token]
+      allow = %w(name password password_confirmation invitation_token)
       params.require(:user).permit(allow)
     end
   end
