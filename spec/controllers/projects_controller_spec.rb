876c9cace8917eb24708b2ac2be6a03d24cc2ea5
Halan Pinheiro <halan.pinheiro@codeminer42.com>
2014-04-22 21:36:01 -0300

    Improve projects controller

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,30 +1,20 @@
 require 'spec_helper'
 
 describe ProjectsController do
-  login_user
+  before{ login user }
+  let(:company) { create :company }
+  let(:user) { create :user, is_admin: true, company: company }
+  let(:project) { create :project, company: company }
+
+
 
   describe 'POST create' do
-    let(:user) { double('user') }
-    let(:project) { double('project') }
-    let(:company) { FactoryGirl.build(:company) }
-
-    before do
-      user.stub(company: company)
-      controller.stub(current_user: user)
-    end
-
-    context 'when authorize pass' do
-      before { controller.stub(load_and_authorize_resource: true) }
-      before { controller.stub(authorize!: true) }
       it 'should create a project' do
         params = {
           when_day: '2013-08-20',
           project: { name: '1234' }
         }
 
-        Project.should_receive(:new).and_return(project)
-        project.should_receive(:company=).with(user.company)
-        project.should_receive(:save).and_return(true)
         post :create, params
         expect(response).to redirect_to projects_path
       end
@@ -35,35 +25,21 @@ describe ProjectsController do
           project: { name: '' }
         }
 
-        Project.should_receive(:new).and_return(project)
-        project.should_receive(:company=).with(user.company)
-        project.should_receive(:save).and_return(false)
         post :create, params
         expect(response).to render_template :new
       end
-    end
 
     context 'PUT update' do
-      let(:user) { double('user') }
-      let(:project) { FactoryGirl.create(:project) }
-
-      before do
-        Project.stub(:find).with(project.id.to_s) { project }
-      end
 
       context 'when authorize pass' do
-        before { controller.stub(load_and_authorize_resource: true) }
-        before { controller.stub(authorize!: true) }
-
         it 'should update a project' do
           params = {
             id: project.id,
             project: {
               name: '3214'
-          }
+            }
           }
 
-          expect(project).to receive(:update).and_return(true)
           put :update, params
           expect(response).to redirect_to projects_path
         end
@@ -73,10 +49,9 @@ describe ProjectsController do
             id: project.id,
             project: {
               name: ''
-          }
+           }
           }
 
-          expect(project).to receive(:update).and_return(false)
           put :update, params
           expect(response).to render_template :edit
         end
