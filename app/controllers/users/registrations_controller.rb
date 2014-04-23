a35406c2be9d32b87c2ac1c39edf4b2123a4e181
Halan Pinheiro <halan.pinheiro@codeminer42.com>
2014-04-23 14:19:19 -0300

    Remove useless code from registrations_controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,56 +1,27 @@
 module Users
   class RegistrationsController < Devise::RegistrationsController
-    def new
-      build_resource(company: Company.new)
-      respond_with resource
-    end
-
-    def create
-      build_resource(sign_up_params)
-      resource.is_admin = true
-      if resource.save
-        if resource.active_for_authentication? then signed_success
-        else signed_inactive_response
-        end
-      else
-        clean_up_passwords resource
-        render action: :new
-      end
-    end
-
     private
 
-    def signed_success # MOVEME to an responder
-      set_flash_message :notice, :signed_up if is_navigational_format?
-      sign_up(resource_name, resource)
-      respond_with resource, location: after_sign_up_path_for(resource)
-    end
-
-    def signed_inactive_response # MOVEME to an responder
-      set_flash_message(
-        :notice,
-        :"signed_up_but_#{resource.inactive_message}"
-      ) if is_navigational_format?
-
-      expire_session_data_after_sign_in!
-      respond_with(resource,
-                   location: after_inactive_sign_up_path_for(resource))
+    def build_resource user_params
+      super({
+        company: Company.new,
+        is_admin: true
+      }.merge user_params)
     end
 
     def sign_up_params
-      allow = [
+      params.require(:user).permit([
         :name,
         :email,
         :password,
         :password_confirmation,
         company_attributes: [:name]
-      ]
-      params.require(:user).permit(allow)
+      ])
     end
 
     def sign_up(resource_name, resource)
       NotificationMailer.notify_successful_signup(resource).deliver
-      sign_in(resource_name, resource)
+      super
     end
   end
 end
