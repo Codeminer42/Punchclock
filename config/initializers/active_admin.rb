ee024aec249bddc9cb901ed1f1b4f6339b3d0d8a
Halan Pinheiro <halan.pinheiro@codeminer42.com>
2014-04-28 15:12:14 -0300

    Fix i18n on production

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -1,3 +1,5 @@
+pre_defined_paths = Rails.application.config.i18n.load_path
+
 ActiveAdmin.setup do |config|
   config.site_title = 'Punchclock'
   config.authentication_method = :authenticate_admin_user!
@@ -7,3 +9,10 @@ ActiveAdmin.setup do |config|
   config.batch_actions = true
   config.csv_options = { col_sep: ';', force_quotes: true }
 end
+
+
+# Hotfix for https://github.com/gregbell/active_admin/issues/434
+I18n.enforce_available_locales = false
+I18n.default_locale = Rails.application.config.i18n.default_locale
+I18n.load_path += pre_defined_paths
+I18n.reload!
