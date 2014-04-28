dfaed1d732b165c50c94ed08dd3bcdd1d60bf93f
Halan Pinheiro <halan.pinheiro@codeminer42.com>
2014-04-28 14:46:06 -0300

    Fix i18n config

--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,7 @@ module Punchclock
     config.i18n.load_path += Dir[
       Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s
     ]
-    I18n.enforce_available_locales = false
-    config.i18n.available_locales = [:'pt-BR', :en]
+    config.i18n.enforce_available_locales = false
     config.i18n.locale = :'pt-BR'
   end
 end
