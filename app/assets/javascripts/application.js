9e7aa09f9147ac273ddae20ec9e22a28869707ab
Halan Pinheiro <halan.pinheiro@codeminer42.com>
2015-12-12 19:39:58 -0300

    Remove inline js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,3 +3,17 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require jquery_nested_form
+
+$(function() {
+  var menu = $('#navigation-menu');
+  var menuToggle = $('#js-mobile-menu');
+
+  $(menuToggle).on('click', function(e) {
+    e.preventDefault();
+    menu.slideToggle(function(){
+      if(menu.is(':hidden')) {
+        menu.removeAttr('style');
+      }
+    });
+  });
+});
