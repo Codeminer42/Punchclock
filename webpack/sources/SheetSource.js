8e971e37600b8698624f0a0e95c41793b53c2583
Halan Pinheiro <halan.pinheiro@codeminer42.com>
2016-01-02 19:57:10 -0300

    Fix CSRF of calendar

--- webpack/sources/SheetSource.js
+++ webpack/sources/SheetSource.js
@@ -17,11 +17,11 @@ export default {
 
   save: function() {
     const state = SheetStore.getState();
-    let _reqquest = request.post(url);
+    let _request = request.post(url);
 
     if(csrf.token) {
-      let _request = req.set('X-CSRF-Param', csrf.param.attributes.content.value)
-                        .set('X-CSRF-Token', csrf.token.attributes.content.value);
+      _request = _request.set('X-CSRF-Param', csrf.param.attributes.content.value)
+                         .set('X-CSRF-Token', csrf.token.attributes.content.value);
     }
 
     return _request.send({ delete: state.deleteds, add: state.sheets }).endAsync();
