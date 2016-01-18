6187dca129dc3bbf9973a51ea4c058bba37c75ea
Halan Pinheiro <halan.pinheiro@codeminer42.com>
2016-01-18 16:35:16 -0300

    Revert "Remove useless javascripts"
    This reverts commit 87a9ade7182067f7a4787faedc6518c4adc0d2d8.
    
    Conflicts:
    	app/assets/javascripts/application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,28 @@ $(function() {
       }
     });
   });
+
+
+  $(".file-field-import-csv").change(function() {
+      $(this).parents("form").submit();
+  });
+
+  $('input.datepicker').datepicker({ dateFormat: 'yy-mm-dd' });
+
 });
+
+function openNotificationCenter(){
+  document.getElementById("n-r-count").innerHTML = "Notifications <span class=\"caret\"/>";
+}
+
+function markAsRead(id){
+  document.getElementById("n-r-count").innerHTML = "Notifications <span class=\"caret\"/>";
+  var element = "n-rd-" + id;
+  document.getElementById(element).remove();
+  $.ajax({
+    type: 'PUT',
+    dataType: "json",
+    url: 'notification/' + id,
+    data: { notification: { read: true } }
+  })
+}
