e6bab471939446e91394d10d59601d1e3b5e4654
Danilo Resende <danilo.isr@gmail.com>
2016-07-13 10:11:49 -0300

    Use UTC when showing punches

--- webpack/components/Punch.jsx
+++ webpack/components/Punch.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import moment from 'moment';
 
 function format(timestamp) {
-  return moment(timestamp).format('HH:mm');
+  return moment.utc(timestamp).format('HH:mm');
 }
 
 export default ({punch}) =>
