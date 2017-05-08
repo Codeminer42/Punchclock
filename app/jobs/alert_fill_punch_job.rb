fd6b67cbdf8ff5fbf1863d6b9d7bfb6c8e8ee83c
Diogo Sonsim <diogo.sonsim@gmail.com>
2017-05-08 15:07:30 -0300

    add include sidekiq

--- app/jobs/alert_fill_punch_job.rb
+++ app/jobs/alert_fill_punch_job.rb
@@ -1,9 +1,14 @@
 class AlertFillPunchJob < ActiveJob::Base
+  include Sidekiq
   queue_as :default
 
   def perform
-    User.active.find_each do |user|
-      NotificationMailer.notify_user_to_fill_punch(user).deliver_now
+    Company.all.find_each do |company|
+      admins = company.users.active.is_admin
+
+      company.users.active.find_each do |user|
+        NotificationMailer.notify_user_to_fill_punch(user).deliver_now
+      end
     end
   end
 
