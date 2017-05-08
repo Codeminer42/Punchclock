fb2695cc5cfde7292dcb452b1899c613aaadb15b
Diogo Sonsim <diogo.sonsim@gmail.com>
2017-05-08 08:56:57 -0300

    implement Sidekiq

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,9 @@
+require 'sidekiq'
+
+Sidekiq.configure_server do |config|
+  config.redis = {url: 'redis://localhost:6379/0'}
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = {url: 'redis://localhost:6379/0'}
+end
