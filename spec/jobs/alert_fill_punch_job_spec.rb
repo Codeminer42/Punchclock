d86df342e93429cb3699dc6df08a6431ebd23fe8
Diogo Sonsim <diogo.sonsim@gmail.com>
2017-05-09 14:30:10 -0300

    Fixed test

--- spec/jobs/alert_fill_punch_job_spec.rb
+++ spec/jobs/alert_fill_punch_job_spec.rb
@@ -5,26 +5,31 @@ RSpec.describe AlertFillPunchJob, type: :job do
 
   describe '#perform' do
 
-  let!(:user) { create(:user, active: true) }
-  let!(:user2) { create(:user, active: false) }
-  let(:message_delivery) { instance_double(ActionMailer::MessageDelivery) }
+    let!(:active_user) { create(:user, active: true) }
+    let!(:inactive_user) { create(:user, active: false) }
+    let(:message_delivery) { instance_double(ActionMailer::MessageDelivery) }
 
-  before do
-    allow(message_delivery).to receive(:deliver_later)
+    before do
+      allow(NotificationMailer).to receive(:notify_user_to_fill_punch).and_return(message_delivery)
+      allow(message_delivery).to receive(:deliver_later)
+    end
+
+    subject(:job) { described_class.perform_later }
+
+    it 'is in default queue' do
+      expect(AlertFillPunchJob.new.queue_name).to eq('default')
+    end
+
+    it 'sends an email only to active users' do
+      expect(NotificationMailer).to receive(:notify_user_to_fill_punch).with(active_user)
+
+      perform_enqueued_jobs { job }
+    end
+
+    it 'do not sends an email to inactive users' do
+      expect(NotificationMailer).not_to receive(:notify_user_to_fill_punch).with(inactive_user)
+
+      perform_enqueued_jobs { job }
+    end
   end
-
-  subject(:job) { described_class.perform_later }
-
-  it 'is in default queue' do
-    expect(AlertFillPunchJob.new.queue_name).to eq('default')
-  end
-
-  it 'sends an email only to active users' do
-    expect(NotificationMailer).to receive(:notify_user_to_fill_punch).with(user).and_return(message_delivery)
-
-    expect(NotificationMailer).not_to receive(:notify_user_to_fill_punch).with(user2)
-
-    perform_enqueued_jobs { job }
-  end
-end
 end
