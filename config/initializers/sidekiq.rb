e713d1c29dbd517e468cbaaa77c90f2f95222f84
Diogo Sonsim <diogo.sonsim@gmail.com>
2017-05-09 12:48:21 -0300

    Add envvar

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 require 'sidekiq'
 
 Sidekiq.configure_server do |config|
-  config.redis = {url: 'redis://localhost:6379/0'}
+  config.redis = {url: ENV['REDIS_URL']}
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = {url: 'redis://localhost:6379/0'}
+  config.redis = {url: ENV['REDIS_URL']}
 end
