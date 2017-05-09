7e7b1f5dc3975b8f1ece9d6f79d29d7a6b2e097f
Diogo Sonsim <diogo.sonsim@gmail.com>
2017-05-09 13:48:58 -0300

    Create test to sends an email only to active users

--- spec/jobs/alert_fill_punch_job_spec.rb
+++ spec/jobs/alert_fill_punch_job_spec.rb
@@ -1,9 +1,16 @@
-require 'spec_helper'
-require 'sidekiq/testing'
-Sidekiq::Testing.fake!
+require 'rails_helper'
 
 RSpec.describe AlertFillPunchJob, type: :job do
- describe '.perform_later' do
+  include ActiveJob::TestHelper
+ describe '#perform' do
+
+   let!(:user) { create(:user, active: true) }
+   let!(:user2) { create(:user, active: false) }
+   let(:message_delivery) { instance_double(ActionMailer::MessageDelivery) }
+
+   before do
+     allow(message_delivery).to receive(:deliver_later)
+   end
 
     subject(:job) { described_class.perform_later }
 
@@ -11,6 +18,12 @@ RSpec.describe AlertFillPunchJob, type: :job do
       expect(AlertFillPunchJob.new.queue_name).to eq('default')
     end
 
-  end
+    it 'sends an email only to active users' do
+      expect(NotificationMailer).to receive(:notify_user_to_fill_punch).with(user).and_return(message_delivery)
 
+      expect(NotificationMailer).not_to receive(:notify_user_to_fill_punch).with(user2)
+
+      perform_enqueued_jobs { job }
+    end
+  end
 end
