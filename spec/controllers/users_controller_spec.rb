f623cc0a7af2ed0ceaebed4a90938d4263b4b9db
Lucas Monteiro <lucas.monteiro@codeminer42.com>
2017-06-13 10:17:26 -0300

    Remove decorator call form index action

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,11 +1,10 @@
 require 'spec_helper'
 
 describe UsersController do
+  let(:user) { create(:user) }
   before { login user }
 
   describe 'PUT update' do
-    let(:user) { create(:user) }
-
     context 'with valid informations' do
       let(:user_attributes)  { { name: '1234', email: '1234@1234.com', hour_cost: 15.0 } }
 
@@ -14,6 +13,16 @@ describe UsersController do
       it 'should update user' do
         expect(assigns(:user)).to have_attributes(user_attributes)
       end
+    end
+  end
+
+  describe 'GET index' do
+    before do
+      get :index
+    end
+
+    it "returns a success response status" do
+      expect(response.status).to eq(200)
     end
   end
 end
