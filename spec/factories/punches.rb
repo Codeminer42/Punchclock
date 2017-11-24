07b4c451dbcde426a71fa6e7641d353950f3ec77
alessandro <c_abatista@godaddy.com>
2017-11-24 09:56:01 -0200

    fix punch factory

--- spec/factories/punches.rb
+++ spec/factories/punches.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :punch do
-    sequence(:from) { |i| DateTime.new(2001, 1, 5, 8, i, 0, 0) }
-    sequence(:to) { |i| DateTime.new(2001, 1, 5, 17, i, 0, 0) }
+    sequence(:from) { |i| DateTime.new(2001, 1, 5, 8, 0, 0, 0) }
+    sequence(:to) { |i| DateTime.new(2001, 1, 5, 17, 0, 0, 0) }
     extra_hour '01:25'
     user
     project { FactoryGirl.create(:project, company: user.company) }
