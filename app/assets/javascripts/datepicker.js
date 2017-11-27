ae407505dd82b20d50d55766ab93af1af7b477f2
alessandro <c_abatista@godaddy.com>
2017-11-27 14:14:15 -0200

    fix punch create

--- app/assets/javascripts/datepicker.js
+++ app/assets/javascripts/datepicker.js
@@ -1,37 +1,39 @@
 
-const extractHolidays = function($el) {
-  return $el.data('nationalHolidays').concat($el.data('regionalHolidays'));
-}
+// DISABLE FOR NOW
+// const extractHolidays = function($el) {
+//   return $el.data('nationalHolidays').concat($el.data('regionalHolidays'));
+// }
 
-const toPairMonthDay = function(date) { return [date.getMonth() + 1, date.getDate()]; }
+// const toPairMonthDay = function(date) { return [date.getMonth() + 1, date.getDate()]; }
 
-const isSamePair = function(today) {
-  return function(holiday) {
-    return holiday[0] == today[0] &&
-      holiday[1] == today[1]
-  }
-}
+// const isSamePair = function(today) {
+//   return function(holiday) {
+//     return holiday[0] == today[0] &&
+//       holiday[1] == today[1]
+//   }
+// }
 
-function isHoliday(date) {
-  const allHolidays = extractHolidays($('.datepicker'));
-  const datePair = toPairMonthDay(date);
-  return !allHolidays.find(isSamePair(datePair));
-}
+// function isHoliday(date) {
+//   const allHolidays = extractHolidays($('.datepicker'));
+//   const datePair = toPairMonthDay(date);
+//   return !allHolidays.find(isSamePair(datePair));
+// }
 
-function noWeekendsOrHolidays(date) {
-  const noWeekend = $.datepicker.noWeekends(date);
-  if (noWeekend[0]) {
-    return [isHoliday(date)];
-  } else {
-    return noWeekend;
-  }
-}
+// function noWeekendsOrHolidays(date) {
+//   const noWeekend = $.datepicker.noWeekends(date);
+//   if (noWeekend[0]) {
+//     return [isHoliday(date)];
+//   } else {
+//     return noWeekend;
+//   }
+// }
 
 function setDatePicker() {
   const defaultOptions = { dateFormat: 'dd/mm/yy' };
-  const allowOvertime = $(".datepicker").data("allowOvertime");
-  const options = allowOvertime
-    ? defaultOptions
-    : { defaultOptions: defaultOptions, beforeShowDay: noWeekendsOrHolidays };
-  $('input.datepicker').datepicker(options);
+  // const allowOvertime = $(".datepicker").data("allowOvertime");
+  // const options = allowOvertime
+  //   ? defaultOptions
+  //   : { defaultOptions: defaultOptions, beforeShowDay: noWeekendsOrHolidays };
+
+  $('input.datepicker').datepicker(defaultOptions);
 }
