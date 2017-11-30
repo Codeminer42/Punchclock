525cef4c487581ed74836c1c1f85b9a704c2239e
alessandro <c_abatista@godaddy.com>
2017-11-30 12:49:07 -0200

    add sidekiq web monitor

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,8 @@
 require 'sidekiq'
+require 'sidekiq/web'
 
-Sidekiq.configure_server do |config|
-  config.redis = {url: ENV['REDIS_URL']}
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = {url: ENV['REDIS_URL']}
+if Rails.env.production?
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    username == ENV["SIDEKIQ_USERNAME"] && password == ENV["SIDEKIQ_PASSWORD"]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Punchclock::Application.routes.draw do
+  mount Sidekiq::Web => '/sidekiq'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
+
   ActiveAdmin.routes(self)
   devise_for :users
 
