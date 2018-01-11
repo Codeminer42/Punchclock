d94b8ecb8116d66eca738438197b01930d88b846
Cecatto <bruno.cecatto@codeminer42.com>
2018-01-11 14:12:09 -0200

    ci configure db:migrate

--- spec/factories/clients.rb
+++ spec/factories/clients.rb
@@ -1,7 +1,14 @@
 FactoryGirl.define do
   factory :client do
     name "MyString"
-company nil
+    company nil
   end
 
+  trait :active_client do
+    active true
+  end
+
+  trait :inactive_client do
+    active false
+  end
 end
--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Client, type: :model do
+
+  let!(:active_client) { FactoryGirl.create(:client, :active_client) }
+  let!(:inactive_client) { FactoryGirl.create(:client, :inactive_client) }
+
   describe 'relations' do
     it { is_expected.to belong_to :company }
   end
@@ -10,8 +14,19 @@ RSpec.describe Client, type: :model do
   end
 
   describe 'scopes' do
+    it 'is active' do
+      expect(active_client).to be_active
+    end
+
+    it 'is inactive' do
+      expect(inactive_client).to_not be_active
+    end
+  end
+
+  describe 'attribute active default' do
     it 'default active true' do
       expect(subject.active).to be true
     end
   end
+
 end
