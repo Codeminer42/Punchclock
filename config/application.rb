5b3a3fa15ae93bd0b7d7b5211ff2b37830b32112
Ricardo Eihara <ricardo.hejr@gmail.com>
2018-01-16 16:50:30 -0200

    Remove config transactional callbacks

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,6 @@ module Punchclock
     config.i18n.default_locale = :'pt-BR'
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
     config.active_job.queue_adapter = :sidekiq
   end
 end
