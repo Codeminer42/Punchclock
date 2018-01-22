f0004c44539e3608e94fff42fbbe07cd8c2e574a
Cecatto <bruno.cecatto@codeminer42.com>
2018-01-22 16:47:33 -0200

    static let to test working day

--- spec/lib/extra_hour_calculator_spec.rb
+++ spec/lib/extra_hour_calculator_spec.rb
@@ -4,25 +4,22 @@ RSpec.describe ExtraHourCalculator do
 
   describe '#call' do
 
-    let(:weeks_ago) { 2.day.ago.to_date + 1.day }
-    let(:starting_morning) { weeks_ago + 8.hours }
-    let(:end_of_the_morning) { weeks_ago + 12.hours }
-    let(:starting_afternoon) { weeks_ago + 13.hours }
-    let(:end_of_the_afternoon) { weeks_ago + 17.hours }
-
+    let(:weeks_ago) { Time.new(2018, 01, 17, 8, 0, 0, "+00:00")}
+    let(:starting_morning) { Time.new(2018, 01, 17, 8, 0, 0, "+00:00") }
+    let(:end_of_the_morning) {  Time.new(2018, 01, 17, 12, 0, 0, "+00:00") }
+    let(:starting_afternoon) {  Time.new(2018, 01, 17, 13, 0, 0, "+00:00") }
+    let(:end_of_the_afternoon) {  Time.new(2018, 01, 17, 17, 0, 0, "+00:00") }
+    let(:end_of_the_afternoon_with_extra_hour) {  Time.new(2018, 01, 17, 19, 0, 0, "+00:00") }
     let(:admin) { create :admin_user }
     let(:company) { admin.company }
-
     let(:active_user_with_hour) { create(:user, :active_user) }
     let(:active_user_without_hour) { create(:user, :active_user) }
-
     let(:worked_days) { [weeks_ago.strftime("%d/%m/%Y") ] }
 
     before do
-
-      create(:punch, from: starting_morning,          to: end_of_the_morning,             user: active_user_with_hour, company: company)
-                                                      #adding an extra hour
-      create(:punch, from: starting_afternoon,        to: end_of_the_afternoon + 1.hour,  user: active_user_with_hour, company: company)
+      create(:punch, from: starting_morning, to: end_of_the_morning, user: active_user_with_hour, company: company)
+      #adding an extra hour
+      create(:punch, from: starting_afternoon, to: end_of_the_afternoon_with_extra_hour,  user: active_user_with_hour, company: company)
       # next day
       create(:punch, from: starting_morning + 1.day,  to: end_of_the_morning + 1.day,     user: active_user_with_hour, company: company)
 
