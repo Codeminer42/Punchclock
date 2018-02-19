f701d42bc5ba03e863fa463ceb91b21d205f9bd1
alessandro <c_abatista@godaddy.com>
2018-02-19 09:59:05 -0300

    send punch alert only to codeminers

--- app/jobs/alert_fill_punch_job.rb
+++ app/jobs/alert_fill_punch_job.rb
@@ -2,11 +2,12 @@ class AlertFillPunchJob < ApplicationJob
   queue_as :default
 
   def perform
-    User.active.find_each do |user|
+    codeminer = Company.find_by name: "Codeminer42"
+    codeminer.users.active.find_each do |user|
       NotificationMailer.notify_user_to_fill_punch(user).deliver_later
     end
 
-    AdminUser.find_each do |admin|
+    codeminer.admin_users.find_each do |admin|
       NotificationMailer.notify_user_to_fill_punch(admin).deliver_later
     end
   end
