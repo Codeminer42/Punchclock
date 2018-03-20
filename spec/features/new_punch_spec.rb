1195588c338baa0de0793a6994c8d93ea4bd436e
ALEXANDRE KARPINSKI MANIKOWSKI <lakaravena@gmail.com>
2018-03-20 11:57:02 -0300

    create front-end tests for updating and creating punches with invalid data

--- spec/features/new_punch_spec.rb
+++ spec/features/new_punch_spec.rb
@@ -9,7 +9,7 @@ feature 'Add new Punch' do
     visit '/punches/new'
     expect(page).to have_content I18n.t(
       :creating, scope: %i(helpers actions), model: Punch.model_name.human
-    )
+      )
 
     within '#new_punch' do
       fill_in 'punch[from_time]', with: '08:00'
@@ -19,6 +19,22 @@ feature 'Add new Punch' do
       click_button 'Criar Punch'
     end
     expect(page).to have_content('Punch foi criado com sucesso.')
+  end
+
+  scenario 'creating invalid punch' do
+    visit '/punches/new'
+    expect(page).to have_content I18n.t(
+      :creating, scope: %i(helpers actions), model: Punch.model_name.human
+      )
+
+    within '#new_punch' do
+      fill_in 'punch[from_time]', with: '12:00'
+      fill_in 'punch[to_time]', with: '8:00'
+      fill_in 'punch[when_day]', with: '2001-01-05'
+      select active_project.name, from: 'punch[project_id]'
+      click_button 'Criar Punch'
+    end
+    expect(page).to have_content('Punch não pôde ser criado.')
   end
 
   scenario 'select box without inactive project' do
