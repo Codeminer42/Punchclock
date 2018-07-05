b8ba088e15915c0acf40920a40c28b43481fe0a0
Elias Rodrigues <elias.rodrigues@usp.br>
2018-07-05 16:33:57 -0300

    Adjust extra_hour in spec

--- spec/factories/punches.rb
+++ spec/factories/punches.rb
@@ -2,9 +2,12 @@ FactoryBot.define do
   factory :punch do
     sequence(:from) { |i| DateTime.new(2001, 1, 5, 8, 0, 0, 0) }
     sequence(:to) { |i| DateTime.new(2001, 1, 5, 17, 0, 0, 0) }
-    extra_hour '01:25'
     user
     project { FactoryBot.create(:project, company: user.company) }
     company { user.company }
+
+    trait :is_extra_hour do
+      extra_hour true
+    end
   end
 end
