c0ed5a2c4b33b37ed04ab76c4fd8455a7477a633
Elias Rodrigues <elias.rodrigues@usp.br>
2018-07-11 16:27:33 -0300

    Add some more tests punches_helper

--- spec/helpers/punches_helper_spec.rb
+++ spec/helpers/punches_helper_spec.rb
@@ -1,17 +1,17 @@
 require 'spec_helper'
 
 describe PunchesHelper do
-  context '#secs_to_formated_hour' do
-    it 'with parameter 1' do
-      result =  helper.secs_to_formated_hour(1)  
-
-      expect(result).to eq '00:00'
-    end
-
-    it 'with parameter 100' do
-      result =  helper.secs_to_formated_hour(100)  
-
-      expect(result).to eq '00:01' 
-    end
+  describe '#secs_to_formated_hour' do
+    it { expect(helper.secs_to_formated_hour(    1)).to eq '00:00' }
+    it { expect(helper.secs_to_formated_hour(   60)).to eq '00:01' }
+    it { expect(helper.secs_to_formated_hour(  100)).to eq '00:01' }
+    it { expect(helper.secs_to_formated_hour(  200)).to eq '00:03' }
+    it { expect(helper.secs_to_formated_hour(  310)).to eq '00:05' }
+    it { expect(helper.secs_to_formated_hour( 1000)).to eq '00:16' }
+    it { expect(helper.secs_to_formated_hour( 1040)).to eq '00:17' }
+    it { expect(helper.secs_to_formated_hour( 3600)).to eq '01:00' }
+    it { expect(helper.secs_to_formated_hour( 3659)).to eq '01:00' }
+    it { expect(helper.secs_to_formated_hour( 3660)).to eq '01:01' }
+    it { expect(helper.secs_to_formated_hour(25925)).to eq '07:12' }
   end
 end
