29bf9c6f03fc761a6bc911e7604e3c855a9dd9aa
lucasdeassis <lucasassis413@gmail.com>
2018-09-06 13:39:16 -0300

    improve password update failure case specs

--- spec/controllers/passwords_controller_spec.rb
+++ spec/controllers/passwords_controller_spec.rb
@@ -9,10 +9,10 @@ describe PasswordsController do
 
     let(:user) { create :user, password: current_password }
 
-    describe 'when user update your account' do
+    describe 'when user updates its account' do
       context 'with correct authorization' do
-        context 'with valid password' do
-          it 'update your password' do
+        context 'with valid password params' do
+          it 'updates user password' do
             params = {
               id: user.id,
               user: {
@@ -27,19 +27,95 @@ describe PasswordsController do
           end
         end
 
-        context 'with invalid password' do
-          it 'update your password' do
-            params = {
-              id: user.id,
-              user: {
-                current_password: current_password,
-                password: '',
-                password_confirmation: new_password
+        context 'with the invalid param of' do
+          context '"password"' do
+            it 'renders edit' do
+              params = {
+                id: user.id,
+                user: {
+                  current_password: current_password,
+                  password: '',
+                  password_confirmation: new_password
+                }
               }
-            }
 
-            patch :update, params: params
-            expect(response).to render_template(:edit)
+              patch :update, params: params
+              expect(response).to render_template(:edit)
+            end
+
+            it 'yields a "password cannot be empty" flash alert' do
+              params = {
+                id: user.id,
+                user: {
+                  current_password: current_password,
+                  password: '',
+                  password_confirmation: new_password
+                }
+              }
+
+              patch :update, params: params
+              expect(flash[:alert]).to have_content('Password não pode ficar em branco')
+            end
+          end
+
+          context '"current password"' do
+            it 'renders edit' do
+              params = {
+                id: user.id,
+                user: {
+                  current_password: 'pasword',
+                  password: new_password,
+                  password_confirmation: new_password
+                }
+              }
+
+              patch :update, params: params
+              expect(response).to render_template(:edit)
+            end
+
+            it 'yields a "invalid current password" flash alert' do
+              params = {
+                id: user.id,
+                user: {
+                  current_password: 'pasword',
+                  password: new_password,
+                  password_confirmation: new_password
+                }
+              }
+
+              patch :update, params: params
+              expect(flash[:alert]).to have_content('Current password não é válido')
+            end
+          end
+
+          context '"password confirmation"' do
+            it 'renders edit' do
+              params = {
+                id: user.id,
+                user: {
+                  current_password: current_password,
+                  password: new_password,
+                  password_confirmation: ''
+                }
+              }
+
+              patch :update, params: params
+              expect(response).to render_template(:edit)
+            end
+
+            it 'yields a "password confirmation mismatch" flash alert' do
+              params = {
+                id: user.id,
+                user: {
+                  current_password: current_password,
+                  password: new_password,
+                  password_confirmation: ''
+                }
+              }
+
+              patch :update, params: params
+              expect(flash[:alert]).to have_content('Password confirmation não é igual a Password')
+            end
           end
         end
       end
