334627989684ec1f7f00aeedb07fdf0789ec8c7c
Alessandro Dias <alessandro.dias@codeminer42.com>
2018-10-01 10:48:28 -0300

    enable es6 in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   config.public_file_server.enabled = false
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   config.assets.compile = false
   config.assets.digest = true
   config.assets.version = '1.0'
