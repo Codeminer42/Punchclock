c910156e1e61d7c176940ea67fe29c9cdc30ef37
Leonardo Negreiros <leonardo.negreiros@codeminer42.com>
2018-10-09 15:53:16 -0300

    Refactor of Capyabra config file
    The headless_chrome was moved to capybara.rb to get a cleaner code
    and also was added capybara server so it wont be needed
    to use the puma server as default.

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,1 +1,15 @@
 require 'capybara/rspec'
+
+Capybara.server = :webrick
+
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: {  args: %w[no-sandbox headless disable-gpu window-size=1280,1024] }
+  )
+
+  Capybara::Selenium::Driver.new app,
+    browser: :chrome,
+    desired_capabilities: capabilities
+end
+
+Capybara.javascript_driver = :headless_chrome
