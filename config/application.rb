7e2e5935589413a06125d0f99f3e8c8603aa1611
Leonardo Negreiros <leonardo.negreiros@codeminer42.com>
2018-10-09 15:41:59 -0300

    Upgrade to rails 5.2.1

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,16 @@
 require_relative 'boot'
+
 require 'csv'
+require "rails"
+# Pick the frameworks you want:
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+# require "action_cable/engine"
+require "sprockets/railtie"
 
-require 'active_record/railtie'
-require 'action_controller/railtie'
-require 'action_mailer/railtie'
-require 'sprockets/railtie'
-
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Punchclock
