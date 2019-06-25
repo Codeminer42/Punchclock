524960437d2c6c6ce09a4206fd349db99db94dc3
Eduardo <jeduardo824@gmail.com>
2019-06-25 17:55:55 -0300

    Add 'role' field to Users
    This commit adds the field 'role' to the admin page of Users to create and edit Users with the new field. It also includes additions on the User model to validate and on UserDecorator to 'humanize' the field. Some changes on the User spec and factory were made to test the new field.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -10,6 +10,7 @@ FactoryBot.define do
     github                { 'gitUser' }
     hour_cost             { 15.0 }
     contract_type         { 'employee' }
+    role                  { 'user' }
     association :office, factory: :office
     company
 
