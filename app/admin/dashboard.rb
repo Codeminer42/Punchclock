8cc7bdae76f10f4191fdbf2680f71288ce519a38
Cassiano Sampaio <cassianobsampaio@gmail.com>
2019-06-27 17:04:53 -0300

    Add select 2 Search by Office to admin dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -7,6 +7,7 @@ ActiveAdmin.register_page "Dashboard" do
   content title: proc{ I18n.t("active_admin.dashboard") } do
     panel t('search_fields', scope: 'active_admin'), class: 'search-fields' do
       current_company = current_admin_user.company
+
       users_collection = current_company.users.active.includes(:office).order(:name).map do |user| 
         user_label = "#{user.name.titleize} - #{user.email} - "
         user_label += "#{user.level.humanize} - " if user.engineer?
@@ -21,6 +22,7 @@ ActiveAdmin.register_page "Dashboard" do
           office.id
         ]
       end
+
 
       render "search_field", search_model: User, url_path: admin_users_path, collection: users_collection
       render "search_field", search_model: Office, url_path: admin_offices_path, collection: offices_collection
