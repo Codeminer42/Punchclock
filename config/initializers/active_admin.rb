b2492849894486415c68e259c7c98ed311ee81f5
Alessandro Dias <alessandrodbatista@gmail.com>
2019-06-29 14:37:34 -0300

    set dashboard as admin home

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,22 +4,16 @@ pre_defined_paths = Rails.application.config.i18n.load_path
 ActiveAdmin.setup do |config|
   config.site_title = proc {
     (!admin_user_signed_in? || current_admin_user.is_super?) ? "Punchclock" : "Punchclock (#{current_admin_user.company})"
-
   }
 
-  config.download_links = [:csv, :json]
+  config.download_links = [:csv]
   config.authentication_method = :authenticate_admin_user!
   config.current_user_method = :current_admin_user
   config.logout_link_path = :destroy_admin_user_session_path
   config.comments = false
   config.batch_actions = true
   config.csv_options = { col_sep: ';', force_quotes: true }
-  config.root_to = 'admin_users#index'
   config.authorization_adapter = ActiveAdmin::CanCanAdapter
-
-  # config.scope_to unless: -> { current_admin_user.is_super? } do
-  #   current_admin_user.company
-  # end
 
   # == Menu System
   config.namespace :admin do |admin|
@@ -37,39 +31,3 @@ I18n.enforce_available_locales = false
 I18n.default_locale = Rails.application.config.i18n.default_locale
 I18n.load_path += pre_defined_paths
 I18n.reload!
-
-# ActiveAdmin.setup do |config|
-  # config.site_title = "Miner Camp"
-  # config.authentication_method = :authenticate_miner!
-
-  # config.authorization_adapter = 'OnlyAdmins'
-
-  # config.on_unauthorized_access = :access_denied
-  # config.current_user_method = :current_miner
-
-  # config.logout_link_path = :destroy_miner_session_path
-
-  # config.localize_format = :long
-
-  # == Menu System
-  #
-  # config.namespace :admin do |admin|
-  #   admin.build_menu do |menu|
-  #     menu.add label: 'Miners' do |submenu|
-  #       submenu.add label: 'Available', url: '/admin/miners?scope=not_allocated'
-  #     end
-  #   end
-
-  #   admin.build_menu do |menu|
-  #     menu.add label: 'Customers' do |submenu|
-  #       submenu.add label: 'New Customer', url: '/admin/customers/new'
-  #     end
-  #   end
-
-  #   admin.build_menu :utility_navigation do |menu|
-  #     menu.add label: 'Evaluation Page', url: '/'
-  #     admin.add_current_user_to_menu  menu
-  #     admin.add_logout_button_to_menu menu
-  #   end
-  # end
-# end
