a758a75c0720d3659b86f589da014c91dc271f82
Gabriel Miranda <gabrielrumiranda@gmail.com>
2019-11-20 13:41:51 -0300

    Translate fields in dashboard page

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -36,10 +36,10 @@ ActiveAdmin.register_page "Dashboard" do
       render "search_field", search_model: Project, url_path: admin_projects_path, collection: projects_collection
     end
 
-    panel t('average_score', scope: 'active_admin'), class: 'average-score' do
+    panel t(I18n.t('average_score'), scope: 'active_admin'), class: 'average-score' do
       table_for User.level.values do
-        column(:level, &:humanize)
-        column(:users_average) { |level| User.with_level(level).overall_score_average }
+        column(I18n.t('level'), &:humanize)
+        column(I18n.t('users_average')) { |level| User.with_level(level).overall_score_average }
       end
     end
   end
