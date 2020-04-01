e4ceddec0c82fdfefabd23068e86eed578bfe05d
Fabio L Janiszevski <fabiosammy@gmail.com>
2020-04-01 10:42:55 -0300

    Add repositories create to and engineers users to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -42,6 +42,7 @@ def create_company(name:, office_cities:, project_names:, clients_name:)
       admin.password_confirmation = 'password'
       admin.role = :super_admin
       admin.company = company
+      admin.token = SecureRandom.base58(32)
       admin.skip_confirmation!
     end
 
@@ -52,6 +53,8 @@ def create_company(name:, office_cities:, project_names:, clients_name:)
       admin.password_confirmation = 'password'
       admin.role = :admin
       admin.company = company
+      admin.token = SecureRandom.base58(32)
+      admin.token = '9X9ti7nAeN3J2w9hn1om9ztpPMHrT7Mj' if name == 'Codeminer42'
       admin.skip_confirmation!
     end
 
@@ -63,10 +66,13 @@ def create_company(name:, office_cities:, project_names:, clients_name:)
       user = User.find_or_create_by!(email: "user.teste#{i}@#{name}.com") do |user|
         user.name = "Usuario_#{name}_#{i}"
         user.email = "user.teste#{i}@#{name}.com"
-        user.occupation = 'administrative'
+        user.occupation = :engineer
         user.password = 'password'
         user.company = company
         user.office = offices.sample
+        user.level = User.level.values.sample
+        user.specialty = User.specialty.values.sample
+        user.github = "#{name}.user.teste#{i}"
         user.skip_confirmation!
       end
 
@@ -77,9 +83,10 @@ def create_company(name:, office_cities:, project_names:, clients_name:)
       )
     end
   end
+  Company.find_by(name: name)
 end
 
-create_company(
+codeminer42 = create_company(
   name: 'Codeminer42',
   office_cities: [
     'Anápolis',
@@ -110,7 +117,133 @@ create_company(
   ]
 )
 
-create_company(name: 'WatersCo', office_cities: ['North Valerie', 'Lilachester'], project_names: ['Tres Zap', 'Latlux'], clients_name: ['Client1','Client2'])
+waters_co = create_company(
+  name: 'WatersCo',
+  office_cities: [
+    'North Valerie',
+    'Lilachester'
+  ],
+  project_names: [
+    'Tres Zap',
+    'Latlux'
+  ],
+  clients_name: [
+    'Client1',
+    'Client2'
+  ]
+)
 
+repositories = [
+  'http://github.com/flutter/flutter',
+  'http://github.com/facebook/create-react-app',
+  'http://github.com/ant-design/ant-design',
+  'http://github.com/trekhleb/javascript-algorithms',
+  'http://github.com/mui-org/material-ui',
+  'http://github.com/rails/rails',
+  'http://github.com/gatsbyjs/gatsby',
+  'http://github.com/impress/impress.js',
+  'http://github.com/jgthms/bulma',
+  'http://github.com/robbyrussell/oh-my-zsh',
+  'http://github.com/facebook/jest',
+  'http://github.com/iamkun/dayjs',
+  'http://github.com/plataformatec/devise',
+  'http://github.com/sequelize/sequelize',
+  'http://github.com/Marak/faker.js',
+  'http://github.com/tootsuite/mastodon',
+  'http://github.com/helm/charts',
+  'http://github.com/facebook/flow',
+  'http://github.com/jaredpalmer/formik',
+  'http://github.com/date-fns/date-fns',
+  'http://github.com/elixir-lang/elixir',
+  'http://github.com/BoostIO/Boostnote',
+  'http://github.com/tailwindcss/tailwindcss',
+  'http://github.com/tastejs/todomvc',
+  'http://github.com/diaspora/diaspora',
+  'http://github.com/thepracticaldev/dev.to',
+  'http://github.com/rubocop-hq/rubocop',
+  'http://github.com/activeadmin/activeadmin',
+  'http://github.com/sinatra/sinatra',
+  'http://github.com/stympy/faker',
+  'http://github.com/faker-ruby/faker',
+  'http://github.com/denysdovhan/spaceship-prompt',
+  'http://github.com/sherlock-project/sherlock',
+  'http://github.com/akiran/react-slick',
+  'http://github.com/expo/expo',
+  'http://github.com/segmentio/evergreen',
+  'http://github.com/moment/luxon',
+  'http://github.com/jquense/yup',
+  'http://github.com/reactjs/reactjs.org',
+  'http://github.com/vuejs/vuejs.org',
+  'http://github.com/Solido/awesome-flutter',
+  'http://github.com/plouc/nivo',
+  'http://github.com/svenfuchs/rails-i18n',
+  'http://github.com/pry/pry',
+  'http://github.com/grommet/grommet',
+  'http://github.com/rails/webpacker',
+  'http://github.com/react-navigation/react-navigation',
+  'http://github.com/callstack/react-native-paper',
+  'http://github.com/drapergem/draper',
+  'http://github.com/thoughtbot/administrate',
+  'http://github.com/vcr/vcr',
+  'http://github.com/fatfreecrm/fat_free_crm',
+  'http://github.com/ctrlpvim/ctrlp.vim',
+  'http://github.com/lancedikson/bowser',
+  'http://github.com/JeffreyWay/laravel-mix',
+  'http://github.com/awesome-print/awesome_print',
+  'http://github.com/mikel/mail',
+  'http://github.com/xotahal/react-native-material-ui',
+  'http://github.com/travisjeffery/timecop',
+  'http://github.com/thoughtbot/shoulda-matchers',
+  'http://github.com/react-native-community/react-native-blur',
+  'http://github.com/Rocketseat/unform',
+  'http://github.com/rubykube/peatio',
+  'http://github.com/lobsters/lobsters',
+  'http://github.com/teamcapybara/capybara',
+  'http://github.com/omarroth/invidious',
+  'http://github.com/igorescobar/jQuery-Mask-Plugin',
+  'http://github.com/rubygems/rubygems.org',
+  'http://github.com/welldone-software/why-did-you-render',
+  'http://github.com/cucumber/cucumber-ruby',
+  'http://github.com/ffaker/ffaker',
+  'http://github.com/chrismccord/render_sync',
+  'http://github.com/infinitered/gluegun',
+  'http://github.com/education/classroom',
+  'http://github.com/twilio/twilio-ruby',
+  'http://github.com/gocardless/statesman',
+  'http://github.com/sporkmonger/addressable',
+  'http://github.com/idyll-lang/idyll',
+  'http://github.com/plentz/lol_dba',
+  'http://github.com/codetriage/codetriage',
+  'http://github.com/mozilla-mobile/android-components',
+  'http://github.com/withspectrum/spectrum',
+  'http://github.com/mojombo/chronic',
+  'http://github.com/kotlintest/kotlintest',
+  'http://github.com/rspec/rspec-core',
+  'http://github.com/consul/consul',
+  'http://github.com/ifmeorg/ifme',
+  'http://github.com/publiclab/plots2',
+  'http://github.com/rails-sqlserver/activerecord-sqlserver-adapter',
+  'http://github.com/SimpleMobileTools/Simple-Gallery',
+  'http://github.com/react-native-community/cli',
+  'http://github.com/mozilla/multi-account-containers',
+  'http://github.com/openSUSE/osem',
+  'https://github.com/rubyforgood/diaper/',
+  'https://github.com/rubyforgood/voices-of-consent/',
+  'https://github.com/openfoodfoundation/openfoodnetwork',
+  'https://github.com/City-Bureau/city-scrapers',
+  'https://github.com/vuetifyjs/vuetify/',
+  'https://github.com/best-flutter/flutter_swiper',
+  'https://github.com/hasura/graphql-engine',
+  'https://github.com/mperham/sidekiq',
+  'https://github.com/nsarno/knock',
+  'https://github.com/ruby/ruby',
+  'https://github.com/ReactiveX/rxdart',
+  'https://github.com/jquense/yup',
+  'https://github.com/buefy/buefy',
+  'https://github.com/ruby-i18n/i18n'
+]
 
-
+repositories.each do |repository|
+  Repository.create! link: repository, company: codeminer42
+  Repository.create! link: repository, company: waters_co
+end
