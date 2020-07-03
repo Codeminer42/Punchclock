cf03d2a3e7184b8297b08e35de3032b6a7e0a0f0
Brunno Souza <brunno.souza@codeminer42.com>
2020-07-03 17:15:37 +0000

    revoke permission to Punch / User at role OpenSourceManager
    Fix problem err too many redirects at change user role

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
+
 pre_defined_paths = Rails.application.config.i18n.load_path
 
 ActiveAdmin.setup do |config|
   config.site_title = proc {
-    (!user_signed_in? || current_user.super_admin?) ? "Punchclock" : "Punchclock (#{current_user.company})"
+    !user_signed_in? || current_user.super_admin? ? 'Punchclock' : "Punchclock (#{current_user.company})"
   }
 
   config.download_links = [:csv]
@@ -14,17 +15,21 @@ ActiveAdmin.setup do |config|
   config.batch_actions = true
   config.csv_options = { col_sep: ';', force_quotes: true }
   config.authorization_adapter = ActiveAdmin::CanCanAdapter
+  config.cancan_ability_class = AbilityAdmin
+  config.on_unauthorized_access = :access_denied
 
   # == Menu System
   config.namespace :admin do |admin|
     admin.build_menu do |menu|
       menu.add label: User.model_name.human(count: 2) do |submenu|
-        submenu.add label: 'Não alocados', url: '/admin/users?scope=not_allocated', priority: 2
+        submenu.add label: 'Não alocados', 
+                    url: '/admin/users?scope=not_allocated', 
+                    priority: 2, 
+                    if: proc { current_user.is_admin? }
       end
     end
   end
 end
-
 
 # Hotfix for https://github.com/gregbell/active_admin/issues/434
 I18n.enforce_available_locales = false
