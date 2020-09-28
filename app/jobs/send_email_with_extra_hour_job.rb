984a38982d774f1fb891c2aedf759197d9be8ad6
Victor Gutierrez <victor.gutierrez@codeminer42.com>
2020-09-28 16:04:16 -0400

    Fix: fix indentation issue

--- app/jobs/send_email_with_extra_hour_job.rb
+++ app/jobs/send_email_with_extra_hour_job.rb
@@ -9,4 +9,5 @@ class SendEmailWithExtraHourJob queue_as :default
     .group_by(&:user_name).to_a
 
   NotificationMailer.notify_admin_extra_hour(extra_hour_punches, admins_emails).deliver_later unless extra_hour_punches.empty?
-end end
+  end
+end
