ca40177a97062a6a935fa9d957949468b9dbf2f9
Emerson Araki <emerson.araki@codeminer42.com>
2021-01-07 19:04:57 +0000

    Allow select holidays in Dashboard when user has overtime allowed

--- app/javascript/packs/components/Day.js
+++ app/javascript/packs/components/Day.js
@@ -2,21 +2,22 @@ import React from 'react';
 import cx from 'classnames/bind';
 import Punches from './Punches';
 import DayTitle from './DayTitle';
-import { isHoliday } from '../utils/calendar';
+import { isHoliday, overtimeAllowed } from '../utils/calendar';
 
 
 const Day = ({ day, selecteds, holidays, inner, onToggle, today, selected, sheet }) => {
   let holiday = () => isHoliday(selecteds, day, holidays)
+  let overtime = () => overtimeAllowed()
 
   let handleToggle = () => {
-    if(inner && !holiday()) {
+    if(inner && (!holiday() || overtime())) {
       onToggle(day, selecteds);
     }
   }
 
   let classNames = cx(
     `weekday-${day.day()}`,
-    `${(holiday()) ? ' weekday-holiday' : ''}`,
+    `${(holiday() && !overtime()) ? ' weekday-holiday' : ''}`,
     (inner ? 'inner' : 'out'),
     { today: today, selected: selected }
   );
