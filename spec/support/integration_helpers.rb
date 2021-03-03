e373596e67939956769493fe965e7ceb40047714
Isaias <isaias.carvalho@codeminer42.com>
2021-03-03 16:09:09 -0300

    Update integration helper

--- spec/support/integration_helpers.rb
+++ spec/support/integration_helpers.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
 module IntegrationHelpers
-  def sign_in(user)
+  def sign_in(user, otp: nil)
     visit root_path
 
     fill_in 'user_email', with: user.email
     fill_in 'user_password', with: user.password
+
+    if user.otp_required_for_login
+      fill_in 'user_otp_attempt', with: (otp || user.current_otp)
+    end
 
     click_button 'Login'
   end
