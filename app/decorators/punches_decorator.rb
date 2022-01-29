b9635d9b2e9148f91267b37ebc1d5ec01ef1401b
Alessandro Dias <alessandro.dias@codeminer42.com>
2022-01-29 17:48:57 -0300

    increate test coverage

--- app/decorators/punches_decorator.rb
+++ app/decorators/punches_decorator.rb
@@ -3,9 +3,10 @@
 class PunchesDecorator < Draper::CollectionDecorator
   delegate :reorder, :page, :current_page, :total_pages, :limit_value,
            :total_count, :num_pages, :to_key
+  include PunchesHelper
 
   def total_hours
-    h.secs_to_formated_hour(object.total)
+    secs_to_formated_hour(object.total)
   end
 
   def by_date
