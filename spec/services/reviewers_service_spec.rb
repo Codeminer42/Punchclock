327de571faffae6a9d2e112168227272b64d35d5
Heitor Cardozo <heitor711n@gmail.com>
2022-05-16 11:35:47 -0300

    Updated ReviwersService test

--- spec/services/reviewers_service_spec.rb
+++ spec/services/reviewers_service_spec.rb
@@ -9,24 +9,24 @@ RSpec.describe ReviewersService, type: :service do
     context 'when the attributes are valid' do
       it 'updates reviewed_by attributes' do
         ReviewersService.save_review(contribution.id, contribution.user)
-        expect { contribution.reload.reviewed_by }.to change {
-                                                        contribution.reviewed_by
-                                                      }.from(nil).to(contribution.user.name)
+        expect { contribution.reload.reviewed_by_id }.to change {
+                                                           contribution.reviewed_by_id
+                                                         }.from(nil).to(contribution.user.id)
       end
 
       it 'updates reviewed_at attributes' do
-        mock_time = Time.current.round(6)
-        allow(Time).to receive(:current).and_return(mock_time)
-
-        ReviewersService.save_review(contribution.id, contribution.user)
-        contribution.reload
-        expect(contribution.reviewed_at).to eq(mock_time)
+        mock_time = Time.current.round
+        travel_to(mock_time) do
+          ReviewersService.save_review(contribution.id, contribution.user)
+          contribution.reload
+          expect(contribution.reviewed_at).to eq(mock_time)
+        end
       end
     end
 
     context 'when the attributes are invalid' do
       it 'raises an error' do
-        expect { ReviewersService.save_review(nil, contribution.user) }.to raise_error(ActiveRecord::RecordNotFound)
+        expect { ReviewersService.save_review(nil, nil) }.to raise_error(ActiveRecord::RecordNotFound)
       end
     end
   end
