3917fa3981fe61e61b2f68b448a250887354982b
Heitor Cardozo <heitor711n@gmail.com>
2022-05-16 11:33:47 -0300

    Updated reviewed_by migration

--- db/migrate/20220503230849_add_reviewed_by_at_and_by_to_contributions.rb
+++ db/migrate/20220503230849_add_reviewed_by_at_and_by_to_contributions.rb
@@ -1,6 +1,6 @@
 class AddReviewedByAtAndByToContributions < ActiveRecord::Migration[6.1]
   def change
-    add_reference :contributions, :reviewed_by, references: :users, index: true
-    add_column :contributions, :reviewed_at, :datetime
+     add_reference :contributions, :reviewed_by, references: :users, index: true
+     add_column :contributions, :reviewed_at, :datetime
   end
 end
