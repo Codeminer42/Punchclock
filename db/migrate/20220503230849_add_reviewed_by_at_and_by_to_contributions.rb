5592564a1c7e21b8c0007840fca6d50d345f9d99
Heitor Cardozo <heitor711n@gmail.com>
2022-05-17 16:53:44 -0300

    updated column name

--- db/migrate/20220503230849_add_reviewed_by_at_and_by_to_contributions.rb
+++ db/migrate/20220503230849_add_reviewed_by_at_and_by_to_contributions.rb
@@ -1,6 +1,6 @@
 class AddReviewedByAtAndByToContributions < ActiveRecord::Migration[6.1]
   def change
-     add_reference :contributions, :reviewed_by, references: :users, index: true
+     add_reference :contributions, :reviewer, references: :users, index: true
      add_column :contributions, :reviewed_at, :datetime
   end
 end
