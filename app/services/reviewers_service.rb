69976d19442c7231a10df312b8bb88202463dbaa
Heitor Cardozo <heitor711n@gmail.com>
2022-05-17 16:54:35 -0300

    Updated service name

--- app/services/reviewers_service.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class ReviewersService
-  def self.save_review(contribution_id, user)
-    contribution = Contribution.find(contribution_id)
-    contribution.update(reviewed_by_id: user.id, reviewed_at: Time.current)
-  end
-end
