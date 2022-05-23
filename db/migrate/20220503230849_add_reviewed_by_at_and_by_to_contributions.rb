85618b30ea9bc7870b15e55c93dcec7e64d2f411
Wender Freese <wender.jean@gmail.com>
2022-05-23 10:56:49 -0300

    Update db/migrate/20220503230849_add_reviewed_by_at_and_by_to_contributions.rb

--- db/migrate/20220503230849_add_reviewed_by_at_and_by_to_contributions.rb
+++ db/migrate/20220503230849_add_reviewed_by_at_and_by_to_contributions.rb
@@ -1,5 +1,8 @@
 class AddReviewedByAtAndByToContributions < ActiveRecord::Migration[6.1]
   def change
+    add_reference :contributions, :reviewer, references: :users, index: true
+    add_column :contributions, :reviewed_at, :datetime
+  end
      add_reference :contributions, :reviewer, references: :users, index: true
      add_column :contributions, :reviewed_at, :datetime
   end
