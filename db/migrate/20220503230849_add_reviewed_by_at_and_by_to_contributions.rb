8fd520486230694543123df48cb870dd2dd2100b
Heitor Cardozo <heitor711n@gmail.com>
2022-05-23 11:13:49 -0300

    Small lint on migration

--- db/migrate/20220503230849_add_reviewed_by_at_and_by_to_contributions.rb
+++ db/migrate/20220503230849_add_reviewed_by_at_and_by_to_contributions.rb
@@ -3,7 +3,4 @@ class AddReviewedByAtAndByToContributions < ActiveRecord::Migration[6.1]
     add_reference :contributions, :reviewer, references: :users, index: true
     add_column :contributions, :reviewed_at, :datetime
   end
-     add_reference :contributions, :reviewer, references: :users, index: true
-     add_column :contributions, :reviewed_at, :datetime
-  end
 end
