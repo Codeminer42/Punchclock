8c7dbe085ce4fadff3d5434d94cf0fd31c02dab7
Guilherme Monteiro <dkglima@gmail.com>
2022-06-07 15:47:54 -0300

    feat: add show action endpoint for punch api

--- app/controllers/api/v1/punches_controller.rb
+++ app/controllers/api/v1/punches_controller.rb
@@ -5,10 +5,18 @@ module Api
         render json: scoped_punches, status: :ok, each_serializer: PunchSerializer
       end
 
+      def show
+        render json: scoped_punch, status: :found, each_serializer: PunchSerializer
+      end
+
       private
 
       def scoped_punches
         current_user.punches
+      end
+
+      def scoped_punch
+        scoped_punches.find(params[:id])
       end
     end
   end
