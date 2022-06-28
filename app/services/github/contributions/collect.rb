b8a67fa5bb971563621adcaabf150701ea4a6383
geeksilva97 <edigleyssonsilva@gmail.com>
2022-06-28 00:38:53 -0300

    improve method organization

--- app/services/github/contributions/collect.rb
+++ app/services/github/contributions/collect.rb
@@ -8,50 +8,45 @@ module Github
       def initialize(company:, client:)
         @company = company
         @client = client
-        @engineers_wrapper = EngineersWrapper.new(company: company)
-        @repositories_wrapper = RepositoriesWrapper.new(company: company)
       end
 
       def all
-        return [] if company.blank? or repositories.empty? or engineers.empty?
+        return [] if company.blank? or repositories_wrapper.repositories.empty? or engineers_wrapper.engineers.empty?
 
         yesterday_date = 1.day.ago.strftime("%Y-%m-%d")
-
         query = "created:#{yesterday_date} is:pr #{authors_query} #{repositories_query}"
 
-        client.search.issues(q: query).items.map do | pull_request |
-          repository_name = pull_request.html_url.split("/")[-4..-3].join("/")
-          user_id = @engineers_wrapper.get_engineer_id_by_github_user(pull_request.user.login)
-          repository_id = @repositories_wrapper.get_repository_id_by_name(repository_name)
-
-          Result.new(user_id, repository_id, pull_request.html_url, pull_request.created_at)
-        end
+        fetch_pull_requests(search_query: query)
       end
 
       private
 
       attr_reader :company, :client
 
+      def fetch_pull_requests(search_query:)
+        client.search.issues(q: search_query).items.map do | pull_request |
+          repository_name = pull_request.html_url.split("/")[-4..-3].join("/")
+          user_id = engineers_wrapper.get_engineer_id_by_github_user(pull_request.user.login)
+          repository_id = repositories_wrapper.get_repository_id_by_name(repository_name)
+
+          Result.new(user_id, repository_id, pull_request.html_url, pull_request.created_at)
+        end
+      end
+
+      def engineers_wrapper
+        @engineers_wrapper ||= EngineersWrapper.new(company: company)
+      end
+
+      def repositories_wrapper
+        @repositories_wrapper ||= RepositoriesWrapper.new(company: company)
+      end
+
       def repositories_query
-        @repositories_wrapper.to_query
+        repositories_wrapper.to_query
       end
 
       def authors_query
-        @engineers_wrapper.to_query
-      end
-
-      def engineers
-        @engineers ||= company.users
-                              .engineer
-                              .active
-                              .pluck(:id, :github)
-      end
-
-      def repositories
-        @repositories ||= company.repositories
-                                 .pluck(:id, :link)
-                                 .map { |id, url| [id, url.split('/')[-2..-1]].flatten }
-                                 .compact
+        engineers_wrapper.to_query
       end
     end
   end
