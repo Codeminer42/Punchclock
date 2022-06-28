d55be50dc54aa77077ae440d66bbc3dd075c6331
geeksilva97 <edigleyssonsilva@gmail.com>
2022-06-28 00:29:15 -0300

    add repositories wrapper

---
+++ app/services/github/contributions/repositories_wrapper.rb
@@ -0,0 +1,36 @@
+module Github
+  module Contributions
+    class RepositoriesWrapper
+      def initialize(company:)
+        @company = company
+      end
+
+      def get_repository_id_by_name(repository_name)
+        to_h[repository_name]
+      end
+
+      def to_query
+        repositories.map do |repository_owner, repository_name, repository_id|
+          "repo:#{repository_owner}/#{repository_name}"
+        end.join(' ')
+      end
+
+      private
+
+      attr_reader :company
+
+      def repositories
+        @repositories ||= company.repositories
+                                 .pluck(:id, :link)
+                                 .map { |id, url| [url.split('/')[-2..-1], id].flatten }
+                                 .compact
+      end
+
+      def to_h
+        @repositories_map ||= repositories.map do |repository_owner, repository_name, repository_id|
+          ["#{repository_owner}/#{repository_name}", repository_id]
+        end.to_h
+      end
+    end
+  end
+end
