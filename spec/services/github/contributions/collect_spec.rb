eb3ad1426a9bc411f9871a0bec4e84451b6f47dd
geeksilva97 <edigleyssonsilva@gmail.com>
2022-06-28 02:23:19 -0300

    add spec for when the pull requests are fecthed

--- spec/services/github/contributions/collect_spec.rb
+++ spec/services/github/contributions/collect_spec.rb
@@ -51,5 +51,22 @@ RSpec.describe Github::Contributions::Collect, type: :service do
 
       it { expect { subject }.to raise_error  }
     end
+
+    context 'when Pull Requests are fetched from GitHub API' do
+      let(:company) { build_stubbed(:company) }      
+      let(:repositories) { double(pluck: [[1, 'http://example.com']]) }
+      let(:engineers) { double(active: double(pluck: [['github_user', 1]])) }
+      let(:users) { double(engineer: engineers) }
+      let(:pull_requests) { double(items: [ double(created_at: '2022-01-01', html_url: 'http://example.com/owner/name/pull/123', user: double(login: 'github_user')) ]) }
+      let(:client_search) { double(issues: pull_requests) }
+
+      before do
+        allow(company).to receive(:repositories).and_return(repositories)
+        allow(company).to receive(:users).and_return(users)
+        allow(client).to receive(:search).and_return(client_search)
+      end
+
+      it { is_expected.not_to be_empty }
+    end
   end
 end
