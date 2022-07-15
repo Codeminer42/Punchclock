3fed318381959624c1ec41bd372b74c6381a8dae
Iago Dahlem Lorensini <iagodahlemlorensini@gmail.com>
2022-07-15 10:50:56 -0300

    Setup TailwindCSS

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,4 @@
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
 //= link application.css
+//= link_tree ../builds
---
+++ config/tailwind.config.js
@@ -0,0 +1,23 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: [
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim}",
+    "./app/assets/images/**/*.{png,jpg,jpeg}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Lato", "sans-serif", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+  ],
+};
