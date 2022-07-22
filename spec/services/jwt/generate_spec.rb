0bf3e837b817a159d7c9e117efb18ccbff901dac
André Moreira <andre.moreira@codeminer42.com>
2022-07-22 13:00:15 -0300

    WIP fix generate_spec

--- spec/services/jwt/generate_spec.rb
+++ spec/services/jwt/generate_spec.rb
@@ -1,52 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe Jwt::Generate do
-  before do
-    allow(SecureRandom).to receive(:uuid).and_return('valid_uuid')
-    @expected_token = "eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NDE2MTA4MDAsInN1YiI6MSwiZW52IjoidGVzdCIsImp0aSI6InZhbGlkX3V1aWQifQ.yOFK_juAoBONyGYbf9f1PZ5zXcNtTiDptCEMwxcmOGA"
-  end
-
   context '.initialize' do
     #mostrar que ele sempre gera um jti diferente
     subject { described_class.new(user_id: 1) }
 
     it 'has correct payload' do
-      expect(subject.payload).to include('exp', 'sub', 'env', 'jti')
+      expect(subject.payload).to include(:exp, :sub, :env, :jti)
+    end
+
+    it 'aways returns different tokens' do
+      expect(subject.token).not_to eq(described_class.new(user_id: 1).token)
     end
 
     it 'returns correct parse to string' do
-      expect(subject.to_s).to eq(@expected_token)
+      expect(subject.to_s).to eq(subject.token)
     end
 
     it 'returns correct parse to hash' do
-      expect(subject.to_h).to eq({ access_token: @expected_token })
+      expect(subject.to_h).to eq({ access_token: subject.token })
     end
 
     it 'returns correct parse to json' do
-      expect(subject.to_json).to eq({ access_token: @expected_token }.to_json)
+      expect(subject.to_json).to eq({ access_token: subject.token }.to_json)
     end
   end
 
   describe '.token' do
-    subject { described_class.new(user_id: 1).token }
-
-    it 'calls JWT::encode' do
-      #exprect encode to be called
-    end
-
+    subject { class_instance.token }
+    let(:class_instance) { described_class.new(user_id: 1) }
+    let(:payload) { class_instance.payload }
     let(:decode_token) { JWT.decode(subject, ENV['JWT_SECRET_KEY']) }
+    
+    it 'calls JWT::encode' do
+      allow(JWT).to receive(:encode).and_return('expected_token')
+      expect(subject).to eq('expected_token')
+      expect(JWT).to have_received(:encode).with(payload, ENV['JWT_SECRET_KEY'])
+    end
 
     it 'returns a valid token' do
       expect { decode_token }.not_to raise_error(JWT::DecodeError)
     end
 
     it 'contains correct data' do
-      decoded_token = decode_token
-      expect(decode_token).to eq([{'env'=>Rails.env,
-                                   'exp'=>1.week.from_now.to_i,
-                                   'jti'=>'valid_uuid',
-                                   'sub'=>1},
-                                   {'alg'=>'HS256'}])
+      expect(decode_token[0].symbolize_keys!).to eq(payload)
     end
   end
 end
