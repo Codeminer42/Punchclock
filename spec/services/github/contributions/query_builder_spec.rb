1633fe1a74483dc44fadeee2286ca55ad035b273
geeksilva97 <edigleyssonsilva@gmail.com>
2022-07-30 15:44:06 -0300

    fix query builder spec

--- spec/services/github/contributions/query_builder_spec.rb
+++ spec/services/github/contributions/query_builder_spec.rb
@@ -5,24 +5,28 @@ RSpec.describe Github::Contributions::QueryBuilder do
 
   describe '#build_query_string' do
     subject { described_class.build_query_string(authors_query, repositories_query) } 
-
+    let(:time_now) { Time.rfc3339('2022-06-06T00:00:00-03:00') }
     let(:authors_query) { 'author:user1 author:user2' }
     let(:repositories_query) { 'repo:owner1/name1 repo:owner2/name2' }
 
-    context 'when the DAYS_TO_SEARCH_CONTRIBUTIONS env var is not set' do
-      let!(:date_to_search) { 1.day.ago.strftime('%Y-%m-%d') }
+    before do
+      allow(Time).to receive(:now).and_return(time_now)
+    end
 
-      it { is_expected.to eq "created:#{date_to_search} is:pr author:user1 author:user2 repo:owner1/name1 repo:owner2/name2" }
+    context 'when the DAYS_TO_SEARCH_CONTRIBUTIONS env var is not set' do
+      before do
+        ENV['DAYS_TO_SEARCH_CONTRIBUTIONS'] = nil
+      end
+
+      it { is_expected.to eq "created:2022-06-05 is:pr author:user1 author:user2 repo:owner1/name1 repo:owner2/name2" }
     end
 
     context 'when the DAYS_TO_SEARCH_CONTRIBUTIONS env var is set' do
-      let!(:date_to_search) { 2.day.ago.strftime('%Y-%m-%d') }
-
       before do
         ENV['DAYS_TO_SEARCH_CONTRIBUTIONS'] = 2.to_s
       end
 
-      it { is_expected.to eq "created:#{date_to_search} is:pr author:user1 author:user2 repo:owner1/name1 repo:owner2/name2" }
+      it { is_expected.to eq "created:2022-06-04 is:pr author:user1 author:user2 repo:owner1/name1 repo:owner2/name2" }
     end
   end
 end
