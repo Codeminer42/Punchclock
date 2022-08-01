87c077075eba3fda2e4e91fb6684645b8b432447
Edigleysson Silva <edigleyssonsilva@gmail.com>
2022-08-01 09:19:58 -0300

    Refactor contributions collecting (#122)
    * use Search API to collect contributions
    
    * refactor methods
    
    * add wrapper for engineers
    
    * add repositories wrapper
    
    * improve method organization
    
    * add new context to github collection spec
    
    * add spec for when the pull requests are fecthed
    
    * add engineers wrapper spec
    
    * add repositories wrapper spec
    
    * refactor class interface
    
    * update engineer wrapper spec
    
    * update repositories wrapper spec
    
    * refactor method visibility
    
    * refactor collect class
    
    * refactor collect
    
    * remove unused code
    
    * improve readability
    
    * improve collection service readability
    
    * add query builder
    
    * add query builder to the collection service
    
    * initialize wrapper in the contructor
    
    * move wrappers to a specific module
    
    * add pull request wrapper
    
    * use pull request wrapper instead of struct
    
    * remove unused code
    
    * add strip to avoid extra spaces in query mounting
    
    * fix specs
    
    * fix query builder spec
    
    * add pull request wrapper spec

---
+++ spec/services/github/contributions/wrappers/repositories_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+RSpec.describe Github::Contributions::Wrappers::Repositories, type: :service do
+
+  let(:company) { build_stubbed(:company) }
+
+  before do
+    allow(company).to receive(:repositories).and_return(repositories)
+  end
+
+  describe '#empty?' do
+    subject(:empty) { described_class.new(company: company).empty? }
+
+    context 'when there are no repositories' do
+      let(:repositories) { double(pluck: []) }
+
+      it { is_expected.to be true }
+    end
+
+    context 'when there are repositories' do
+      let(:repositories) { double(pluck: [[1, 'https://github.com/owner1/name1'], [2, 'https://github.com/owner2/name2']]) }
+
+      it { is_expected.to be false }
+    end
+  end
+
+  describe '#to_query' do
+    subject(:to_query) { described_class.new(company: company).to_query }
+
+    context 'when there are no repositories' do
+      let(:repositories) { double(pluck: []) }
+
+      it { is_expected.to be_empty }
+    end
+
+    context 'when there are repositories' do
+      let(:repositories) { double(pluck: [[1, 'https://github.com/owner1/name1'], [2, 'https://github.com/owner2/name2']]) }
+
+      it { is_expected.to eq 'repo:owner1/name1 repo:owner2/name2'}
+    end
+  end
+
+  describe '#find_repository_id_by_name' do
+    subject(:find_repository_id_by_name) { described_class.new(company: company).find_repository_id_by_name(repository_to_find) }
+    let(:repository_to_find) { 'owner3/name3' }
+
+    context 'when the id is not found' do
+      let(:repositories) { double(pluck: [[1, 'https://github.com/owner1/name1'], [2, 'https://github.com/owner2/name2']]) }
+
+      it { is_expected.to be_nil }
+    end
+
+    context 'when the user is found' do
+      let(:repositories) { double(pluck: [[100, 'https://github.com/owner3/name3'], [2, 'https://github.com/owner2/name2']]) }
+
+      it { is_expected.to eq 100  }
+    end
+  end
+end
