1225fbd2bbdace67fa925d04c8feeb81e4768af5
mateuscelio <79609888+mateuscelio@users.noreply.github.com>
2022-08-18 15:28:29 -0300

    Highlight allocation chart rows based on the end date (#158)
    * Highlights user allocation status based in its end date
    
    * Removes extra tabs
    
    * Refacts helper
    
    * Refacts allocation_chart

--- app/admin/allocation_chart.rb
+++ app/admin/allocation_chart.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+include ActiveAdmin::AllocationChartHelper
+
 ActiveAdmin.register_page 'Allocation Chart' do
   menu label: proc { I18n.t('allocation_chart') }, parent: User.model_name.human(count: 2)
 
@@ -13,9 +15,39 @@ ActiveAdmin.register_page 'Allocation Chart' do
           column(I18n.t('level')) { |allocation| allocation.user.level.humanize }
           column(I18n.t('specialty')) { |allocation| allocation.user.specialty.humanize }
           column(I18n.t('client'), :project)
-          column(I18n.t('allocated_until'), :end_at)
+          column(I18n.t('allocated_until'), class: 'allocated-column') do |allocation|
+            build_allocation_status_cell(allocation)
+          end
         end
       end
     end
+  end
+end
+
+def build_allocation_status_cell(last_allocation)
+  allocation_status = allocation_status_for(last_allocation: last_allocation)
+
+  column_cell_base(
+    content: allocation_status_content(
+      last_allocation: last_allocation,
+      allocation_status: allocation_status
+    ),
+    allocation_status: allocation_status
+  )
+end
+
+def allocation_status_content(last_allocation:, allocation_status:)
+  return last_allocation.end_at if last_allocation.end_at
+
+  unallocated?(allocation_status) ? I18n.t('not_allocated') : I18n.t('allocated')
+end
+
+def unallocated?(allocation_status)
+  allocation_status == Status::NOT_ALLOCATED
+end
+
+def column_cell_base(content:, allocation_status:)
+  div class: "allocated-column__status  allocated-column__status--#{allocation_status}" do
+    content
   end
 end
