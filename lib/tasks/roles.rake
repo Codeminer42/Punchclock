7ed671bad007ce99dacdf877889204b4b7b08228
mateuscelio <mateuscelio10@gmail.com>
2022-09-02 11:19:22 -0300

    Updates task

--- lib/tasks/roles.rake
+++ lib/tasks/roles.rake
@@ -1,19 +1,9 @@
 namespace :roles do
-  desc 'Creates default roles'
-  task create_default_roles: :environment do
-    ActiveRecord::Base.transaction do
-      Role::DEFAULT_ROLES.each do |role|
-        Role.find_or_create_by(name: role)
-      end
-    end
-  end
-
   desc 'Migrate from single role'
   task migrate_from_single_role: :environment do
     ActiveRecord::Base.transaction do
       User.all.each do |user|
-        role = Role.find_by!(name: user.role)
-        user.roles << role unless user.roles.include? role
+        user.update(roles: [user.role])
       end
     end
   end
--- spec/lib/tasks/roles_spec.rb
+++ spec/lib/tasks/roles_spec.rb
@@ -3,45 +3,19 @@ require 'rails_helper'
 describe 'roles' do
   Rails.application.load_tasks
 
-  describe ':create_default_roles' do
-    subject { Rake::Task["roles:create_default_roles"].execute }
-
-    it 'creates default roles' do
-      expect { subject }.to change(Role, :count).from(0).to(6)
-      expect(Role.pluck(:name)).to eq(
-        %w[normal evaluator admin super_admin open_source_manager hr]
-      )
-    end
-
-    context 'when roles already exist' do
-      before { subject }
-      it 'does not create' do
-        expect { subject }.not_to change(Role, :count)
-      end
-    end
-  end
   describe ':migrate_from_single_role' do
     subject { Rake::Task["roles:migrate_from_single_role"].execute }
-    let!(:normal_role) { create(:role, name: 'normal') }
-    let!(:normal_user) { create(:user, role: 'normal') }
-    let!(:super_admin_role) { create(:role, name: 'super_admin') }
-    let!(:super_admin_user) { create(:user, role: 'super_admin') }
+    let!(:normal_user) { build(:user, role: 'normal', roles: []).tap { |u| u.save(validate: false) } }
+    let!(:super_admin_user) { build(:user, role: 'super_admin', roles: []).tap { |u| u.save(validate: false) } }
 
-    it { expect { subject }.to change { normal_user.reload.roles }.from([]).to([normal_role]) }
-    it { expect { subject }.to change { super_admin_user.reload.roles }.from([]).to([super_admin_role]) }
+    it { expect { subject }.to change { normal_user.reload.roles }.from([]).to([Roles::NORMAL]) }
+    it { expect { subject }.to change { super_admin_user.reload.roles }.from([]).to([Roles::SUPER_ADMIN]) }
 
-    describe 'when role does not exist' do
-      let!(:admin_user) { create(:user, role: 'admin') }
-      it 'raises error' do
-        expect { subject }.to raise_error
-      end
-    end
-
-    describe 'when user already has a given role' do
-      let(:super_admin_role) { create(:role, name: 'super_admin') }
-      let!(:super_admin_user) { create(:user, role: 'super_admin', roles: [super_admin_role]) }
-      it 'does not raise error' do
-        expect { subject }.not_to raise_error
+    context 'when user already has a given role' do
+      let!(:super_admin_user) { create(:user, role: 'super_admin', roles: [Roles::ADMIN]) }
+      it 'updates roles' do
+        expect { subject }.to change { super_admin_user.reload.roles }
+          .from([Roles::ADMIN]).to([Roles::SUPER_ADMIN])
       end
     end
   end
