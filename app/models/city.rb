3d65964b36673b207f039cdc3ce3e439fc897baa
Fabio L Janiszevski <fabiosammy@gmail.com>
2022-09-30 11:53:01 -0300

    Move from the custom city approach to the select2 with all cities in there

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,8 @@
 class City < ApplicationRecord
   belongs_to :state
 
+  scope :collection, -> { joins(:state).includes(:state).order('states.name ASC, cities.name ASC') }
+
   validates :name, presence: :true
 
   def to_s
