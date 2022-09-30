93318f67a7308c43bfc2a8091d05c6b5dc162240
Fabio L Janiszevski <fabiosammy@gmail.com>
2022-09-30 11:49:10 -0300

    Remove the unique key fot city name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_16_124000) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_30_143237) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,7 +43,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_16_124000) do
     t.bigint "state_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_cities_on_name", unique: true
     t.index ["state_id"], name: "index_cities_on_state_id"
   end
 
