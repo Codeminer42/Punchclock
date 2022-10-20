2ad30b6f3036054a867f01a5df2d461ce4039dc4
Fabio Saito <fabio.h.saito@gmail.com>
2022-10-20 12:16:19 -0300

    update contributions factory

--- spec/factories/contributions.rb
+++ spec/factories/contributions.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     user { create(:user) }
     repository { create(:repository) }
     state { :received }
+    pr_state { :open }
     sequence :link do |n|
       "https://www.github.com/company/example-#{n}/pull/#{n}"
     end
@@ -17,6 +18,18 @@ FactoryBot.define do
 
     trait :approved do
       state { :approved }
+    end
+
+    trait :open_pr do
+      pr_state { :open }
+    end
+
+    trait :merged_pr do
+      pr_state { :merged }
+    end
+
+    trait :closed_pr do
+      pr_state { :closed }
     end
   end
 end
