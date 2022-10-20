61026535e570ea21ae08f332e9396c68dd4e07f4
Fabio Saito <fabio.h.saito@gmail.com>
2022-10-20 12:15:52 -0300

    refactor and improvements

--- app/services/github/contributions/pull_request_collector.rb
+++ app/services/github/contributions/pull_request_collector.rb
@@ -21,28 +21,32 @@ module Github
 
       def fetch_all
         valid_pull_requests = Contribution.where.not(state: :refused).select(:id, :link, :pr_state)
-        updated_pull_request = valid_pull_requests.map {|contribution| { id: contribution.id, pr_state: current_pr_state(contribution.link)} }
+        updated_pull_request = valid_pull_requests.map {|contribution| { id: contribution.id, pr_state: new_pr_state(contribution.link)} }
 
-        updated_pull_request.select{ |el| el[:pr_state] != valid_pull_requests.find_by(id: el[:id]).pr_state }
+        updated_pull_request.select{ |new_pr| validate_update(valid_pull_requests.find_by(id: new_pr[:id]), new_pr) }
       end
 
-      def current_pr_state(link)
-        pr_body = fetch_pr(link)
-
-        return 'Merged' if pr_body.pull_request&.merged_at
-        pr_body.state.capitalize
+      def validate_update(current_pr, new_pr)
+        return false if new_pr[:pr_state] == 'Fetch Error'
+        current_pr.pr_state != new_pr[:pr_state]
       end
 
-      def fetch_pr(link)
+      def new_pr_state(link)
         begin
-          organization, repository, pr_id = parse_contribution_link(link)
-          client.issues.get(organization, repository, pr_id).body
+          pr_body = fetch_pr(link)
 
+          return 'merged' if pr_body.pull_request&.merged_at
+          pr_body.state
         rescue StandardError => e
           Rails.logger.error e.message
           Rails.logger.error e.backtrace.join("\n")
-          []
+          return 'Fetch Error'
         end
+      end
+
+      def fetch_pr(link)
+        organization, repository, pr_id = parse_contribution_link(link)
+        client.issues.get(organization, repository, pr_id).body
       end
 
       def parse_contribution_link(link)
