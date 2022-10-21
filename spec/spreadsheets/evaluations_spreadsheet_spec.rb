5a5cb64a5ee668b2aa1b12928b147eafa5d00fa8
Fabio Saito <fabio.h.saito@gmail.com>
2022-10-21 15:19:56 -0300

    update tests

--- spec/spreadsheets/evaluations_spreadsheet_spec.rb
+++ spec/spreadsheets/evaluations_spreadsheet_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe EvaluationsSpreadsheet do
     ]
   end
 
-  let (:body_attributes) do
+  let(:body_attributes) do
     [
       evaluation.id,
       evaluation.evaluator.name,
@@ -41,36 +41,15 @@ RSpec.describe EvaluationsSpreadsheet do
 
   describe '#to_string_io' do
     subject do
-      evaluation_spreadsheet
-        .to_string_io
-        .force_encoding('iso-8859-1')
-        .encode('utf-8')
+      evaluation_spreadsheet.to_string_io
     end
 
-    it 'returns spreadsheet data' do
-      is_expected.to include(
-        evaluation.observation,
-        evaluation.english_level.text,
-        I18n.l(evaluation.created_at, format: :long),
-        I18n.l(evaluation.created_at, format: :long)
-      )
+    before do
+      File.open('/tmp/spreadsheet_temp.xlsx', 'wb') {|f| f.write(subject) }
     end
 
-    it 'returns spreadsheet with header' do
-      is_expected.to include(*header_attributes)
-    end
-  end
-
-  describe '#generate_xls' do
-    subject(:spreadsheet) { evaluation_spreadsheet.generate_xls }
-
-    it 'returns spreadsheet object with header' do
-      expect(spreadsheet.row(0)).to containing_exactly(*header_attributes)
-    end
-
-    it 'returns spreadsheet object with body' do
-      expect(spreadsheet.row(1)).to containing_exactly(*body_attributes)
-    end
+    it_behaves_like 'a valid spreadsheet'
+    it_behaves_like 'a spreadsheet with header and body'
   end
 
   describe '#body' do
