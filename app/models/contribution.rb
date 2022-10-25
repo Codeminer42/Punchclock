0e00e909d22c34adcaa061069c5931e4ee1ed640
Alessandro Dias <alessandrodbatista@gmail.com>
2022-10-25 11:19:57 -0300

    run pr state fetch only for contributions with open state

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -42,4 +42,5 @@ class Contribution < ApplicationRecord
   scope :last_week, -> { where("contributions.created_at >= :start_date AND contributions.created_at <= :end_date", { :start_date => 1.week.ago.beginning_of_week, :end_date => 1.week.ago.end_of_week }) }
   scope :active_engineers, -> { joins(:user).merge(User.engineer.active) }
   scope :valid_pull_requests, -> { where.not(state: :refused) }
+  scope :without_pr_state, ->(state) { where.not(pr_state: state) }
 end
--- spec/services/github/contributions/pull_request_collector_spec.rb
+++ spec/services/github/contributions/pull_request_collector_spec.rb
@@ -10,65 +10,63 @@ RSpec.describe Github::Contributions::PullRequestCollector, type: :service do
   end
 
   describe '#all' do
-    subject { described_class.new(client: client) }
-
     let(:client) { class_double(Github) }
+
+    subject { described_class.new(client: client) }
 
     context 'when there are no contributions in database' do
       it 'returns empty array' do
-        expect(subject.all).to eq([])
+        expect(subject.all).to be_empty
       end
     end
 
     context 'when there are contributions in database' do
       context 'when Github API is not able to respond' do
-        let!(:contribution) { create(:contribution, :open_pr) }
-
         before do
+          create(:contribution, :open_pr)
           allow(client).to receive(:issues).and_raise(Github::Error::GithubError)
         end
 
         it 'returns empty' do
-          expect(subject.all).to eq([])
+          expect(subject.all).to be_empty
         end
       end
 
       context 'when Pull Requests are fetched from GitHub API' do
         context 'when pr_state changed' do
-          before do
-            allow(client).to receive(:issues).and_return(pull_request_status)
-          end
-
           let!(:contribution) { create(:contribution, :open_pr) }
           let(:response_body) do
             double(
               state: 'closed',
-              pull_request: double({ merged_at: nil })
+              pull_request: double(merged_at: nil)
             )
           end
-          let(:get) { double(body: response_body) }
-          let(:pull_request_status) { double(get: get) }
           let(:expected_pr_collection) { { id: contribution.id, pr_state: 'closed' } }
 
+          before do
+            create(:contribution, :merged_pr)
+            create(:contribution, :closed_pr)
+
+            allow(client).to receive_message_chain(:issues, :get, :body) { response_body }
+          end
+
           it 'returns contribution id and new PR State' do
-            expect(subject.all).to include(expected_pr_collection)
+            expect(subject.all).to contain_exactly(expected_pr_collection)
           end
         end
 
         context 'when pr_state did not change' do
-          before do
-            allow(client).to receive(:issues).and_return(pull_request_status)
-          end
-
           let!(:contribution) { create(:contribution, :open_pr) }
           let(:response_body) do
             double(
               state: 'open',
-              pull_request: double({ merged_at: nil })
+              pull_request: double(merged_at: nil)
             )
           end
-          let(:get) { double(body: response_body) }
-          let(:pull_request_status) { double(get: get) }
+
+          before do
+            allow(client).to receive_message_chain(:issues, :get, :body) { response_body }
+          end
 
           it 'returns empty' do
             expect(subject.all).to be_empty
