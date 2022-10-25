cad95d84e384821941e9547db331da554f89bc2c
Fabio Saito <fabio.h.saito@gmail.com>
2022-10-25 12:50:37 -0300

    Reimplement questionaries with caxlsx gem (#287)

--- app/spreadsheets/questionnaires_spreadsheet.rb
+++ app/spreadsheets/questionnaires_spreadsheet.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class QuestionnairesSpreadsheet < BaseSpreadsheet
+class QuestionnairesSpreadsheet < DefaultSpreadsheet
   def body(questionnaire)
     [
       questionnaire.id,
