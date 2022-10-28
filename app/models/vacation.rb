3d40ad83e5d193ec7e56b16861ade87d9c046aa5
Fabio Saito <fabio.h.saito@gmail.com>
2022-10-28 15:06:44 -0300

    improve select date on start and end date

--- app/models/vacation.rb
+++ app/models/vacation.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Vacation < ApplicationRecord
+  attr_accessor :starting_day, :ending_day
   extend Enumerize
 
   belongs_to :user
@@ -11,10 +12,32 @@ class Vacation < ApplicationRecord
     pending: 0,
     approved: 1,
     denied: 2,
-    cancelled: 3
+    cancelled: 3,
   }, predicates: true, scope: :shallow
 
   validates :start_date, :end_date, :user, presence: true
-  validates :end_date, comparison: { greater_than: :start_date }, on: :create
-  validates :start_date, comparison: { greater_than: Time.zone.today }, on: :create
+  validates :end_date, comparison: { greater_than: :start_date }
+  validates :start_date, comparison: { greater_than: Time.zone.today }
+
+  def starting_day=(date)
+    @starting_day = date.presence
+    mount_start_date
+    date
+  end
+
+  def ending_day=(date)
+    @ending_day = date.presence
+    mount_end_date
+    date
+  end
+
+  private
+
+  def mount_start_date
+    self.start_date = @starting_day unless @starting_day.nil?
+  end
+
+  def mount_end_date
+    self.end_date = @ending_day unless @ending_day.nil?
+  end
 end
