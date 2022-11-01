69c8b9591deca191071e4f40a415efb3d9e5ca9a
geeksilva97 <edigleyssonsilva@gmail.com>
2022-11-01 05:46:41 -0300

    add not found message

--- spec/controllers/my_contributions_controller_spec.rb
+++ spec/controllers/my_contributions_controller_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe MyContributionsController, type: :controller do
 
   let(:user) { build_stubbed(:user) }
   let(:contribution_list) { [build_stubbed(:contribution), build_stubbed(:contribution)] }
-  let(:contribution_model) { double('ActiveRecord', :order => contribution_list) }
+  let(:contribution_model) { double('ActiveRecord', order: contribution_list) }
   let(:page) { Capybara::Node::Simple.new(response.body) }
 
   before do
     allow(controller).to receive(:authenticate_user!)
     allow(controller).to receive(:current_user).and_return(user)
     allow(user).to receive(:contributions).and_return(contribution_model)
-    allow(contribution_model).to receive(:where).and_return(contributions_model)
+    allow(contribution_model).to receive(:where).and_return(contribution_model)
   end
 
   describe 'GET index' do
@@ -28,18 +28,26 @@ RSpec.describe MyContributionsController, type: :controller do
       expect(response).to have_http_status(:ok)
     end
 
-    # context 'when the user does not contain contributions' do
-    # end
+    context 'when the user does not contain contributions' do
+      let(:contribution_list) { [] }
+
+      it 'renders the table with not found message' do
+        get :index
+
+        expect(page.find('table')).to have_content(I18n.t('my_contributions.no_prs_found'))
+      end
+    end
 
     context 'when the user contains contributions' do
       it 'renders the table with a list of contributions' do
         get :index
 
-        expect(page).to have_selector('table')
-          .and have_content(contribution_list[0].link)
-          .and have_content(Contribution.human_attribute_name("pr_state/#{contribution_list[0].pr_state}"))
-          .and have_content(contribution_list[0].created_at.strftime('%d/%m/%Y'))
-          .and have_content(Contribution.human_attribute_name("state/#{contribution_list[0].state}"))
+        expect(page.find('table')).to have_content(contribution_list[0].link)
+                                  .and have_content(Contribution.human_attribute_name("pr_state/#{contribution_list[0].pr_state}"))
+                                  .and have_content(contribution_list[0].created_at.strftime('%d/%m/%Y'))
+                                  .and have_content(contribution_list[1].link)
+                                  .and have_content(Contribution.human_attribute_name("pr_state/#{contribution_list[1].pr_state}"))
+                                  .and have_content(contribution_list[1].created_at.strftime('%d/%m/%Y'))
       end
     end
   end
