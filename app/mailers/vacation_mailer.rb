e786bab279a9d9f7416b346b6683f837ab4df2bc
Lukazovic <lucas10.vieira.lv@gmail.com>
2022-11-02 10:40:28 -0300

    chore: improve locale code format in vacation mailer

--- app/mailers/vacation_mailer.rb
+++ app/mailers/vacation_mailer.rb
@@ -1,6 +1,6 @@
 class VacationMailer < ApplicationMailer
   def notify_vacation_request(vacation, admins_emails)
     @vacation = vacation
-    mail(to: admins_emails, subject: (t '.subject', user: @vacation.user.name))
+    mail(to: admins_emails, subject: t('.subject', user: @vacation.user.name))
   end
 end
