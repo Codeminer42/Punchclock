2e72bd7a4f7b175707688bb37755ac2075c3b80c
Lucas Vieira <lucas10.vieira.lv@gmail.com>
2022-11-03 09:32:55 -0300

    Send email when requesting vacation (#294)
    * feat: send email when user requests vacations
    
    * fix: typo in vacation start_date locale
    
    * add: notify_vacation_request subject to locale file
    
    * chore: improve locale code format in vacation mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'do-not-reply@punchclock.com'
   layout 'mailer'
 end
