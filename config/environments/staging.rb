99db5bfd09539ce323aa6420af076f86eb32b976
Alessandro Dias <alessandrodbatista@gmail.com>
2022-11-22 08:29:16 -0300

    change email provider

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -20,12 +20,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: ENV['HOST'] }
   config.action_mailer.perform_deliveries = true
-  ActionMailer::Base.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port: '587',
+  config.action_mailer.smtp_settings = {
+    address: ENV['MAILGUN_SMTP_SERVER'],
+    port: ENV['MAILGUN_SMTP_PORT'],
+    user_name: ENV['MAILGUN_SMTP_LOGIN'],
+    password: ENV['MAILGUN_SMTP_PASSWORD'],
     authentication: :plain,
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: 'heroku.com'
+    domain: 'codeminer42.com'
   }
 end
