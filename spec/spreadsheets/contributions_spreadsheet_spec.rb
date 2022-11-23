2c1f14161220b9d56fcd39bbb71707ed00df1ce7
Heitor Cardozo <heitor711n@gmail.com>
2022-11-23 16:15:26 -0300

    refactor contributions_spreadsheet to add all contribution's fields

--- spec/spreadsheets/contributions_spreadsheet_spec.rb
+++ spec/spreadsheets/contributions_spreadsheet_spec.rb
@@ -9,9 +9,12 @@ RSpec.describe ContributionsSpreadsheet do
     %w[
       user
       link
+      created_at
       state
       pr_state
-      created_at
+      reviewed_by
+      reviewed_at
+      rejected_reason
       updated_at
     ].map { |attribute| Contribution.human_attribute_name(attribute) }
   end
@@ -20,24 +23,41 @@ RSpec.describe ContributionsSpreadsheet do
     [
       contribution.user.name,
       contribution.link,
+      contribution.created_at.to_s,
       Contribution.human_attribute_name("state/#{contribution.state}"),
       contribution.pr_state,
-      contribution.created_at.to_s,
+      contribution.reviewed_by,
+      contribution.reviewed_at.to_s,
+      contribution.rejected_reason_text,
       contribution.updated_at.to_s
     ]
   end
 
   describe '#to_string_io' do
-    subject(:to_string_io) do
-      contribution_spreadsheet.to_string_io
+    subject do
+      contribution_spreadsheet
+        .to_string_io
+        .force_encoding('iso-8859-1')
+        .encode('utf-8')
     end
 
-    before do
-      File.open('/tmp/spreadsheet_temp.xlsx', 'wb') {|f| f.write(subject) }
+    it 'returns spreadsheet data' do
+      expect(contribution_spreadsheet).to receive(:generate_xlsx).once
+
+      subject
+    end
+  end
+
+  describe '#generate_xlsx' do
+    subject(:spreadsheet) { contribution_spreadsheet.generate_xlsx }
+
+    it 'returns spreadsheet object with header' do
+      expect(spreadsheet.rows.first.cells.map(&:value)).to containing_exactly(*header_attributes)
     end
 
-    it_behaves_like 'a valid spreadsheet'
-    it_behaves_like 'a spreadsheet with header and body'
+    it 'returns spreadsheet object with body' do
+      expect(spreadsheet.rows.last.cells.map(&:value)).to containing_exactly(*body_attributes)
+    end
   end
 
   describe '#body' do
