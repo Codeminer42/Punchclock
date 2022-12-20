77130501fa022df5bdaeb6c96c9558730b674db6
Carlos Lopes <carlos.el.lopes@gmail.com>
2022-12-20 17:00:00 -0300

    Fix migration that was causing error because it was calling a missing model

--- db/migrate/20221216174823_remove_month_from_exchange_rates.rb
+++ db/migrate/20221216174823_remove_month_from_exchange_rates.rb
@@ -1,7 +1,5 @@
 class RemoveMonthFromExchangeRates < ActiveRecord::Migration[7.0]
   def up
-    ExchangeRate.where(month: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]).delete_all
-
     remove_index :exchange_rates, column: %w[month year currency]
     remove_column :exchange_rates, :month, :integer
     add_index :exchange_rates, %w[year currency], unique: true
