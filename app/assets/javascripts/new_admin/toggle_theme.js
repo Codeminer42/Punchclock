bfb1d3eb139c5c5e0d66eb437fe259146301a753
mateusbw <mateus_bwilding@hotmail.com>
2023-01-13 10:10:56 -0300

    Update theme icon

--- app/assets/javascripts/new_admin/toggle_theme.js
+++ app/assets/javascripts/new_admin/toggle_theme.js
@@ -1,6 +1,8 @@
 const bodyTag = document.querySelector('html')
 
 const changeThemeButton = document.querySelector('[data-change-theme]')
+const lightThemeIcon = document.querySelector('[data-light-icon]')
+const darkThemeIcon = document.querySelector('[data-dark-icon]')
 
 const isDarkThemeEnabled = () => {
   if (window.localStorage.getItem('dark-theme')) {
@@ -17,9 +19,20 @@ const setDarkTheme = (active) => {
   window.localStorage.setItem('dark-theme', active)
 }
 
+const updateThemeIcon = (isDarkTheme) => {
+  if (isDarkTheme) {
+    darkThemeIcon.classList.add('hidden')
+    lightThemeIcon.classList.remove('hidden')
+  } else {
+    lightThemeIcon.classList.add('hidden')
+    darkThemeIcon.classList.remove('hidden')
+  }
+}
+
 const initTheme = () => {
   const isDarkTheme = isDarkThemeEnabled()
   if (isDarkTheme) bodyTag.classList.add('dark')
+  updateThemeIcon(isDarkTheme)
 }
 
 initTheme()
@@ -28,4 +41,5 @@ changeThemeButton.addEventListener('click', () => {
   const isDarkTheme = isDarkThemeEnabled()
   setDarkTheme(!isDarkTheme)
   bodyTag.classList.toggle('dark')
+  updateThemeIcon(!isDarkTheme)
 })
