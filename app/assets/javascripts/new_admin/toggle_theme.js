3645839a6db09ac7f83cd9ee3cc8153cf6a1084a
mateusbw <mateus_bwilding@hotmail.com>
2023-01-13 10:02:46 -0300

    Save theme in local storage

--- app/assets/javascripts/new_admin/toggle_theme.js
+++ app/assets/javascripts/new_admin/toggle_theme.js
@@ -1,7 +1,31 @@
-const bodyTag = document.querySelector('html');
+const bodyTag = document.querySelector('html')
 
 const changeThemeButton = document.querySelector('[data-change-theme]')
 
+const isDarkThemeEnabled = () => {
+  if (window.localStorage.getItem('dark-theme')) {
+    return JSON.parse(window.localStorage.getItem('dark-theme'))
+  }
+
+  if (window.matchMedia)
+    return window.matchMedia('(prefers-color-scheme: dark)').matches
+
+  return false
+}
+
+const setDarkTheme = (active) => {
+  window.localStorage.setItem('dark-theme', active)
+}
+
+const initTheme = () => {
+  const isDarkTheme = isDarkThemeEnabled()
+  if (isDarkTheme) bodyTag.classList.add('dark')
+}
+
+initTheme()
+
 changeThemeButton.addEventListener('click', () => {
+  const isDarkTheme = isDarkThemeEnabled()
+  setDarkTheme(!isDarkTheme)
   bodyTag.classList.toggle('dark')
 })
