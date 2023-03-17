f3da8d0a8460d2834c31e5027834e2790ad3a032
Samuel <samuel.souto7@gmail.com>
2023-03-17 16:50:43 -0300

    Creating allocation chart decorator

--- app/controllers/new_admin/allocation_chart_controller.rb
+++ app/controllers/new_admin/allocation_chart_controller.rb
@@ -7,27 +7,13 @@ module NewAdmin
     def index
       allocations = AllocationsAndUnalocatedUsersQuery.new(Allocation).call
 
-      @allocations = allocations.reduce([]) do |acc, allo|
-        allocation = { :name => allo.user.first_and_last_name,
-                       :project_name => allo.project ? allo.project.name : nil,
-                       :level => decorated_user(allo).level,
-                       :specialty => decorated_user(allo).specialty,
-                       :english_evaluation => decorated_user(allo).english_level,
-                       :skills => decorated_user(allo).skills,
-                       :allocation_end_at => build_date(allo.end_at) }
-
-        acc.push(allocation)
-      end
+      @allocations = allocations.map { |allocation| decorated_allocation(allocation) }
     end
 
     private
 
-    def decorated_user(allocation)
-      UserDecorator.decorate(allocation.user)
-    end
-
-    def build_date(date)
-      date ? date.to_time.strftime("%d/%m/%Y") : I18n.t('not_allocated')
+    def decorated_allocation(allocation)
+      AllocationChartDecorator.decorate(allocation)
     end
   end
 end
