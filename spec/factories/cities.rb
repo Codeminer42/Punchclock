53d1af64544981e735100940d9d2e8c3acb25677
lucas.oliveira <lucas.oliveira@codeminer42.com>
2023-04-19 16:10:23 -0300

    test: add with_holiday trait to cities factory

--- spec/factories/cities.rb
+++ spec/factories/cities.rb
@@ -2,5 +2,11 @@ FactoryBot.define do
   factory :city do
     sequence(:name) {|i| "City Name #{i}" }
     state { create(:state) }
+
+    trait :with_holiday do
+      after :create do |city|
+        city.regional_holidays = create_list(:regional_holiday, 5)
+      end
+    end
   end
 end
