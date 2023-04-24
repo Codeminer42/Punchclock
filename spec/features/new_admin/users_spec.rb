54c1ec50dbb75644898ae2960a14c1c60ede6d88
bedreski <fabianabedreski@gmail.com>
2023-04-24 17:34:42 -0300

    finish user allocation details and tests

--- spec/features/new_admin/users_spec.rb
+++ spec/features/new_admin/users_spec.rb
@@ -60,5 +60,16 @@ describe 'Users', type: :feature do
         have_content('ALOCAÇÃO ATUAL') &&
         have_content(allocation.project_name)
     end 
+
+    it 'shows table of allocations', js: true do 
+      visit "/new_admin/users/#{user.id}"
+      click_button('Alocações')
+      page.has_table?("allocations_table")
+      expect(page).to have_content(another_allocation.project_name)
+      expect(page).to have_content("#{l(another_allocation.start_at)}")
+      expect(page).to have_content("#{l(another_allocation.end_at)}")
+      expect(page).to have_content("#{I18n.t(another_allocation.ongoing)}")
+      expect(page).to have_content('Acessar Alocação')
+    end 
   end 
 end 
