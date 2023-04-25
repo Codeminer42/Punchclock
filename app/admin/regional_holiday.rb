b5a43c1702bcbb26b27f7ebb0530ef2f6abe78d5
lucas.oliveira <lucas.oliveira@codeminer42.com>
2023-04-25 11:02:34 -0300

    refactor: remove offices column from active admin regional holidays page

--- app/admin/regional_holiday.rb
+++ app/admin/regional_holiday.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register RegionalHoliday do
-  permit_params :name, :day, :month, office_ids: [], city_ids: []
+  permit_params :name, :day, :month, city_ids: []
 
   filter :name
-  filter :offices, multiple: true, collection: -> { Office.active.order(:city) }
   filter :cities, multiple: true, collection: -> { City.order(:name) }
   filter :day
   filter :month
@@ -13,9 +12,6 @@ ActiveAdmin.register RegionalHoliday do
     column :name
     column :day
     column :month
-    column :offices do |holiday|
-      offices_by_holiday(holiday)
-    end
     column :cities
     actions
   end
@@ -26,9 +22,6 @@ ActiveAdmin.register RegionalHoliday do
       row :name
       row :day
       row :month
-      row :offices do |holiday|
-        offices_by_holiday(holiday)
-      end
       row :cities
     end
   end
@@ -38,7 +31,6 @@ ActiveAdmin.register RegionalHoliday do
       f.input :name
       f.input :day
       f.input :month
-      f.input :offices, as: :check_boxes, collection: Office.active.order(:city)
       f.input :cities, as: :select, multiple: true, collection: City.order(:name)
     end
       f.actions
