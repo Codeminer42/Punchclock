ad97947e3c093cd4c15e0edd7da12c8292902eee
Jonas <46506018+JonasTeixeira42@users.noreply.github.com>
2023-05-23 11:56:13 -0300

    create talkings migration

---
+++ db/migrate/20230523144746_create_talkings.rb
@@ -0,0 +1,12 @@
+class CreateTalkings < ActiveRecord::Migration[7.0]
+  def change
+    create_table :talkings do |t|
+      t.string :event_name
+      t.string :talk_title
+      t.timestamp :date
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
