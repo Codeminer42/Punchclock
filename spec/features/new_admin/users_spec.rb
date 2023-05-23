4bbd3c3aa25b6a8603b820bc949d50ea7971339d
bedreski <fabianabedreski@gmail.com>
2023-05-23 16:20:45 -0300

    improve test assertions

--- spec/features/new_admin/users_spec.rb
+++ spec/features/new_admin/users_spec.rb
@@ -44,19 +44,19 @@ describe 'Users', type: :feature do
     it 'shows allocations details with user current allocation', js: true do 
       visit "/new_admin/users/#{user.id}"
       click_button('Alocações')
-      expect(page).to have_content('ALOCAÇÃO ATUAL')
-      expect(page).to have_content(allocation.project_name)
+      expect(page).to have_content('ALOCAÇÃO ATUAL') && 
+        have_content(allocation.project_name)
     end 
 
     it 'shows table of allocations', js: true do 
       visit "/new_admin/users/#{user.id}"
       click_button('Alocações')
-      page.has_table?("allocations_table")
-      expect(page).to have_content(another_allocation.project_name)
-      expect(page).to have_content("#{l(another_allocation.start_at)}")
-      expect(page).to have_content("#{l(another_allocation.end_at)}")
-      expect(page).to have_content("#{I18n.t(another_allocation.ongoing)}")
-      expect(page).to have_content('Acessar Alocação')
+      expect(page).to have_table("allocations_table") 
+      expect(page).to have_content(another_allocation.project_name) &&
+        have_content("#{l(another_allocation.start_at)}") &&
+        have_content("#{l(another_allocation.end_at)}") &&
+        have_content("#{I18n.t(another_allocation.ongoing)}") &&
+        have_content('Acessar Alocação')
     end 
   end 
 end 
