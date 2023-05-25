48c435d9793d8d366786fc10fee13310b7688fc9
u can call me Fabi <64296491+bedreski@users.noreply.github.com>
2023-05-25 14:01:57 -0400

    feat: new admin user english evaluation page (#403)
    * new admin page for user allocations
    
    * feature test for user allocations tab
    
    * finish user allocation details and tests
    
    * improve test assertions
    
    * new admin page for user allocations
    
    * Adding tests
    
    * update locale files
    
    * new admin page for user english evaluations
    
    * fix: evaluator name in assert condition
    
    * fix: decorator to display correct date
    
    * fix: delete feature test inside controller spec
    
    * refactor: add composable matchers
    
    * refactor: change english  evaluation query with merge method
    
    ---------
    
    Co-authored-by: Samuel <samuel.souto7@gmail.com>

--- app/controllers/new_admin/users_controller.rb
+++ app/controllers/new_admin/users_controller.rb
@@ -8,6 +8,7 @@ module NewAdmin
       @user = User.find(params[:id]).decorate
       @user_allocations = Allocation.where(user_id: params[:id]).decorate
       @performance_evaluations = Evaluation.joins(:questionnaire).merge(Questionnaire.performance).where(evaluated_id: params[:id]).order(created_at: :desc).decorate
+      @english_evaluations = Evaluation.joins(:questionnaire).merge(Questionnaire.english).where(evaluated_id: params[:id]).order(created_at: :desc).decorate
     end
   end
 end
