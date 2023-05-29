1c17d893f680cd3b295d71919a251ebd2923297f
Jonas <46506018+JonasTeixeira42@users.noreply.github.com>
2023-05-29 17:28:20 -0300

    create update contribution screen

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -1,5 +1,34 @@
+# frozen_string_literal: true
+
 class ContributionsController < ApplicationController
   def index
-    @user_contributions = current_user.contributions.approved.order(pr_state: :desc)
+    @user_contributions = scopped_contributions.approved.order(pr_state: :desc)
+  end
+
+  def edit
+    @contribution = scopped_contributions.find(params[:id])
+  end
+
+  def update
+    @contribution = scopped_contributions.find(params[:id])
+
+    if @contribution.update(contribution_params)
+      redirect_to contributions_path, notice: I18n.t(
+        :notice, scope: "flash.actions.update", resource_name: Contribution.model_name.human
+      )
+    else
+      flash_errors('update')
+      render :edit
+    end
+  end
+
+  private
+
+  def scopped_contributions
+    current_user.contributions
+  end
+
+  def contribution_params
+    params.require(:contribution).permit(:description)
   end
 end
