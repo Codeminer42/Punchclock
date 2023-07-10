4e9f763992e5aae5dad16a0f0edf0fd32f3c419b
Daniel M Brasil <danielmbrasil@protonmail.com>
2023-07-10 11:31:42 -0300

    fix breaking specs and refactor ProfessionalExperience model

--- app/models/professional_experience.rb
+++ app/models/professional_experience.rb
@@ -5,49 +5,43 @@ class ProfessionalExperience < ApplicationRecord
 
   validates_presence_of :company, :position, :start_date
 
-  validate :valid_date?
-
-  validate :greater_than_current_month?, if: -> { correct_format?(start_date) && date_exists?(start_date) }
+  validate :date_format
+  validate :greater_than_current_month?, if: -> { valid_date?(start_date) }
+  validate :greater_than_end_date?, if: -> { valid_date?(start_date) && valid_date?(end_date) }
 
   private
 
-  FIELDS_TO_BE_VALIDATE = %i[start_date end_date].freeze
+  FIELDS_TO_BE_VALIDATED = %i[start_date end_date].freeze
 
   def greater_than_current_month?
-    return if mount_date(start_date) < Date.today.beginning_of_month
+    return if start_date.to_date < Date.today.beginning_of_month
 
     errors.add(:start_date, "Start date can't be ahead of today's date")
   end
 
-  def valid_date?
-    FIELDS_TO_BE_VALIDATE.each do |field|
+  def greater_than_end_date?
+    return if start_date.to_date < end_date.to_date
+
+    errors.add(:start_date, "Start date can't be ahead of end date")
+  end
+
+  def date_format
+    FIELDS_TO_BE_VALIDATED.each do |field|
       next if self[field].blank?
 
-      errors.add(field, 'Invalid date') if !correct_format?(self[field]) || !date_exists?(self[field])
+      errors.add(field, 'Invalid date') unless valid_date?(self[field])
     end
   end
 
-  def date_exists?(date)
-    splitted_string = date.split('/')
+  def valid_date?(date)
+    return false unless correct_format?(date)
 
-    if Date.valid_date?(splitted_string[1].to_i, splitted_string[0].to_i, 1)
-      true
-    else
-      false
-    end
+    month, year = date.split('/').map(&:to_i)
+
+    Date.valid_date?(year, month, 1)
   end
 
   def correct_format?(date)
-    if %r{(\d{2}/\d{4})}.match(date)
-      true
-    else
-      false
-    end
-  end
-
-  def mount_date(date)
-    splitted_string = date.split('/')
-
-    Date.new(splitted_string[1].to_i, splitted_string[0].to_i, 1)
+    %r{(\d{2}/\d{4})}.match?(date)
   end
 end
