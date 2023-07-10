2fb71f269cf0f0d9aef7cb9c1dd3d9a03ed06b12
Daniel M Brasil <danielmbrasil@protonmail.com>
2023-07-10 14:27:17 -0300

    remove unused methods in ProfessionalExperienceDecorator

--- app/decorators/professional_experience_decorator.rb
+++ app/decorators/professional_experience_decorator.rb
@@ -3,21 +3,19 @@
 class ProfessionalExperienceDecorator < Draper::Decorator
   delegate_all
 
-  def start_date
-    model.start_date.to_date.to_fs(:date)
-  end
-
   def end_date
-    return I18n.t(:present) if model.end_date.nil?
-    model.end_date.to_date.to_fs(:date)
+    return I18n.t(:present) if model.end_date.blank?
+
+    model.end_date
   end
 
   def start_date_month
-    I18n.l(model.start_date, format: :month_and_year)
+    I18n.l(model.start_date.to_date, format: :month_and_year)
   end
 
   def end_date_month
-    return end_date if model.end_date.nil?
+    return end_date if model.end_date.blank?
+
     I18n.l(end_date.to_date, format: :month_and_year)
   end
 end
