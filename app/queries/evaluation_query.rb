45ed54ff8b0b6b29bcfa6e3eff227175777b41ab
Daniel M Brasil <danielmbrasil@protonmail.com>
2023-07-12 17:17:55 -0300

    add support for filtering options in EvaluationQuery class
    This commit introduces support for filtering options in the EvaluationQuery class.
    The call method now accepts a set of filtering options, including `evaluator_id`,
    `evaluated_id`, `questionnaire_type`, `created_at_start`, `created_at_end`,
    `evaluation_date_start`, and `evaluation_date_end`.

--- app/queries/evaluation_query.rb
+++ app/queries/evaluation_query.rb
@@ -1,12 +1,30 @@
 # frozen_string_literal: true
 
 class EvaluationQuery
-  def initialize(relation = Evaluation)
-    @relation = relation
+  def initialize(**options)
+    @evaluator_id          = options[:evaluator_id]
+    @evaluated_id          = options[:evaluated_id]
+    @questionnaire_type    = options[:questionnaire_type]
+    @created_at_start      = options[:created_at_start]
+    @created_at_end        = options[:created_at_end]
+    @evaluation_date_start = options[:evaluation_date_start]
+    @evaluation_date_end   = options[:evaluation_date_end]
   end
 
   def call
-    relation
+    query = scope
+    merge_options!(query)
+
+    query
+  end
+
+  private
+
+  attr_reader :evaluator_id, :evaluated_id, :questionnaire_type, :created_at_start, :created_at_end,
+              :evaluation_date_start, :evaluation_date_end
+
+  def scope
+    Evaluation
       .select('
         evaluations.*,
         evaluated.name as evaluated,
@@ -20,7 +38,53 @@ class EvaluationQuery
       ')
   end
 
-  private
+  def merge_options!(query)
+    query.merge!(by_evaluator(query))
+    query.merge!(by_evaluated(query))
+    query.merge!(by_questionnaire_type(query))
+    query.merge!(by_created_date(query))
+    query.merge!(by_evaluation_date(query))
+  end
 
-  attr_reader :relation
+  def by_evaluator(query)
+    return query unless evaluator_id.present?
+
+    query.where('evaluator.id = ?', evaluator_id)
+  end
+
+  def by_evaluated(query)
+    return query unless evaluated_id.present?
+
+    query.where('evaluated.id = ?', evaluated_id)
+  end
+
+  def by_questionnaire_type(query)
+    return query unless questionnaire_type.present?
+
+    query.where('questionnaires.kind = ?', questionnaire_type)
+  end
+
+  def by_created_date(query)
+    return query unless created_at_start.present? || created_at_end.present?
+
+    if created_at_start && created_at_end
+      query.where(evaluations: { created_at: created_at_start..created_at_end })
+    elsif created_at_start
+      query.where('evaluations.created_at >= ?', created_at_start)
+    elsif created_at_end
+      query.where('evaluations.created_at <= ?', created_at_end)
+    end
+  end
+
+  def by_evaluation_date(query)
+    return query unless evaluation_date_start.present? || evaluation_date_end.present?
+
+    if evaluation_date_start && evaluation_date_end
+      query.where(evaluations: { evaluation_date: evaluation_date_start..evaluation_date_end })
+    elsif evaluation_date_start
+      query.where('evaluations.evaluation_date >= ?', evaluation_date_start)
+    elsif evaluation_date_end
+      query.where('evaluations.evaluation_date <= ?', evaluation_date_end)
+    end
+  end
 end
