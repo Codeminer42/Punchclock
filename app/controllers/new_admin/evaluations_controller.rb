e5fa81cf17b16724ded6b31efd5922ec771f9b10
Daniel M Brasil <danielmbrasil@protonmail.com>
2023-07-13 10:42:30 -0300

    rename new_admin/evaluations_controller.rb

---
+++ app/controllers/new_admin/evaluations_controller.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module NewAdmin
+  class EvaluationsController < ApplicationController
+    layout 'new_admin'
+
+    def index
+      @evaluations = EvaluationQuery.new(**search_params).call.decorate
+    end
+
+    def show; end
+
+    private
+
+    def search_params
+      params.permit(
+        :evaluator_id,
+        :evaluated_id,
+        :questionnaire_type,
+        :created_at_start,
+        :created_at_end,
+        :evaluation_date_start,
+        :evaluation_date_end
+      ).to_h
+    end
+  end
+end
