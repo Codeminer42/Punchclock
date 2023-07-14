Codeminer42/Punchclock

commit d9acc12112c88849976ef797153375892f573515

feature: new admin page for user info (#388)
* New admin page for user info

* Adding tests

---------

Co-authored-by: Alessandro Dias Batista <alessandro.dias@codeminer42.com>

app/decorators/user_decorator.rb
@@ -51,4 +51,43 @@ class UserDecorator < Draper::Decorator
     return if model.city.nil?
     "#{model.city.name} - #{model.city.state.code}"
   end
+
+  def offices_managed
+    model.managed_offices.pluck(:city).to_sentence
+  end
+
+  def roles_sentence
+    roles = model.roles.values.reduce([]) do |acc, role|
+      acc.push(I18n.t("user.role.#{role}"))
+    end
+
+    roles.to_sentence.humanize
+  end
+
+  def mentor_name
+    return if model.mentor.nil?
+    model.mentor.first_and_last_name
+  end
+
+  def allow_overtime
+    I18n.t(model.allow_overtime) || 'N/A'
+  end
+
+  def active
+    I18n.t(model.active) || 'N/A'
+  end
+
+  def started_at
+    return if model.started_at.nil?
+    I18n.l(user.started_at)
+  end
+
+  def office_city
+    model.office.city
+  end
+
+  def otp_required_for_login
+    return I18n.t('false') if model.otp_required_for_login.nil?
+    I18n.t(model.otp_required_for_login)
+  end
 end

app/views/new_admin/users/_details.html.erb
@@ -1,0 +1,360 @@
+<div class="w-full overflow-hidden rounded-lg ring-1 ring-black ring-opacity-5">
+  <div class="w-full overflow-x-auto">
+    <table class="w-full whitespace-no-wrap">
+      <thead>
+        <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-white dark:bg-slate-700">
+          <th colspan="100%" class="px-4 py-3"><%= t 'user_details' %></th>
+        </tr>
+      </thead>
+
+      <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
+        <tr class="text-gray-700 dark:text-gray-300">
+          <th class="w-1/12">
+            <div class="ml-3 text-xs font-semibold tracking-wide text-left text-gray-500 uppercase">
+              <%= User.human_attribute_name('name') %>
+            </div>
+          </th>
+          <td class="px-4 py-3">
+            <div class="flex items-center text-sm">
+              <p class="text-sm text-gray-600 dark:text-gray-400 font-semibold">
+                <%= user.first_and_last_name %>
+              </p>
+            </div>
+          </td>
+        </tr>
+        <tr class="text-gray-700 dark:text-gray-300">
+          <th class="w-1/12">
+            <div class="ml-3 text-xs font-semibold tracking-wide text-left text-gray-500 uppercase">
+              <%= User.human_attribute_name('email') %>
+            </div>
+          </th>
+          <td class="px-4 py-3">
+            <div class="flex items-center text-sm">
+              <p class="text-sm text-gray-600 dark:text-gray-400 font-semibold">
+                <%= user.email %>
+              </p>
+            </div>
+          </td>
+        </tr>
+        <tr class="text-gray-700 dark:text-gray-300">
+          <th class="w-1/12">
+            <div class="ml-3 text-xs font-semibold tracking-wide text-left text-gray-500 uppercase">
+              2fa
+            </div>
+          </th>
+          <td class="px-4 py-3">
+            <div class="flex items-center text-sm">
+              <p class="text-sm text-gray-600 dark:text-gray-400 font-semibold">
+                <%= user.otp_required_for_login %>
+              </p>
+            </div>
+          </td>
+        </tr>
+        <tr class="text-gray-700 dark:text-gray-300">
+          <th class="w-1/12">
+            <div class="ml-3 text-xs font-semibold tracking-wide text-left text-gray-500 uppercase">
+              <%= User.human_attribute_name('github') %>
+            </div>
+          </th>
+          <td class="px-4 py-3">
+            <div class="flex items-center text-sm">
+              <p class="text-sm text-gray-600 dark:text-gray-400 font-semibold">
+                <%= user.github %>
+              </p>
+            </div>
+          </td>
+        </tr>
+        <tr class="text-gray-700 dark:text-gray-300">
+          <th class="w-1/12">
+            <div class="ml-3 text-xs font-semibold tracking-wide text-left text-gray-500 uppercase">
+              <%= User.human_attribute_name('office') %>
+            </div>
+          </th>
+          <td class="px-4 py-3">
+            <div class="flex items-center text-sm">
+              <p class="text-sm text-gray-600 dark:text-gray-400 font-semibold">
+                <%= user.office_city %>
+              </p>
+            </div>
+          </td>
+        </tr>
+        <tr class="text-gray-700 dark:text-gray-300">
+          <th class="w-1/12">
+            <div class="ml-3 text-xs font-semibold tracking-wide text-left text-gray-500 uppercase">
+              <%= User.human_attribute_name('city') %>
+            </div>
+          </th>
+          <td class="px-4 py-3">
+            <div class="flex items-center text-sm">
+              <p class="text-sm text-gray-600 dark:text-gray-400 font-semibold">
+                <%= user.city_text %>
+              </p>
+            </div>
+          </td>
+        </tr>
+        <tr class="text-gray-700 dark:text-gray-300">
+          <th class="w-1/12">
+            <div class="ml-3 text-xs font-semibold tracking-wide text-left text-gray-500 uppercase">
+              <%= User.human_attribute_name('managed_offices') %>
+            </div>
+          </th>
+          <td class="px-4 py-3">
+            <div class="flex items-center text-sm">
+              <p class="text-sm text-gray-600 dark:text-gray-400 font-semibold">
+                <%= user.offices_managed %>
+              </p>
+            </div>
+          </td>
+        </tr>
+        <tr class="text-gray-700 dark:text-gray-300">
+          <th class="w-1/12">
+            <div class="ml-3 text-xs font-semibold tracking-wide text-left text-gray-500 uppercase">
+              <%= User.human_attribute_name('english_score') %>
+            </div>
+          </th>
+          <td class="px-4 py-3">
+            <div class="flex items-center text-sm">
+              <p class="text-sm text-gray-600 dark:text-gray-400 font-semibold">
+                <%= user.english_score %>
+              </p>
+            </div>
+          </td>
+        </tr>
+        <tr class="text-gray-700 dark:text-gray-300">
+          <th class="w-1/12">
+            <div class="ml-3 text-xs font-semibold tracking-wide text-left text-gray-500 uppercase">
+              <%= User.human_attribute_name('overall_score') %>
+            </div>
+          </th>
+          <td class="px-4 py-3">
+            <div class="flex items-center text-sm">
+              <p class="text-sm text-gray-600 dark:text-gray-400 font-semibold">
+                <%= user.overall_score %>
+              </p>
+            </div>
+          </td>
+        </tr>
+        <tr class="text-gray-700 dark:text-gray-300">
+          <th class="w-1/12">
+            <div class="ml-3 text-xs font-semibold tracking-wide text-left text-gray-500 uppercase">
+              <%= User.human_attribute_name('performance_score') %>
+            </div>
+          </th>
+          <td class="px-4 py-3">
+            <div class="flex items-center text-sm">
+              <p class="text-sm text-gray-600 dark:text-gray-400 font-semibold">
+                <%= user.performance_score %>
+              </p>
+            </div>
+          </td>
+        </tr>
+        <tr class="text-gray-700 dark:text-gray-300">
+          <th class="w-1/12">
+            <div class="ml-3 text-xs font-semibold tracking-wide text-left text-gray-500 uppercase">
+              <%= User.human_attribute_name('occupation') %>
+            </div>
+          </th>
+          <td class="px-4 py-3">
+            <div class="flex items-center text-sm">
+              <p class="text-sm text-gray-600 dark:text-gray-400 font-semibold">
+                <%= t("user.occupation.#{user.occupation}") %>
+              </p>
+            </div>
+          </td>
+        </tr>
+        <tr class="text-gray-700 dark:text-gray-300">
+          <th class="w-1/12">
+            <div class="ml-3 text-xs font-semibold tracking-wide text-left text-gray-500 uppercase">
+              <%= User.human_attribute_name('specialty') %>
+            </div>
+          </th>
+          <td class="px-4 py-3">
+            <div class="flex items-center text-sm">
+              <p class="text-sm text-gray-600 dark:text-gray-400 font-semibold">
+                <%= user.specialty %>
+              </p>
+            </div>
+          </td>
+        </tr>
+        <tr class="text-gray-700 dark:text-gray-300">
+          <th class="w-1/12">
+            <div class="ml-3 text-xs font-semibold tracking-wide text-left text-gray-500 uppercase">
+              <%= User.human_attribute_name('level') %>
+            </div>
+          </th>
+          <td class="px-4 py-3">
+            <div class="flex items-center text-sm">
+              <p class="text-sm text-gray-600 dark:text-gray-400 font-semibold">
+                <%= user.level %>
+              </p>
+            </div>
+          </td>
+        </tr>
+        <tr class="text-gray-700 dark:text-gray-300">
+          <th class="w-1/12">
+            <div class="ml-3 text-xs font-semibold tracking-wide text-left text-gray-500 uppercase">
+              <%= User.human_attribute_name('contract_type') %>
+            </div>
+          </th>
+          <td class="px-4 py-3">
+            <div class="flex items-center text-sm">
+              <p class="text-sm text-gray-600 dark:text-gray-400 font-semibold">
+                <%= t("user.contract_type.#{user.contract_type.downcase}") %>
+              </p>
+            </div>
+          </td>
+        </tr>
+        <tr class="text-gray-700 dark:text-gray-300">
+          <th class="w-1/12">
+            <div class="ml-3 text-xs font-semibold tracking-wide text-left text-gray-500 uppercase">
+              <%= User.human_attribute_name('contract_company_country') %>
+            </div>
+          </th>
+          <td class="px-4 py-3">
+            <div class="flex items-center text-sm">
+              <p class="text-sm text-gray-600 dark:text-gray-400 font-semibold">
+                <%= t("user.contract_company_country.#{user.contract_company_country}") %>
+              </p>
+            </div>
+          </td>
+        </tr>
+        <tr class="text-gray-700 dark:text-gray-300">
+          <th class="w-1/12">
+            <div class="ml-3 text-xs font-semibold tracking-wide text-left text-gray-500 uppercase">
+              <%= User.human_attribute_name('roles') %>
+            </div>
+          </th>
+          <td class="px-4 py-3">
+            <div class="flex items-center text-sm">
+              <p class="text-sm text-gray-600 dark:text-gray-400 font-semibold">
+                <%= user.roles_sentence %>
+              </p>
+            </div>
+          </td>
+        </tr>
+        <tr class="text-gray-700 dark:text-gray-300">
+          <th class="w-1/12">
+            <div class="ml-3 text-xs font-semibold tracking-wide text-left text-gray-500 uppercase">
+              <%= User.human_attribute_name('skills') %>
+            </div>
+          </th>
+          <td class="px-4 py-3">
+            <div class="flex items-center text-sm">
+              <p class="text-sm text-gray-600 dark:text-gray-400 font-semibold">
+                <%= user.skills %>
+              </p>
+            </div>
+          </td>
+        </tr>
+        <tr class="text-gray-700 dark:text-gray-300">
+          <th class="w-1/12">
+            <div class="ml-3 text-xs font-semibold tracking-wide text-left text-gray-500 uppercase">
+              <%= User.human_attribute_name('mentor') %>
+            </div>
+          </th>
+          <td class="px-4 py-3">
+            <% if user.mentor %>
+              <div class="flex items-center text-sm">
+                <p class="text-sm text-gray-600 dark:text-gray-400">
+                  <%= link_to new_admin_admin_user_path(user.mentor_id), class: "inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 underline hover:no-underline dark:hover:text-gray-200" do %>
+                    <%= user.mentor_name  %>
+                  <% end %>
+                </p>
+              </div>
+            <% else %>
+              <div class="flex items-center text-sm">
+                <p class="text-sm text-gray-600 dark:text-gray-400 font-semibold">
+                  <%= user.mentor_name %>
+                </p>
+              </div>
+            <% end %>
+          </td>
+        </tr>
+        <tr class="text-gray-700 dark:text-gray-300">
+          <th class="w-1/12">
+            <div class="ml-3 text-xs font-semibold tracking-wide text-left text-gray-500 uppercase">
+              <%= User.human_attribute_name('allow_overtime') %>
+            </div>
+          </th>
+          <td class="px-4 py-3">
+            <div class="flex items-center text-sm">
+              <p class="text-sm text-gray-600 dark:text-gray-400 font-semibold">
+                <%= user.allow_overtime %>
+              </p>
+            </div>
+          </td>
+        </tr>
+        <tr class="text-gray-700 dark:text-gray-300">
+          <th class="w-1/12">
+            <div class="ml-3 text-xs font-semibold tracking-wide text-left text-gray-500 uppercase">
+              <%= User.human_attribute_name('active') %>
+            </div>
+          </th>
+          <td class="px-4 py-3">
+            <div class="flex items-center text-sm">
+              <p class="text-sm text-gray-600 dark:text-gray-400 font-semibold">
+                <%= user.active %>
+              </p>
+            </div>
+          </td>
+        </tr>
+        <tr class="text-gray-700 dark:text-gray-300">
+          <th class="w-1/12">
+            <div class="ml-3 text-xs font-semibold tracking-wide text-left text-gray-500 uppercase">
+              <%= User.human_attribute_name('started_at') %>
+            </div>
+          </th>
+          <td class="px-4 py-3">
+            <div class="flex items-center text-sm">
+              <p class="text-sm text-gray-600 dark:text-gray-400 font-semibold">
+                <%= user.started_at %>
+              </p>
+            </div>
+          </td>
+        </tr>
+        <tr class="text-gray-700 dark:text-gray-300">
+          <th class="w-1/12">
+            <div class="ml-3 text-xs font-semibold tracking-wide text-left text-gray-500 uppercase">
+              <%= User.human_attribute_name('created_at') %>
+            </div>
+          </th>
+          <td class="px-4 py-3">
+            <div class="flex items-center text-sm">
+              <p class="text-sm text-gray-600 dark:text-gray-400 font-semibold">
+                <%= l(user.created_at, format: :date_time) %>
+              </p>
+            </div>
+          </td>
+        </tr>
+        <tr class="text-gray-700 dark:text-gray-300">
+          <th class="w-1/12">
+            <div class="ml-3 text-xs font-semibold tracking-wide text-left text-gray-500 uppercase">
+              <%= User.human_attribute_name('updated_at') %>
+            </div>
+          </th>
+          <td class="px-4 py-3">
+            <div class="flex items-center text-sm">
+              <p class="text-sm text-gray-600 dark:text-gray-400 font-semibold">
+                <%= l(user.updated_at, format: :date_time) %>
+              </p>
+            </div>
+          </td>
+        </tr>
+        <tr class="text-gray-700 dark:text-gray-300">
+          <th class="w-1/12">
+            <div class="ml-3 text-xs font-semibold tracking-wide text-left text-gray-500 uppercase">
+              <%= User.human_attribute_name('observation') %>
+            </div>
+          </th>
+          <td class="px-4 py-3">
+            <div class="flex items-center text-sm">
+              <p class="text-sm text-gray-600 dark:text-gray-400 font-semibold">
+                <%= user.observation %>
+              </p>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>

spec/controllers/new_admin/users_controller_spec.rb
@@ -1,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe NewAdmin::UsersController do
+  render_views
+
+  let(:user) { create(:user, name: "Jorge").decorate }
+  let(:page) { Capybara::Node::Simple.new(response.body) }
+
+  before do
+    allow(controller).to receive(:authenticate_user!)
+    allow(controller).to receive(:current_user).and_return(user)
+  end
+
+  describe 'GET #show' do
+    let!(:allocation) { create(:allocation,
+                                start_at: 2.months.after,
+                                end_at: 3.months.after,
+                                user: user,
+                                ongoing: true) }
+
+
+    it 'returns successful status' do
+      get :show, params: { id: user.id }
+
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'return the users information' do
+      get :show, params: { id: user.id }
+
+      expect(page).to have_content(user.name)
+                  .and have_content(user.email)
+                  .and have_content(user.otp_required_for_login)
+                  .and have_content(user.github)
+                  .and have_content(user.office_city)
+  end
+end

app/controllers/new_admin/users_controller.rb
@@ -1,0 +1,11 @@
+# frozen_string_literal: true
+
+module NewAdmin
+  class UsersController < ApplicationController
+    layout "new_admin"
+
+    def show
+      @user = User.find(params[:id]).decorate
+    end
+  end
+end

app/views/new_admin/allocation_chart/_allocations.html.erb
@@ -14,11 +14,13 @@
     <% allocations.each do |allocation| %>
       <tr class="text-gray-700 dark:text-gray-400">
         <td class="px-4 py-3">
-          <div class="flex items-center text-sm">
-            <p class="text-sm text-gray-600 dark:text-gray-400 font-semibold">
-              <%= allocation.user.first_and_last_name %>
-            </p>
-          </div>
+          <%= link_to new_admin_admin_user_path(allocation.user), class: "inline-flex items-center w-full text-sm font-semibold transition-colors duration-150" do %>
+            <div class="flex items-center text-sm underline hover:text-white">
+              <p class="text-sm text-gray-600 dark:text-gray-400 dark:hover:text-gray-200">
+                <%= allocation.user.first_and_last_name %>
+              </p>
+            </div>
+          <% end %>
         </td>
         <td class="px-4 py-3">
           <div class="flex items-center text-sm">

app/views/new_admin/users/show.html.erb
@@ -1,0 +1,17 @@
+<% content_for :page_title do %>
+  <%= @user.first_and_last_name %>
+<% end %>
+
+<div>
+  <button class="px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 border border-transparent rounded-t-lg
+  bg-primary-600 active:bg-primary-600 hover:bg-primary-700 focus:outline-none focus:ring" data-tab-id="user_details"><%= Allocation.human_attribute_name('user') %></button>
+<div>
+<div class="w-full overflow-hidden rounded-lg ring-1 ring-black ring-opacity-5">
+  <div class="w-full overflow-x-auto">
+    <div data-tab-content>
+      <div id="user_details">
+        <%= render 'details', user: @user %>
+      </div>
+    </div>
+  </div>
+</div>

config/locales/pt-BR/pt-BR.yml
@@ -25,6 +25,9 @@ pt-BR:
       short: "%m/%Y"
       time: "%H:%M"
       month_name: '%B'
+  time:
+    formats:
+      date_time: "%d/%m/%Y %H:%M"
   english_evaluations: 'Avaliações de Inglês'
   hour_report_curent_month: 'Report de Horas do mês atual'
   hour_report_past_month: 'Report de Horas do mês anterior'
@@ -33,6 +36,7 @@ pt-BR:
   allocated: 'Alocado'
   projects: 'Projetos'
   project_details: 'Detalhes do Projeto'
+  user_details: 'Detalhes do Usuário'
   punch_details: 'Detalhes do Punch'
   perfomance_evaluations: 'Avaliações de Desempenho'
   allocation: 'Alocação'
@@ -155,10 +159,27 @@ pt-BR:
   send_reset_instructions: "Enviar as instruções para resetar a senha"
   didnt_receive_instructions: "Não recebeu as instruções de confirmação?"
   resend_instructions: "Reenviar instruções de confirmação"
-  user: "Usuário"
   project: "Projeto"
   allocation_details: "Detalhes da Alocação"
   working_hours: "Horas Trabalhadas"
   forecast: "Previsão"
+  user:
+    occupation:
+      administrative: Administrativo
+      engineer: Engenheiro
+    contract_type:
+      internship: Estagiário
+      employee: Funcionário
+      contractor: PJ
+      associate: Associado
+    contract_company_country:
+      brazil: Brasil
+      usa: Estados Unidos
+    role:
+      evaluator: Avaliador
+      admin: Admin
+      open_source_manager: Responsável OpenSource
+      hr: Recursos Humanos
+      commercial: Comercial
   edit_allocation: "Editar Alocação"
   details: "Detalhes"

config/routes.rb
@@ -27,7 +27,8 @@ Rails.application.routes.draw do
   namespace :new_admin do
     resources :mentoring, only: :index
     resources :allocation_chart, only: :index
-    resources :allocations, only: %i[new create show update edit], as: :user
+    resources :allocations, only: %i[new create show update edit], as: :user_allocations
+    resources :users, only: :show, as: :admin_user
     resources :punches, only: :show, as: :user_punches
   end
 

commit ade263b00cf2603b66d47791f7493a43acf000e9

Fix regressions in new admin pages (#468)
* fix regressions introduced by (#388)

* fix regressions introduced by (#402)

app/admin/user.rb
@@ -142,7 +142,7 @@ ActiveAdmin.register User do
         end
       end
 
-      tab I18n.t('perfomance_evaluations') do
+      tab I18n.t('performance_evaluations') do
         attributes_table do
           row :evaluation do
             table_for user.evaluations.by_kind(:performance).order(created_at: :desc) do

app/decorators/user_decorator.rb
@@ -81,25 +81,7 @@ class UserDecorator < Draper::Decorator
     model.mentor.first_and_last_name
   end
 
-  def allow_overtime
-    I18n.t(model.allow_overtime) || 'N/A'
-  end
-
-  def active
-    I18n.t(model.active) || 'N/A'
-  end
-
-  def started_at
-    return if model.started_at.nil?
-    I18n.l(user.started_at)
-  end
-
   def office_city
     model.office.city
-  end
-
-  def otp_required_for_login
-    return I18n.t('false') if model.otp_required_for_login.nil?
-    I18n.t(model.otp_required_for_login)
   end
 end

app/views/new_admin/users/_details.html.erb
@@ -45,7 +45,7 @@
           <td class="px-4 py-3">
             <div class="flex items-center text-sm">
               <p class="text-sm text-gray-600 dark:text-gray-400 font-semibold">
-                <%= user.otp_required_for_login %>
+                <%= t(user.otp_required_for_login?) %>
               </p>
             </div>
           </td>
@@ -279,7 +279,7 @@
           <td class="px-4 py-3">
             <div class="flex items-center text-sm">
               <p class="text-sm text-gray-600 dark:text-gray-400 font-semibold">
-                <%= user.allow_overtime %>
+                <%= t(user.allow_overtime?) %>
               </p>
             </div>
           </td>
@@ -293,7 +293,7 @@
           <td class="px-4 py-3">
             <div class="flex items-center text-sm">
               <p class="text-sm text-gray-600 dark:text-gray-400 font-semibold">
-                <%= user.active %>
+                <%= t(user.active?) %>
               </p>
             </div>
           </td>
@@ -307,7 +307,7 @@
           <td class="px-4 py-3">
             <div class="flex items-center text-sm">
               <p class="text-sm text-gray-600 dark:text-gray-400 font-semibold">
-                <%= user.started_at %>
+                <%= l(user.started_at) if user.started_at.present? %>
               </p>
             </div>
           </td>

spec/controllers/new_admin/users_controller_spec.rb
@@ -32,7 +32,7 @@ describe NewAdmin::UsersController do
 
       expect(page).to have_content(user.name)
                   .and have_content(user.email)
-                  .and have_content(user.otp_required_for_login)
+                  .and have_content(I18n.t(user.otp_required_for_login?))
                   .and have_content(user.github)
                   .and have_content(user.office_city)
     end