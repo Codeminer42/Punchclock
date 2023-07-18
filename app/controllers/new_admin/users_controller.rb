b244d2b9a32af16db45576eeb369f13536094a81
Daniel M Brasil <danielmbrasil@protonmail.com>
2023-07-18 17:33:16 -0300

    require authentication and authorization for new_admin/users/:id

--- app/controllers/new_admin/users_controller.rb
+++ app/controllers/new_admin/users_controller.rb
@@ -2,18 +2,49 @@
 
 module NewAdmin
   class UsersController < ApplicationController
-    layout "new_admin"
+    layout 'new_admin'
+    before_action :load_user_data, only: :show
+
+    before_action :authenticate_user!
 
     def show
-      @user = User.find(params[:id]).decorate
-      @user_allocations = Allocation.where(user_id: params[:id]).decorate
-      @performance_evaluations = Evaluation.joins(:questionnaire).merge(Questionnaire.performance).where(evaluated_id: params[:id]).order(created_at: :desc).decorate
-      @english_evaluations = Evaluation.joins(:questionnaire).merge(Questionnaire.english).where(evaluated_id: params[:id]).order(created_at: :desc).decorate
-      if params[:from].nil? and params[:to].nil?
-        @punches = Punch.where(user_id: params[:id]).decorate
+      @punches = filter_punches_by_date(params[:id], params[:from], params[:to])
+      AbilityAdmin.new(current_user).authorize! :read, Punch
+    end
+
+    private
+
+    def load_user_data
+      @user = find_user(params[:id])
+      @user_allocations = find_user_allocations(params[:id])
+      @performance_evaluations = find_performance_evaluations(params[:id])
+      @english_evaluations = find_english_evaluations(params[:id])
+    end
+
+    def find_user(id)
+      User.find(id).decorate
+    end
+
+    def find_user_allocations(id)
+      Allocation.where(user_id: id).decorate
+    end
+
+    def find_performance_evaluations(id)
+      Evaluation.joins(:questionnaire).merge(Questionnaire.performance)
+                .where(evaluated_id: id).order(created_at: :desc).decorate
+    end
+
+    def find_english_evaluations(id)
+      Evaluation.joins(:questionnaire).merge(Questionnaire.english)
+                .where(evaluated_id: id).order(created_at: :desc).decorate
+    end
+
+    def filter_punches_by_date(id, from, to)
+      if from.present? && to.present?
+        Punch.filter_by_date(id, from, to).decorate
       else
-        @punches = Punch.filter_by_date(params[:id], params[:from], params[:to]).decorate
-      end 
+        Punch.where(user_id: id).decorate
+      end
     end
   end
 end
--- spec/controllers/new_admin/users_controller_spec.rb
+++ spec/controllers/new_admin/users_controller_spec.rb
@@ -5,36 +5,58 @@ require 'rails_helper'
 describe NewAdmin::UsersController do
   render_views
 
-  let(:user) { create(:user, name: "Jorge").decorate }
   let(:page) { Capybara::Node::Simple.new(response.body) }
 
-  before do
-    allow(controller).to receive(:authenticate_user!)
-    allow(controller).to receive(:current_user).and_return(user)
-  end
-
   describe 'GET #show' do
-    let!(:allocation) { create(:allocation,
-                                start_at: 2.months.after,
-                                end_at: 3.months.after,
-                                user: user,
-                                ongoing: true) }
-
-
-    it 'returns successful status' do
-      get :show, params: { id: user.id }
-
-      expect(response).to have_http_status(:ok)
+    let(:user) { create(:user, name: 'Jorge').decorate }
+    let!(:allocation) do
+      create(:allocation,
+             start_at: 2.months.after,
+             end_at: 3.months.after,
+             user: user,
+             ongoing: true)
     end
 
-    it 'return the users information' do
-      get :show, params: { id: user.id }
+    context 'when the user is signed in' do
+      before do
+        sign_in(user)
+      end
 
-      expect(page).to have_content(user.name)
-                  .and have_content(user.email)
-                  .and have_content(I18n.t(user.otp_required_for_login?))
-                  .and have_content(user.github)
-                  .and have_content(user.office_city)
+      context 'when the user is an admin' do
+        let(:user) { create(:user, :admin, name: 'Jorge').decorate }
+
+        it 'returns a successful status' do
+          get :show, params: { id: user.id }
+
+          expect(response).to have_http_status(:ok)
+        end
+
+        it 'return the users information' do
+          get :show, params: { id: user.id }
+
+          expect(page).to have_content(user.name)
+                      .and have_content(user.email)
+                      .and have_content(I18n.t(user.otp_required_for_login?))
+                      .and have_content(user.github)
+                      .and have_content(user.office_city)
+        end
+      end
+
+      context 'when the user is not an admin' do
+        it 'redirects to the root path' do
+          get :show, params: { id: user.id }
+
+          expect(response).to redirect_to(root_path)
+        end
+      end
+    end
+
+    context 'when the user is not signed in' do
+      it 'redirects to the sign in path' do
+        get :show, params: { id: user.id }
+
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
   end
 end
