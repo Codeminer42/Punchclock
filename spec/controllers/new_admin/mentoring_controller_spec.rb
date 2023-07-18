a7e0125dbd77b40f33869ffd17746e58d2194335
Daniel M Brasil <danielmbrasil@protonmail.com>
2023-07-18 15:54:52 -0300

    add specs for NewAdmin::MentoringController

---
+++ spec/controllers/new_admin/mentoring_controller_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+RSpec.describe NewAdmin::MentoringController do
+  describe 'GET #index' do
+    context 'when the user is signed in' do
+      before do
+        sign_in(user)
+      end
+
+      context 'when the user is an admin' do
+        let(:user)    { create(:user, :admin) }
+        let!(:mentor) { create(:user) }
+        let!(:mentee) { create(:user, mentor: mentor) }
+
+        it 'renders index template' do
+          get :index
+
+          expect(response).to render_template(:index)
+        end
+      end
+
+      context 'when the user is not an admin' do
+        let(:user) { create(:user) }
+
+        it 'redirects to the root path' do
+          get :index
+
+          expect(response).to redirect_to(root_path)
+        end
+      end
+    end
+
+    context 'when the user is not signed in' do
+      it 'redirects to the sign in path' do
+        get :index
+
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+end
