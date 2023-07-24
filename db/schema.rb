df1ada029ab7356b0dd6e07685b16b1a583c3a02
thiagofportella <thiagofportella@gmail.com>
2023-07-24 13:57:13 -0300

    feat: allow description in repositories for admins to register

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_14_130703) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_24_160449) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -188,6 +188,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_14_130703) do
     t.datetime "updated_at", precision: nil, null: false
     t.string "language"
     t.boolean "highlight", default: false
+    t.text "description"
   end
 
   create_table "skills", force: :cascade do |t|
--- spec/factories/repositories.rb
+++ spec/factories/repositories.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :repository do
-    sequence :link do |n| 
+    sequence :link do |n|
       "https://github.com/Codeminer42/project-#{Faker::Number.number(digits: 2)}#{n}"
     end
     language { Faker::ProgrammingLanguage.name }
+    description { Faker::Movies::HarryPotter.quote }
   end
 end
