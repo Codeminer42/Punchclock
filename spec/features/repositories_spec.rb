d624d45c97441b707adfe3c6f61f019959968a0c
Thiago Portella <52546576+thiagofportella@users.noreply.github.com>
2023-07-26 15:12:32 -0300

    feat: Add repository description (#490)
    * feat: allow description in repositories for admins to register
    
    * feat: display repository description in index
    
    ---------
    
    Co-authored-by: Alessandro Dias Batista <alessandro.dias@codeminer42.com>

--- spec/features/repositories_spec.rb
+++ spec/features/repositories_spec.rb
@@ -4,37 +4,41 @@ describe 'Repositories list', type: :feature do
   let!(:authed_user) { create_logged_in_user }
 
   context 'without filter' do
-    
     let!(:repository) { create(:repository).decorate }
     let!(:second_repository) { create(:repository).decorate }
-  
+
     before do
       visit repositories_path
     end
-  
+
     it 'has the right count of 25 repositories' do
       create_list(:repository, 26)
-  
+
       visit repositories_path
 
       expect(page).to have_css('.list-group .list-group-item', :count => 25)
     end
-  
+
     it 'have all links for repositories' do
       expect(page).to have_link(repository.link, href: repository.link) &
                       have_link(second_repository.link, href: second_repository.link)
     end
-  
+
     it 'have all languages for repositories' do
       expect(page).to have_text(repository.languages) &
                       have_text(second_repository.languages)
+    end
+
+    it 'has all descriptions for repositories' do
+      expect(page).to have_text(repository.description) &
+                      have_text(second_repository.description)
     end
   end
 
   context 'when filtering by languages' do
     let!(:repository) { create(:repository, language: 'javascript,ruby,python').decorate }
     let!(:second_repository) { create(:repository, language: 'Shell,react,python').decorate }
-  
+
     before do
       visit repositories_path
     end
@@ -42,7 +46,7 @@ describe 'Repositories list', type: :feature do
     it 'have the language javascript' do
       fill_in 'search-input-field', with: 'javascript'
       find('#filter-button').click
-      
+
       expect(page).to have_text(repository.languages)
       expect(page).not_to have_text(second_repository.languages)
     end
@@ -50,7 +54,7 @@ describe 'Repositories list', type: :feature do
     it 'not have any repository with Go' do
       fill_in 'search-input-field', with: 'go'
       find('#filter-button').click
-      
+
       expect(page).not_to have_text(repository.languages)
       expect(page).not_to have_text(second_repository.languages)
     end
@@ -58,7 +62,7 @@ describe 'Repositories list', type: :feature do
     it 'have two repositories with python' do
       fill_in 'search-input-field', with: 'python'
       find('#filter-button').click
-      
+
       expect(page).to have_text(repository.languages) &
                       have_text(second_repository.languages)
       expect(page).to have_css('.list-group .list-group-item', :count => 2)
