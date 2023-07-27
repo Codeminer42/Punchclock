71aa8f3a84c6ff0ea9b8835ea01a85df6a636d26
thiagofportella <thiagofportella@gmail.com>
2023-07-27 17:12:55 -0300

    refactor: change collection decorator class to support pagination in evaluations decorator

--- app/decorators/evaluation_decorator.rb
+++ app/decorators/evaluation_decorator.rb
@@ -4,6 +4,10 @@ class EvaluationDecorator < Draper::Decorator
   delegate_all
   decorates_association :evaluation
 
+  def self.collection_decorator_class
+    PaginatingDecorator
+  end
+
   def created_at
     model.created_at.to_date.to_fs(:date)
   end
