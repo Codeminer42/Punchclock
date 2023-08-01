4be6526843e41d579749540f10148c69ee16e7ae
thiagofportella <thiagofportella@gmail.com>
2023-08-01 18:33:41 -0300

    feat: create query for projects

---
+++ spec/queries/new_admin/projects_query_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe NewAdmin::ProjectsQuery do
+  describe '#self.call' do
+    context 'when no filters is applied' do
+      let(:projects) { create_list(:project, 2) }
+
+      it 'retrieves projects' do
+        expect(described_class.call({})).to match_array(projects)
+      end
+    end
+
+    context 'when filters are applied' do
+      let(:active_projects) { create_list(:project, 2, :active) }
+      let(:inactive_projects) { create_list(:project, 2, :inactive) }
+
+      it 'retireve filtered projects' do
+        filters = { active: true }
+
+        expect(described_class.call(filters)).to match_array(active_projects)
+      end
+    end
+  end
+end
