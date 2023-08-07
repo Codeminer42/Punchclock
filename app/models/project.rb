3cd77287714b760ad677b1bf39e1a77df4d787ef
thiagofportella <thiagofportella@gmail.com>
2023-08-07 10:39:18 -0300

    refactor: move scopes from concern to model in project.rb

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,8 +3,6 @@
 class Project < ApplicationRecord
   extend Enumerize
 
-  include ::Filters::ProjectsFilter
-
   has_many :punches
   has_many :allocations, dependent: :destroy
 
@@ -15,6 +13,12 @@ class Project < ApplicationRecord
             uniqueness: true
 
   validates :market, presence: true
+
+  scope :active, -> { where(active: true) }
+  scope :inactive, -> { where(active: false) }
+  scope :by_market, ->(market) { where(market:) if market.present? }
+  scope :by_name_like, ->(name) { where("projects.name ILIKE ?", "%#{name}%") if name.present? }
+  scope :by_operation, ->(active) { where(active:) if active.present? }
 
   def disable!
     update!(active: false)
