88fe52443260db1017bf72971798d23b6b08b43f
thiagofportella <thiago.portella@codeminer42.com>
2023-09-18 10:18:13 -0300

    refactor: order professional experiences by start date in user admin

--- app/models/professional_experience.rb
+++ app/models/professional_experience.rb
@@ -9,6 +9,10 @@ class ProfessionalExperience < ApplicationRecord
   validate :greater_than_current_month?, if: -> { valid_date?(start_date) }
   validate :greater_than_end_date?, if: -> { valid_date?(start_date) && valid_date?(end_date) }
 
+  scope :ordered_by_start_date, lambda {
+    sort_by { |professional_experience| Date.strptime(professional_experience.start_date, '%m/%Y') }
+  }
+
   private
 
   FIELDS_TO_BE_VALIDATED = %i[start_date end_date].freeze
