41398a7c2823b6ad207a54d64dd8e5e365689ff2
NehamaMandelbaum <nehama1836@gmail.com>
2023-10-13 14:58:34 -0300

    query for retrieving filtered questionnaires

---
+++ spec/queries/new_admin/questionnaires_query_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe NewAdmin::QuestionnairesQuery do
+  describe '#self.call' do
+    context 'when no filters are applied' do
+      let(:questionnaires) { create_list(:questionnaire, 2) }
+
+      it 'retrieves the questionnaires' do
+        expect(described_class.call({})).to match_array(questionnaires)
+      end
+    end
+
+    context 'when filters are applied' do
+      let!(:active_questionnaires) { create_list(:questionnaire, 2) }
+      let!(:inactive_questionnaires) { create_list(:questionnaire, 2, active: false) }
+      let!(:english_questionnaires) { create_list(:questionnaire, 2, :kind_english) }
+
+      it 'retireve filtered projects' do
+        filters = { active: true, kind: 'english' }
+
+        expect(described_class.call(filters)).to match_array(english_questionnaires)
+      end
+    end
+  end
+end
