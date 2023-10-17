35a8ca09d06854dbfebc24905c6b43fff87eaa22
jonathanrib3 <jonathanrib.3@gmail.com>
2023-10-17 10:28:56 -0300

    add scopes to get repositories by languages, name search and get repositories with distinct languages

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -6,4 +6,13 @@ class Repository < ApplicationRecord
   def to_s
     link
   end
+
+  scope :languages, -> { select(:language).distinct.where.not(language: [nil, '']) }
+  scope :by_repository_name_like, lambda { |repository_name|
+    if repository_name.present?
+      where("link ILIKE ?", "%#{repository_name}%")
+        .or(where("link ILIKE ?", "%#{repository_name}"))
+    end
+  }
+  scope :by_languages, ->(languages) { where(language: languages) if languages.present? }
 end
