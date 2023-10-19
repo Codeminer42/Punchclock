0a6380ed419108d07b8ef314ee3a84512305dbde
Sergio Souza <sergiosouzauff@gmail.com>
2023-10-19 18:11:07 -0300

    create education experiences index action

--- app/controllers/education_experiences_controller.rb
+++ app/controllers/education_experiences_controller.rb
@@ -4,7 +4,11 @@ class EducationExperiencesController < ApplicationController
   before_action :authenticate_user!
 
   def index
+<<<<<<< HEAD
     @undecorated_experiences = EducationExperience.for_user(current_user.id)
     @education_experiences = EducationExperiencePaginationDecorator.new(params, @undecorated_experiences)
+=======
+    @education_experiences = PunchesPaginationDecorator.new(params, EducationExperience.where(user_id: current_user.id))
+>>>>>>> 1aaaab0b (create education experiences index action)
   end
 end
