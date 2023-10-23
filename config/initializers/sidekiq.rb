82b9a69a1223e96ead07cdfd9c845c8fa21603cd
NehamaMandelbaum <nehama1836@gmail.com>
2023-10-23 13:36:43 -0300

    fixing redis url in sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -9,14 +9,14 @@ end
 
 Sidekiq.configure_server do |config|
   config.redis = {
-    url: ENV[ENV["REDIS_PROVIDER"]],
+    url: ENV["REDIS_URL"],
     ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
   }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = {
-    url: ENV[ENV["REDIS_PROVIDER"]],
+    url: ENV["REDIS_URL"],
     ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
   }
 end
