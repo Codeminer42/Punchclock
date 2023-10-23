35799018b921a454fef78a3151738bf91535ae31
NehamaMandelbaum <nehama1836@gmail.com>
2023-10-23 14:20:04 -0300

    adding scope by_user for professional experiences

--- app/models/professional_experience.rb
+++ app/models/professional_experience.rb
@@ -13,6 +13,8 @@ class ProfessionalExperience < ApplicationRecord
     all.sort_by { |professional_experience| Date.strptime(professional_experience.start_date, '%m/%Y') }
   end
 
+  scope :by_user, ->(user_id) { where(user_id:) }
+
   private
 
   FIELDS_TO_BE_VALIDATED = %i[start_date end_date].freeze
@@ -20,13 +22,15 @@ class ProfessionalExperience < ApplicationRecord
   def greater_than_current_month?
     return if start_date.to_date < Date.today.end_of_month
 
-    errors.add(:start_date, I18n.t('activerecord.errors.models.professional_experience.attributes.start_date.greater_than_current_month'))
+    errors.add(:start_date,
+               I18n.t('activerecord.errors.models.professional_experience.attributes.start_date.greater_than_current_month'))
   end
 
   def greater_than_end_date?
     return if start_date.to_date < end_date.to_date
 
-    errors.add(:start_date, I18n.t('activerecord.errors.models.professional_experience.attributes.start_date.greater_than_end_date'))
+    errors.add(:start_date,
+               I18n.t('activerecord.errors.models.professional_experience.attributes.start_date.greater_than_end_date'))
   end
 
   def date_format
