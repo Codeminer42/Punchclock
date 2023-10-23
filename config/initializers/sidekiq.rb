d5c3e8fabeb81f6b2551c7a2c086f7c737549554
NehamaMandelbaum <nehama1836@gmail.com>
2023-10-23 12:22:28 -0300

    adding configs to sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,3 +6,17 @@ if Rails.env.production?
     username == ENV["SIDEKIQ_USERNAME"] && password == ENV["SIDEKIQ_PASSWORD"]
   end
 end
+
+Sidekiq.configure_server do |config|
+  config.redis = {
+    url: ENV[ENV["REDIS_PROVIDER"]],
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = {
+    url: ENV[ENV["REDIS_PROVIDER"]],
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
+end
