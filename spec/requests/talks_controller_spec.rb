3968d59979320451129785500d0c90d36ef88f7a
jonathanrib3 <jonathanrib.3@gmail.com>
2023-10-24 15:10:00 -0300

    fix: talks requests tests to use rails controller helpers

--- spec/requests/talks_controller_spec.rb
+++ spec/requests/talks_controller_spec.rb
@@ -3,29 +3,27 @@ require 'rails_helper'
 RSpec.describe TalksController, type: :request do
   describe 'GET index' do
     context 'when user is authenticated' do
-      let(:user) { create(:user) }
-      let(:talk_list) { create_list(:talk, user:) }
+      let!(:user) { create(:user) }
+      let!(:talk_list) { create_list(:talk, 2, user:) }
 
       before do
         sign_in user
-        get :index
       end
 
       it 'renders the index template' do
+        get talks_path
         expect(response).to render_template(:index)
       end
 
       it 'has http status 200' do
+        get talks_path
         expect(response).to have_http_status(:ok)
-      end
-
-      it 'renders current user talks' do 
       end
     end
 
     context 'when user is not authenticated' do
       before do
-        get :index
+        get talks_path
       end
 
       it 'redirects to sign in page' do
