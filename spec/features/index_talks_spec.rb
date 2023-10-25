d5516fd0dfc7c7968b9dfe2616b9cb54f08acb71
jonathanrib3 <jonathanrib.3@gmail.com>
2023-10-25 14:54:04 -0300

    refactor: talks index page feature tests
    refactors the spec that checks if a message is rendered when there are no
    talks for the current user to check also if that message is rendered when
    some other user has talks but the current user hasn't it.

--- spec/features/index_talks_spec.rb
+++ spec/features/index_talks_spec.rb
@@ -43,6 +43,8 @@ RSpec.describe 'Talks Index page', type: :feature do
 
     context 'when the user has no saved talk' do
       let!(:user) { create(:user) }
+      let!(:another_user) { create(:user) }
+      let!(:talks) { create_list(:talk, 2, user: another_user) }
 
       before do
         sign_in user
