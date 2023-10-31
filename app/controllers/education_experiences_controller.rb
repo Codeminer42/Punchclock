36056c5a17ad841c1e5e3caa771d1cfcecb0fa87
NehamaMandelbaum <nehama1836@gmail.com>
2023-10-31 16:28:38 -0300

    fixing conflicts

--- app/controllers/education_experiences_controller.rb
+++ app/controllers/education_experiences_controller.rb
@@ -4,12 +4,7 @@ class EducationExperiencesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-<<<<<<< HEAD
-    @undecorated_experiences = EducationExperience.for_user(current_user.id)
-    @education_experiences = EducationExperiencePaginationDecorator.new(params, @undecorated_experiences)
-=======
     @education_experiences = PunchesPaginationDecorator.new(params, EducationExperience.where(user_id: current_user.id))
->>>>>>> 1aaaab0b (create education experiences index action)
   end
 
   def new
