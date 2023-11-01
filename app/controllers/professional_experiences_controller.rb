9b2342fab51ed8f78a493178cc1b2adba69e330b
Nehama Mandelbaum <75209689+nehamamandelbaum@users.noreply.github.com>
2023-11-01 14:47:50 -0300

    Implementing professional experience create (#590)
    * adding create professional experience
    
    * refactoring flash messages and specs
    
    * correcting failing tests
    
    * refactoring create pro experiences
    
    * small fixes

--- app/controllers/professional_experiences_controller.rb
+++ app/controllers/professional_experiences_controller.rb
@@ -4,4 +4,36 @@ class ProfessionalExperiencesController < ApplicationController
   def index
     @professional_experiences = current_user.professional_experiences.page(params[:page]).per(params[:per])
   end
+
+  def new
+    @professional_experience = ProfessionalExperience.new
+  end
+
+  def create
+    @professional_experience = current_user.professional_experiences.new(professional_experience_params)
+
+    if @professional_experience.save
+      redirect_to professional_experiences_path,
+                  notice: I18n.t(:notice, scope: "flash.actions.create",
+                                          resource_name: ProfessionalExperience.model_name.human)
+    else
+      flash_errors('create', ProfessionalExperience.model_name.human, error_message)
+      render :new
+    end
+  end
+
+  private
+
+  def professional_experience_params
+    params.require(:professional_experience).permit(:company, :position, :description, :responsabilities, :start_date,
+                                                    :end_date)
+  end
+
+  def errors
+    @professional_experience.errors.full_messages.join('. ')
+  end
+
+  def error_message
+    I18n.t(:errors, scope: "flash", errors:)
+  end
 end
--- spec/requests/professional_experiences_spec.rb
+++ spec/requests/professional_experiences_spec.rb
@@ -42,4 +42,68 @@ RSpec.describe ProfessionalExperience, type: :request do
       end
     end
   end
+
+  describe 'GET #new' do
+    context 'when user is signed in' do
+      let(:user) { create(:user) }
+      before { sign_in user }
+
+      it 'renders the new template' do
+        get new_professional_experience_path
+
+        expect(response).to render_template(:new)
+      end
+    end
+
+    context 'when user is not signed in' do
+      it 'redirects to sign in page' do
+        get new_professional_experience_path
+
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+
+  describe 'POST #create' do
+    context 'when user is signed in' do
+      let(:user) { create(:user) }
+      before { sign_in user }
+
+      context 'with valid params' do
+        let(:valid_params) do
+          { professional_experience: attributes_for(:professional_experience) }
+        end
+
+        it 'creates a new professional experience' do
+          expect { post professional_experiences_path, params: valid_params }.to change(ProfessionalExperience, :count).by(1)
+        end
+
+        it 'attaches the professional experience to the signed in user' do
+          post professional_experiences_path, params: valid_params
+
+          expect(ProfessionalExperience.last.user).to eq(user)
+        end
+
+        it 'redirects to professional_experiences_path' do
+          post professional_experiences_path, params: valid_params
+
+          expect(response).to redirect_to(professional_experiences_path)
+        end
+      end
+
+      context 'with invalid params' do
+        let(:invalid_params) { { professional_experience: { company: '', position: '' } } }
+
+        it 'does not create the student' do
+          expect { post professional_experiences_path, params: invalid_params }.not_to change(ProfessionalExperience, :count)
+        end
+
+        it 'renders the new template' do
+          post professional_experiences_path, params: { professional_experience: invalid_params }
+
+          expect(response).to render_template(:new)
+        end
+      end
+    end
+  end
 end
