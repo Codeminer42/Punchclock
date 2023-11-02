77955dfe5a1edc0b9b39feb75b98299c69a94708
Nehama Mandelbaum <75209689+nehamamandelbaum@users.noreply.github.com>
2023-11-01 21:00:59 -0300

    Implementing show for talks (#600)
    * add talks controller and index route
    
    * feat: add talks index page
    
    * refactor: adjust colspan from 3 to 5 on talks index view's table
    
    * fix: talks requests tests to use rails controller helpers
    
    * refactor: scoped_talks to private on talks controller
    
    * refactor: talks index page feature tests
    
    refactors the spec that checks if a message is rendered when there are no
    talks for the current user to check also if that message is rendered when
    some other user has talks but the current user hasn't it.
    
    * refactor: remove scoped talks from talks controller
    
    * implementing show for talks
    
    ---------
    
    Co-authored-by: jonathanrib3 <jonathanrib.3@gmail.com>
    Co-authored-by: Alessandro Dias Batista <alessandro.dias@codeminer42.com>

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -1,7 +1,15 @@
 class TalksController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound do
+    redirect_to '/404'
+  end
+  
   before_action :authenticate_user!
 
   def index
     @talks = current_user.talks.page(params[:page])
+  end
+
+  def show
+    @talk = current_user.talks.find(params[:id])
   end
 end
