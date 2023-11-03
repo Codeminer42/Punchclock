b9f7a9d12b3f2a7b85fa6e21cb313f6af5814161
Sergio Souza <80131514+sergioosouzaa@users.noreply.github.com>
2023-11-03 12:08:16 -0300

    Feat/education experience create (#585)
    * create education experiences index action
    
    * create tests
    
    * create pagination
    
    * fix index html
    
    * fix index view
    
    * include test for coverage
    
    * fix file name
    
    * create education experiences index action
    
    * create new/create endpoints
    
    * create: feature test for creation
    
    * improve feature tests
    
    * fix new view
    
    * fix pr comments
    
    * fixing conflicts
    
    * small changes and fixes in controller, tests
    
    * removing unused decorator
    
    ---------
    
    Co-authored-by: Sergio Souza <sergio.souza@codeminer42.com>
    Co-authored-by: NehamaMandelbaum <nehama1836@gmail.com>

---
+++ spec/controllers/education_experiences_controller.rb
@@ -0,0 +1,76 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe EducationExperiencesController, type: :controller do
+  render_views
+
+  describe 'GET #index' do
+    let(:user) { create(:user) }
+
+    let(:page) { Capybara::Node::Simple.new(response.body) }
+
+    context 'when the user is logged in' do
+      before do
+        sign_in user
+      end
+
+      it 'renders the index template' do
+        get :index
+
+        expect(response).to render_template(:index)
+      end
+
+      it 'has status 200' do
+        get :index
+
+        expect(response).to have_http_status(:ok)
+      end
+
+      context 'when the user does not contain education experiences' do
+        it 'renders the table with not found message' do
+          get :index
+
+          expect(page.find('table')).to have_content('Nenhuma experiência educacional cadastrada')
+        end
+      end
+
+      context 'when the user has educatione experiences' do
+        let!(:education_experiences) { create_list(:education_experience, 2, user_id: user.id) }
+
+        it 'renders the table with a list of contributions' do
+          get :index
+
+          expect(page.find('table')).to have_content(education_experiences[0].institution)
+                                    .and have_content(education_experiences[0].course)
+                                    .and have_content(education_experiences[0].start_date.strftime('%d/%m/%Y'))
+                                    .and have_content(education_experiences[0].end_date.strftime('%d/%m/%Y'))
+                                    .and have_content(education_experiences[1].institution)
+                                    .and have_content(education_experiences[1].course)
+                                    .and have_content(education_experiences[1].start_date.strftime('%d/%m/%Y'))
+                                    .and have_content(education_experiences[1].end_date.strftime('%d/%m/%Y'))
+        end
+
+        it 'paginates results' do
+          get :index, params: { per: 2, page: 1 }
+
+          expect(assigns(:education_experiences).count).to eq(2)
+        end
+
+        it 'decorates regional holidays' do
+          get :index, params: { per: 1 }
+
+          expect(assigns(:education_experiences).last).to be_an_instance_of(EducationExperienceDecorator)
+        end
+      end
+    end
+
+    context 'when the user is not logged in' do
+      it 'redirects the user to the sign in page' do
+        get :index
+
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+end
