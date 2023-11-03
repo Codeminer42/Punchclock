041bc86fbbffa2fb274d81eed6c5cf43df9a0097
NehamaMandelbaum <nehama1836@gmail.com>
2023-11-03 13:56:52 -0300

    fixes, changes and refactors

--- app/models/education_experience.rb
+++ app/models/education_experience.rb
@@ -14,6 +14,4 @@ class EducationExperience < ApplicationRecord
                           message: :less_than_start_date,
                           greater_than: ->(education_experience) { education_experience.start_date },
                           allow_nil: true
-
-  scope :for_user, ->(user_id) { where(user_id:) }
 end
