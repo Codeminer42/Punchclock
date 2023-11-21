107f884cae23584abbb9744df9ada133475b2909
NehamaMandelbaum <nehama1836@gmail.com>
2023-11-21 13:05:11 -0300

    adding spreadsheet specs and translation

--- app/spreadsheets/detailed_month_forecast_spreadsheet.rb
+++ app/spreadsheets/detailed_month_forecast_spreadsheet.rb
@@ -13,13 +13,13 @@ class DetailedMonthForecastSpreadsheet < DefaultSpreadsheet
 
   def header
     [
-      'Usuário',
-      'Projeto/cliente',
-      'Taxa horária',
-      'Data de início',
-      'Data de término',
-      'Horas trabalhadas',
-      'Faturamento total'
+      Allocation.human_attribute_name('user'),
+      Allocation.human_attribute_name('project'),
+      Allocation.human_attribute_name('hourly_rate'),
+      Allocation.human_attribute_name('start_at'),
+      Allocation.human_attribute_name('end_at'),
+      I18n.t('allocation.worked_hours'),
+      I18n.t('allocation.total_revenue')
     ]
   end
 end
