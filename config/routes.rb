72e5a6fcce8a6bc0ac72f01d382a4ba42006572a
EduardoSCosta <eduardo.araujo.codeminer@crmbonus.com>
2023-12-01 09:09:36 -0300

    create skills controller

--- config/routes.rb
+++ config/routes.rb
@@ -50,6 +50,7 @@ Rails.application.routes.draw do
     resources :questionnaires, only: :update, as: :update_questionnaire
     resources :questionnaires, only: :destroy, as: :destroy_questionnaire
     resources :repositories, only: %i[index]
+    resources :skills
 
     namespace :projects do
       resources :allocate_users, only: %i[new create]
