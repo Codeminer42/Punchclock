1402221c631d511099bcd025416bc1a53d737d66
Eduardo Souza Costa Araújo <eduardoscaraujo@gmail.com>
2023-12-05 16:42:27 -0300

    Feat: Index page for users in new admin (#632)
    * create user scopes
    
    * create users query
    
    * create users index in new admin
    
    * update new_admin sidebar
    
    * update user section in the new admin sidebar
    
    * add params in new admin users controller
    
    * update users query
    
    * update user controller spec
    
    * update user i18n
    
    * update params names
    
    * add hours report button to user page in new admin
    
    ---------
    
    Co-authored-by: EduardoSCosta <eduardo.araujo.codeminer@crmbonus.com>

--- config/routes.rb
+++ config/routes.rb
@@ -34,7 +34,7 @@ Rails.application.routes.draw do
     resources :evaluations, only: %i[index show]
     resources :allocation_charts, only: :index
     resources :allocations, only: %i[show update edit], as: :user_allocations
-    resources :users, only: %i[show update edit], as: :admin_user
+    resources :users, only: %i[index show update edit], as: :admin_user
     resources :punches, only: :show, as: :user_punches
     resources :regional_holidays, except: %i[show update destroy]
     resources :regional_holidays, only: :show, as: :show_regional_holiday
