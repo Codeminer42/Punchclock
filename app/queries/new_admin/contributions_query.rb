afef281ea8961fbf82812a7ab6e30e0059ac121c
NehamaMandelbaum <nehama1836@gmail.com>
2023-12-06 10:10:59 -0300

    creating query object for contributions

---
+++ app/queries/new_admin/contributions_query.rb
@@ -0,0 +1,25 @@
+module NewAdmin
+  class ContributionsQuery
+    def initialize(filters)
+      self.filters = filters
+    end
+
+    def self.call(*args)
+      new(*args).call
+    end
+
+    def call
+      Contribution.order(created_at: :asc)
+                  .by_user(filters[:user_id])
+                  .by_state(filters[:state])
+                  .by_reviewed_at_from(filters[:by_reviewed_at_from])
+                  .by_reviewed_at_until(filters[:by_reviewed_at_until])
+                  .by_created_at_from(filters[:created_at_from])
+                  .by_created_at_until(filters[:created_at_until])
+    end
+
+    private
+
+    attr_accessor :filters
+  end
+end
---
+++ spec/queries/new_admin/contributions_query_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe NewAdmin::ContributionsQuery do
+  describe '#self.call' do
+    context 'when no filters are applied' do
+      let(:contributions) { create_list(:contribution, 2) }
+
+      it 'retrieves the contributions' do
+        expect(described_class.call({})).to match_array(contributions)
+      end
+    end
+
+    context 'when filters are applied' do
+      let!(:approved_contributions) { create_list(:contribution, 2, :approved) }
+      let!(:user) { create(:user) }
+      let!(:user_contribution) { create(:contribution, :approved) }
+
+      it 'retrieves filtered projects' do
+        user_contribution.users << user
+        filters = { user_id: user.id, state: 'approved' }
+
+        expect(described_class.call(filters)).to contain_exactly(user_contribution)
+      end
+    end
+  end
+end
