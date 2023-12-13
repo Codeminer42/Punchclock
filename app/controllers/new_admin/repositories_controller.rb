78c6fcd7de7fa98a6b2b4418c049ee8da2e08250
Eduardo Souza Costa Araújo <eduardoscaraujo@gmail.com>
2023-12-13 10:22:40 -0300

    Feat: New repository page in new admin (#614)
    * add new admin repositories index route and controller
    
    * refactor: adjust PR#560 review points
    
    * update repositories controller
    
    * create new repository view
    
    * add new and create methods to repositories controller
    
    * update repository feature spec
    
    * fix new admin repository form
    
    * Feat: Edit repository new admin (#617)
    
    * create edit repository view
    
    * create edit and update methods in repositories controller
    
    * update new admin repositories i18n
    
    * Feat/delete repository new admin (#619)
    
    * add destroy method for repositories in the new admin
    
    * update delete buttons in repositories views in the new admin
    
    * update delete repository spec
    
    ---------
    
    Co-authored-by: EduardoSCosta <eduardo.araujo.codeminer@crmbonus.com>
    
    ---------
    
    Co-authored-by: EduardoSCosta <eduardo.araujo.codeminer@crmbonus.com>
    
    ---------
    
    Co-authored-by: jonathanrib3 <jonathanrib.3@gmail.com>
    Co-authored-by: EduardoSCosta <eduardo.araujo.codeminer@crmbonus.com>

--- app/controllers/new_admin/repositories_controller.rb
+++ app/controllers/new_admin/repositories_controller.rb
@@ -10,14 +10,77 @@ module NewAdmin
       @repository = Repository.find(params[:id]).decorate
     end
 
+    def new
+      @repository = Repository.new
+    end
+
+    def create
+      @repository = Repository.new(repository_params)
+
+      if @repository.save
+        redirect_on_success new_admin_repositories_path, message_scope: 'create'
+      else
+        render_on_failure :new
+      end
+    end
+
+    def edit
+      @repository = Repository.find(params[:id])
+    end
+
+    def update
+      @repository = Repository.find(params[:id])
+
+      @repository.attributes = repository_params
+
+      if @repository.save
+        redirect_on_success new_admin_show_repository_path(id: @repository.id), message_scope: 'update'
+      else
+        render_on_failure :edit
+      end
+    end
+
+    def destroy
+      @repository = Repository.find(params[:id])
+
+      if @repository.destroy
+        redirect_on_success new_admin_repositories_path, message_scope: "destroy"
+      else
+        render_on_failure :index
+      end
+    end
+
     private
 
     def repositories
       NewAdmin::RepositoryQuery.new(**filter_params).call
     end
 
+    def repository_params
+      params.require(:repository).permit(:link, :language, :description, :highlight)
+    end
+
     def filter_params
       params.permit(:repository_name_search, languages: []).to_h.symbolize_keys
+    end
+
+    def redirect_on_success(url, message_scope:)
+      flash[:notice] = I18n.t(:notice, scope: "flash.actions.#{message_scope}",
+                                       resource_name: Repository.model_name.human)
+      redirect_to url
+    end
+
+    def render_on_failure(template)
+      flash.now[:alert] = @repository.errors.full_messages.to_sentence
+      render template, status: :unprocessable_entity
+    end
+
+    def errors
+      @repository.errors.full_messages.join('. ')
+    end
+
+    def error_message
+      I18n.t(:errors, scope: :flash, errors:)
     end
   end
 end
