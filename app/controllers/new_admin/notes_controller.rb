0d6e64d000e54be0f0bc73b92c27d8b68ff95c47
Nehama Mandelbaum <75209689+nehamamandelbaum@users.noreply.github.com>
2023-12-21 11:05:57 -0300

    Implementing show method for notes in new admin (#624)
    * implementing index for notes new admin
    
    Co-authored-by: jv-vogler jvsvogler@gmail.com
    
    * implementing notes filters
    
    Co-authored-by: Daniel M Brasil <danielmbrasil@protonmail.com>
    
    * paginations, authorizations, missing specs
    
    * adding empty spaces in file ends
    
    * implementing show method
    
    * implementing create method for notes (#627)
    
    * implementing create method for notes
    
    * fixing specs
    
    * implementing edit and update methods (#630)
    
    * implementing edit and update methods
    
    * small changes
    
    * implementing delete method for notes in new admin (#633)
    
    * implementing create method for notes
    
    * implementing edit and update methods
    
    * implementing delete method for notes in new admin
    
    * removing blank lines
    
    ---------
    
    Co-authored-by: Alessandro Dias Batista <alessandro.dias@codeminer42.com>
    
    * fixing intermitent test
    
    ---------
    
    Co-authored-by: Daniel M Brasil <danielmbrasil@protonmail.com>
    Co-authored-by: Alessandro Dias Batista <alessandro.dias@codeminer42.com>

--- app/controllers/new_admin/notes_controller.rb
+++ app/controllers/new_admin/notes_controller.rb
@@ -8,6 +8,44 @@ module NewAdmin
       @notes = paginate_record(notes)
     end
 
+    def show
+      @note = Note.find(params[:id])
+    end
+
+    def new
+      @note = Note.new
+    end
+
+    def create
+      @note = Note.new(note_params)
+
+      if @note.save
+        redirect_on_success new_admin_notes_path, message_scope: 'create'
+      else
+        render_on_failure :new
+      end
+    end
+
+    def edit
+      @note = Note.find(params[:id])
+    end
+
+    def update
+      @note = Note.find(params[:id])
+
+      if @note.update(note_params)
+        redirect_on_success new_admin_show_note_path(id: @note.id), message_scope: 'update'
+      else
+        render_on_failure :edit
+      end
+    end
+
+    def destroy
+      @note = Note.find(params[:id])
+      @note.destroy
+      redirect_on_success new_admin_notes_path, message_scope: 'destroy'
+    end
+
     private
 
     def notes
@@ -21,6 +59,21 @@ module NewAdmin
         :author_id,
         :rate
       )
+    end
+
+    def note_params
+      params.require(:note).permit(:title, :author_id, :user_id, :comment, :rate)
+    end
+
+    def redirect_on_success(url, message_scope:)
+      flash[:notice] = I18n.t(:notice, scope: "flash.actions.#{message_scope}",
+                                       resource_name: Note.model_name.human)
+      redirect_to url
+    end
+
+    def render_on_failure(template)
+      flash.now[:alert] = @note.errors.full_messages.to_sentence
+      render template, status: :unprocessable_entity
     end
   end
 end
