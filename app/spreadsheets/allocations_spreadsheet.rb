441c1af2a93e175cdcf005d630ab78bded761770
NehamaMandelbaum <nehama1836@gmail.com>
2023-12-26 13:21:14 -0300

    changing data returned in allocations spreadsheets

--- app/spreadsheets/allocations_spreadsheet.rb
+++ app/spreadsheets/allocations_spreadsheet.rb
@@ -4,20 +4,20 @@ class AllocationsSpreadsheet < DefaultSpreadsheet
   def body(allocation)
     [
       allocation.user.name,
-      allocation.user.specialty,
-      allocation.user.decorate.office_head_name,
-      allocation.project&.name,
-      translate_date(allocation.end_at)
+      allocation.user.email,
+      allocation.project.name,
+      allocation.user.mentor ? allocation.user.mentor.name : '-',
+      allocation.user.mentor ? allocation.user.mentor.email : '-'
     ]
   end
 
   def header
     [
       User.human_attribute_name('name'),
-      User.human_attribute_name('specialty'),
-      Office.human_attribute_name('head'),
-      Allocation.human_attribute_name('project'),
-      Allocation.human_attribute_name('end_at')
+      User.human_attribute_name('email'),
+      I18n.t('allocation.project_name'),
+      I18n.t('allocation.mentor_name'),
+      I18n.t('allocation.mentor_email')
     ]
   end
 end
