62a559d02f6951299bc1c0476a5ee2ba075ea507
Nehama Mandelbaum <75209689+nehamamandelbaum@users.noreply.github.com>
2023-12-26 14:00:39 -0300

    Export allocations with new data to accomodate needs (#654)
    * changing data returned in allocations spreadsheets
    
    * leaving blank space when user has no mentor

--- spec/spreadsheets/allocations_spreadsheet_spec.rb
+++ spec/spreadsheets/allocations_spreadsheet_spec.rb
@@ -3,59 +3,94 @@
 require 'rails_helper'
 
 RSpec.describe AllocationsSpreadsheet do
-  let(:office_head) { create(:user, name: 'Wender') }
-  let(:office) { create(:office, head: office_head) }
-  let(:user) { create(:user, name: 'Philipe', specialty: 'backend', office: office) }
-  let(:project) { create(:project, name: 'Punchclock') }
-  let(:allocation) { create(:allocation, user: user, project: project, end_at: 10.days.from_now) }
+  context 'when user has mentor' do
+    let(:mentor) { create(:user, name: 'Gabriel', email: 'gabriel@code.com') }
+    let(:user) { create(:user, name: 'Philipe', email: 'philipe@code.com', mentor:) }
+    let(:project) { create(:project, name: 'Punchclock') }
+    let(:allocation) { create(:allocation, user:, project:, end_at: 10.days.from_now) }
 
-  let(:allocation_spreadsheet) { AllocationsSpreadsheet.new([allocation]) }
-  let(:header_attributes) do
-    [
-      User.human_attribute_name('name'),
-      User.human_attribute_name('specialty'),
-      Office.human_attribute_name('head'),
-      Allocation.human_attribute_name('project'),
-      Allocation.human_attribute_name('end_at')
-    ]
-  end
-
-  let(:body_attributes) do
-    [
-      'Philipe',
-      'backend',
-      'Wender',
-      'Punchclock',
-      I18n.l(10.days.from_now, format: '%d de %B de %Y')
-    ]
-  end
-
-  describe '#to_string_io' do
-    subject(:to_string_io) do
-      allocation_spreadsheet.to_string_io
+    let(:allocation_spreadsheet) { AllocationsSpreadsheet.new([allocation]) }
+    let(:header_attributes) do
+      [
+        User.human_attribute_name('name'),
+        User.human_attribute_name('email'),
+        I18n.t('allocation.project_name'),
+        I18n.t('allocation.mentor_name'),
+        I18n.t('allocation.mentor_email')
+      ]
     end
 
-    before do
-      File.open('/tmp/spreadsheet_temp.xlsx', 'wb') {|f| f.write(subject) }
+    let(:body_attributes) do
+      [
+        'Philipe',
+        'philipe@code.com',
+        'Punchclock',
+        'Gabriel',
+        'gabriel@code.com'
+      ]
     end
 
-    it_behaves_like 'a valid spreadsheet'
-    it_behaves_like 'a spreadsheet with header and body'
-  end
+    describe '#to_string_io' do
+      subject(:to_string_io) do
+        allocation_spreadsheet.to_string_io
+      end
 
-  describe '#body' do
-    subject { allocation_spreadsheet.body(allocation) }
+      before do
+        File.open('/tmp/spreadsheet_temp.xlsx', 'wb') { |f| f.write(subject) }
+      end
 
-    it 'return body data' do
-      is_expected.to containing_exactly(*body_attributes)
+      it_behaves_like 'a valid spreadsheet'
+      it_behaves_like 'a spreadsheet with header and body'
+    end
+
+    describe '#body' do
+      subject { allocation_spreadsheet.body(allocation) }
+
+      it 'return body data' do
+        is_expected.to containing_exactly(*body_attributes)
+      end
+    end
+
+    describe '#header' do
+      subject { allocation_spreadsheet.header }
+
+      it 'returns header data' do
+        is_expected.to containing_exactly(*header_attributes)
+      end
     end
   end
 
-  describe '#header' do
-    subject { allocation_spreadsheet.header }
+  context 'when user has no mentor' do
+    let(:user) { create(:user, name: 'Philipe', email: 'philipe@code.com') }
+    let(:project) { create(:project, name: 'Punchclock') }
+    let(:allocation) { create(:allocation, user:, project:, end_at: 10.days.from_now) }
+    let(:allocation_spreadsheet) { AllocationsSpreadsheet.new([allocation]) }
+    let(:header_attributes) do
+      [
+        User.human_attribute_name('name'),
+        User.human_attribute_name('email'),
+        I18n.t('allocation.project_name'),
+        I18n.t('allocation.mentor_name'),
+        I18n.t('allocation.mentor_email')
+      ]
+    end
 
-    it 'returns header data' do
-      is_expected.to containing_exactly(*header_attributes)
+    let(:body_attributes) do
+      [
+        'Philipe',
+        'philipe@code.com',
+        'Punchclock',
+        '',
+        ''
+      ]
+    end
+
+    describe '#body' do
+      subject { allocation_spreadsheet.body(allocation) }
+
+      it 'return body data' do
+        is_expected.to containing_exactly(*body_attributes)
+      end
     end
   end
 end
